7c5dfe2c77b4fd092baeabeb44545203b550cf16
Michael Klein <mbklein@gmail.com>
2011-05-17 23:14:22 -0700

    Change configuration style to match new Dor::Config ModCons object

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,18 +1,25 @@
 cert_dir = File.expand_path(File.join(File.dirname(__FILE__),"../certs"))
 
-Dor::Config.configure do |config|
-  config.fedora_url = 'http://fedoraAdmin:fedoraAdmin@dor-dev.stanford.edu/fedora'
-  config.fedora_cert_file = File.join(cert_dir,"dlss-dev-test.crt")
-  config.fedora_key_file = File.join(cert_dir,"dlss-dev-test.key")
-  config.fedora_key_pass = ''
-
-  config.gsearch_solr_url = 'http://dor-dev.stanford.edu/solr'
-  config.mint_suri_ids = true
-  config.id_namespace = 'druid'
-  config.suri_url = 'http://lyberservices-dev.stanford.edu:8080'
-  config.suri_user = 'labware'
-  config.suri_pass = 'lyberteam'
+Dor::Config.configure do
+  fedora do
+    url 'http://fedoraAdmin:fedoraAdmin@dor-dev.stanford.edu/fedora'
+    cert_file File.join(cert_dir,"dlss-dev-test.crt")
+    key_file File.join(cert_dir,"dlss-dev-test.key")
+    key_pass ''
+  end
   
-  config.exist_url = 'http://viewer:l3l%40nd@lyberapps-dev.stanford.edu/exist/rest'
-  config.catalog_url = 'http://lyberservices-prod.stanford.edu/catalog/mods'
+  gsearch.url 'http://dor-dev.stanford.edu/solr'
+  
+  suri do
+    mint_ids true
+    id_namespace 'druid'
+    url 'http://lyberservices-dev.stanford.edu:8080'
+    user 'labware'
+    pass 'lyberteam'
+  end
+
+  metadata do
+    exist.url 'http://viewer:l3l%40nd@lyberapps-dev.stanford.edu/exist/rest'
+    catalog.url 'http://lyberservices-prod.stanford.edu/catalog/mods'
+  end
 end
