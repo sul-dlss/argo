8d86ee5da53a5493d0209ff67ccadca6afcae5a9
Michael Klein <mbklein@gmail.com>
2011-07-06 19:17:23 -0700

    Pluralize ItemsController; move registration form from /registration to /items/register

--- test/functional/items_controller_test.rb
+++ test/functional/items_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class ItemControllerTest < ActionController::TestCase
+class ItemsControllerTest < ActionController::TestCase
   # Replace this with your real tests.
   test "the truth" do
     assert true
