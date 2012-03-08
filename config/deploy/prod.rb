85e1e4ea61d8e640e3d55eceff1d0daa9fe0359d
Michael Klein <mbklein@gmail.com>
2012-03-08 15:20:08 -0800

    Add environment-specific deploy scripts

---
+++ config/deploy/prod.rb
@@ -0,0 +1,8 @@
+set :rails_env, "production"
+set :deployment_host, "lyberapps-prod.stanford.edu"
+set :branch, "master"
+set :bundle_without, [:deployment,:development,:test]
+
+role :web, deployment_host
+role :app, deployment_host
+role :db,  deployment_host, :primary => true
