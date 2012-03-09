d9a7cad292f0438eb63eab9392960e6e31d5a2d9
Michael Klein <mbklein@gmail.com>
2012-03-09 07:51:00 -0800

    Fix solr bug in Reference model

--- app/models/reference.rb
+++ app/models/reference.rb
@@ -1,7 +1,7 @@
 class Reference
 
   def self.find(druid)
-    Dor::SearchService.query(%{id:"#{druid}"}, :rows => 1).first
+    Dor::SearchService.query(%{id:"#{druid}"}, :rows => 1).docs.first
   end
   
 end
