6850a5e4d45184970cba54b89e5cbb7197891c32
Michael Klein <mbklein@gmail.com>
2012-07-24 17:27:22 -0700

    Move REST registration logic from Argo's ObjectsController#create to Dor::RegistrationService#create_from_request

--- app/controllers/dor/objects_controller.rb
+++ app/controllers/dor/objects_controller.rb
@@ -6,44 +6,12 @@ class Dor::ObjectsController < ApplicationController
 
   def create
     begin
-      other_ids = Array(params[:other_id]).collect do |id|
-        if id =~ /^symphony:(.+)$/
-          "#{$1.length < 14 ? 'catkey' : 'barcode'}:#{$1}"
-        else
-          id
-        end
-      end
-    
-      if params[:label] == ':auto'
-        params.delete(:label)
-        params.delete('label')
-        metadata_id = Dor::MetadataService.resolvable(other_ids).first
-        params[:label] = Dor::MetadataService.label_for(metadata_id)
-      end
-          
-      dor_params = {
-        :pid                => params[:pid],
-        :admin_policy       => params[:admin_policy],
-        :content_model      => params[:model],
-        :label              => params[:label],
-        :object_type        => params[:object_type],
-        :other_ids          => help.ids_to_hash(other_ids),
-        :parent             => params[:parent],
-        :source_id          => help.ids_to_hash(params[:source_id]),
-        :tags               => params[:tag],
-        :seed_datastream    => params[:seed_datastream],
-        :initiate_workflow  => Array(params[:initiate_workflow]) + Array(params[:workflow_id])
-      }
-    
-      dor_params[:tags] << "Registered By : #{webauth.login}"
-      Rails.logger.info(dor_params.inspect)
-      dor_obj = Dor::RegistrationService.register_object(dor_params)
-      pid = dor_obj.pid
-      reg_response = dor_params.dup.merge({ :location => help.object_location(pid), :pid => pid })
-
+      response = Dor::RegistrationService.create_from_request(params)
+      pid = response[:pid]
+      
       respond_to do |format|
-        format.json { render :json => reg_response, :location => help.object_location(pid) }
-        format.xml  { render :xml  => reg_response, :location => help.object_location(pid) }
+        format.json { render :json => response, :location => help.object_location(pid) }
+        format.xml  { render :xml  => response, :location => help.object_location(pid) }
         format.text { render :text => pid, :location => help.object_location(pid) }
         format.html { redirect_to help.object_location(pid) }
       end
--- app/helpers/dor/objects_helper.rb
+++ app/helpers/dor/objects_helper.rb
@@ -8,13 +8,5 @@ module Dor::ObjectsHelper
       params[key.to_sym] = v
     }
   end
-
-  def ids_to_hash(ids)
-    if ids.nil?
-      nil
-    else
-      Hash[Array(ids).collect { |id| id.split(/:/) }]
-    end
-  end
     
 end
