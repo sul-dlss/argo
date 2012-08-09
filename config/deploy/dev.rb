f5394e3b5827929f6c001c8d5e4d6de6ac4e4082
Chris Beer <cabeer@stanford.edu>
2012-08-09 11:53:43 -0700

    update dev capistrano env

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,9 +1,11 @@
 set :rails_env, "development"
 set :activemq_host, "dor-dev.stanford.edu"
-set :deployment_host, "lyberapps-dev.stanford.edu"
+set :deployment_host, "argo-dev.stanford.edu"
 set :repository,  "."
 set :branch, "develop"
 set :bundle_without, [:deployment,:production]
+set :rvm_ruby_string, "1.9.3"
+set :destination, "/home/lyberadmin"
 
 role :web, deployment_host
 role :app, deployment_host
