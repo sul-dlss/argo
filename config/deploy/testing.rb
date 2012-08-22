01e762c116910e142e1de370fda488f26a0313e5
Jonathan Deering <jdeerin1@slu.edu>
2012-08-22 15:34:07 -0700

    fixed deployment again

--- config/deploy/testing.rb
+++ config/deploy/testing.rb
@@ -4,6 +4,9 @@ set :deployment_host, "lyberapps-test.stanford.edu"
 set :repository,  "."
 set :branch, "develop"
 set :bundle_without, [:deployment,:development]
+set :destination, "/var/opt/home/lyberadmin"
+set :application, "argo"
+set :deploy_to, "#{destination}/#{application}"
 
 role :web, deployment_host
 role :app, deployment_host
