47f0b26b2b3a5d56453be8e650e186bb24255b3e
Jonathan Deering <jdeerin1@slu.edu>
2012-08-29 15:36:39 -0700

    Removed premature versioning verbage

--- spec/helpers/dor_object_helper_spec.rb
+++ spec/helpers/dor_object_helper_spec.rb
@@ -14,7 +14,7 @@ describe DorObjectHelper do
           lifecycle_data=Array.new
           lifecycle_data << 'registered:2011-10-24 10:41pm'
           doc={ 'lifecycle_display' => lifecycle_data }
-          @dummy.render_status(doc).should == 'v1 Registered 2011-10-24 03:41pm'
+          @dummy.render_status(doc).should == ' Registered 2011-10-24 03:41pm'
     end
     it 'should include an embargo date if the item is embargoed' do
       lifecycle_data=Array.new
@@ -22,14 +22,14 @@ describe DorObjectHelper do
       embargo_data=Array.new
       embargo_data << 'embargoed until 2012-11-27 3:41pm'
       doc={ 'lifecycle_display' => lifecycle_data,'embargoMetadata_t' =>embargo_data }
-      @dummy.render_status(doc).should == 'v1 Registered 2011-10-24 03:41pm (embargoed until 2012-11-27 07:41am)'
+      @dummy.render_status(doc).should == ' Registered 2011-10-24 03:41pm (embargoed until 2012-11-27 07:41am)'
     end
     it 'should not include an embargo date if the item was released from embargo' do
       lifecycle_data=Array.new
       lifecycle_data << 'registered:2011-10-24 10:41pm'
       embargo_data = 'released 2012-11-27 10:41pm'
       doc={ 'lifecycle_display' => lifecycle_data,'embargoMetadata_t' =>embargo_data }
-      @dummy.render_status(doc).should == 'v1 Registered 2011-10-24 03:41pm'
+      @dummy.render_status(doc).should == ' Registered 2011-10-24 03:41pm'
     end
   end
 end
