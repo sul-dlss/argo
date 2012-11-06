9a168c04216417a710a51d11097074270af416a2
Jonathan Deering <jdeerin1@slu.edu>
2012-11-06 11:29:12 -0800

    removed and errant javascript

--- app/assets/javascripts/report.js
+++
@@ -1,8 +0,0 @@
-function add_constraint()
-{
-	constraints=document.getElementById('constraints');
-	text=document.getElementById('query_field').value;
-	text=document.getElementById('relationship').value;
-	text=document.getElementById('constraint').value;
-	constraints.innerHTML+=text+'<br>';
-}
