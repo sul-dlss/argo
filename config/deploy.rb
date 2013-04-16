be3b9bf6cf17180300387a81dddf6568593efabb
Chris Beer <cabeer@stanford.edu>
2013-04-16 15:22:16 -0700

    move .rvmrc

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,3 @@
-require "rvm/capistrano"                               # Load RVM's capistrano plugin.
 require 'net/ssh/kerberos'
 require 'bundler/setup'
 require 'bundler/capistrano'
@@ -7,12 +6,9 @@ require 'dlss/capistrano'
 set :stages, %W(dev testing prod)
 set :default_stage, "development"
 set :bundle_flags, "--quiet"
-set :rvm_ruby_string, "1.9.3"
-set :rvm_type, :system
 
 require 'capistrano/ext/multistage'
 
-after "deploy:create_symlink", "argo:trust_rvmrc"
 after "deploy:create_symlink", "argo:initialize_htaccess"
 after "deploy:create_symlink", "argo:restart_indexer"
 after "deploy", "deploy:migrate"
@@ -34,10 +30,6 @@ set :keep_releases, 10
 
 
 namespace :argo do
-  task :trust_rvmrc do
-    run "/usr/local/rvm/bin/rvm rvmrc trust #{latest_release}"
-  end
-  
   task :initialize_htaccess do
     run "cd #{latest_release} && bundle exec rake RAILS_ENV=#{rails_env} argo:htaccess"
   end
