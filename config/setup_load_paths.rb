ef8e0900a79291f6ebc71296a9439e5f1e37dabb
Chris Beer <cabeer@stanford.edu>
2013-04-16 15:38:52 -0700

    kill rvm

--- config/setup_load_paths.rb
+++ config/setup_load_paths.rb
@@ -1,15 +1,3 @@
-if ENV['MY_RUBY_HOME'] && ENV['MY_RUBY_HOME'].include?('rvm')
-  begin
-    rvm_path     = File.dirname(File.dirname(ENV['MY_RUBY_HOME']))
-    rvm_lib_path = File.join(rvm_path, 'lib')
-    require 'rvm'
-    RVM.use_from_path! File.dirname(File.dirname(__FILE__))
-  rescue LoadError
-    # RVM is unavailable at this point.
-    raise "RVM ruby lib is currently unavailable."
-  end
-end
-
 # Select the correct item for which you use below.
 # If you're not using bundler, remove it completely.
 #
