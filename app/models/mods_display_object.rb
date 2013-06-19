3c1e90eed850dd3b440b1348ef20f8871e2d4f1d
Jonathan Deering <jdeering@stanford.edu>
2013-06-18 17:01:05 -0700

    add the missing model

---
+++ app/models/mods_display_object.rb
@@ -0,0 +1,19 @@
+class ModsDisplayObject
+  include ModsDisplay::ModelExtension
+  
+  def initialize xml
+    @xml=xml
+  end
+
+  def xml
+    @xml
+  end
+  def modsxml
+    @xml
+  end
+  
+  mods_xml_source do |obj|
+    puts obj.xml
+      obj.xml
+  end
+end
