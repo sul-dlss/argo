725c8b90634f25b011d4f01e15bf6a6437fae9e7
Jonathan Deering <jdeering@stanford.edu>
2013-07-10 13:36:15 -0700

    adding 7 hour offset to robot monitoring

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -3,7 +3,7 @@ class Robot < ActiveRecord::Base
   
   def status
     puts "wf_#{wf}_#{process}_dt_facet"
-     docs = Dor::SearchService.query("wf_#{wf}_#{process}_dt:[NOW-15MINUTES TO NOW]", {:rows => 20, :fl => 'id'})['response']['docs']
+     docs = Dor::SearchService.query("wf_#{wf}_#{process}_dt:[NOW-7HOURS-15MINUTES TO NOW]", {:rows => 20, :fl => 'id'})['response']['docs']
      if docs.length >0 
        true
      else
@@ -11,7 +11,7 @@ class Robot < ActiveRecord::Base
      end
   end
   def status_long
-    docs = Dor::SearchService.query("wf_#{wf}_#{process}_dt:[NOW-1HOUR  TO NOW]", {:rows => 20, :fl => 'id'})['response']['docs']
+    docs = Dor::SearchService.query("wf_#{wf}_#{process}_dt:[NOW-8HOURS  TO NOW]", {:rows => 20, :fl => 'id'})['response']['docs']
     if docs.length >0 
       true
     else
@@ -19,6 +19,6 @@ class Robot < ActiveRecord::Base
     end
   end
   def recent_work
-    docs = Dor::SearchService.query("wf_#{wf}_#{process}_dt:[NOW-1HOUR  TO NOW]", {:rows => 20, :fl => 'id,dc_title_t,apo_title_facet'})['response']['docs']
+    docs = Dor::SearchService.query("wf_#{wf}_#{process}_dt:[NOW-8HOURS  TO NOW]", {:rows => 20, :fl => 'id,dc_title_t,apo_title_facet'})['response']['docs']
   end
 end
