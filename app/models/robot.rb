858bca2e1210884cc7b4cb29d50b0e1eeab20736
Jonathan Deering <jdeering@stanford.edu>
2013-07-15 15:57:23 -0700

    Check workflow queues to see if a robot is inactive because there is no work, and display that differently.

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -2,23 +2,37 @@ class Robot < ActiveRecord::Base
   attr_accessible :wf, :process
   
   def status
-    puts "wf_#{wf}_#{process}_dt_facet"
-     docs = Dor::SearchService.query("wf_#{wf}_#{process}_dt:[NOW-7HOURS-15MINUTES TO NOW]", {:rows => 20, :fl => 'id'})['response']['docs']
-     if docs.length >0 
-       true
-     else
-       false
-     end
+    ready_docs=Dor::SearchService.query("wf_wps_facet:\"#{wf}:#{process}:ready\"")['response']['docs']
+    if ready_docs.length > 0
+      
+      docs = Dor::SearchService.query("wf_#{wf}_#{process}_dt:[NOW-7HOURS-1HOUR TO NOW]", {:rows => 20, :fl => 'id'})['response']['docs']
+      if docs.length >0 
+        @status = 'activity'
+      else
+        @status = 'no_activity'
+      end
+    else
+      @status = 'sleep'
+    end
+    puts @status
+    @status
   end
   def status_long
-    docs = Dor::SearchService.query("wf_#{wf}_#{process}_dt:[NOW-8HOURS  TO NOW]", {:rows => 20, :fl => 'id'})['response']['docs']
-    if docs.length >0 
-      true
+    ready_docs=Dor::SearchService.query("wf_wps_facet:\"#{wf}:#{process}:ready\"")['response']['docs']
+    if ready_docs.length > 0
+      
+      docs = Dor::SearchService.query("wf_#{wf}_#{process}_dt:[NOW-7HOURS-4HOURS TO NOW]", {:rows => 20, :fl => 'id'})['response']['docs']
+      if docs.length >0 
+        @status = 'activity'
+      else
+        @status = 'no_activity'
+      end
     else
-      false
+      @status = 'sleep'
     end
+    @status
   end
   def recent_work
-    docs = Dor::SearchService.query("wf_#{wf}_#{process}_dt:[NOW-8HOURS  TO NOW]", {:rows => 20, :fl => 'id,dc_title_t,apo_title_facet'})['response']['docs']
+    docs = Dor::SearchService.query("wf_#{wf}_#{process}_dt:[NOW-7HOURS-1HOUR  TO NOW]", {:rows => 20, :fl => 'id,dc_title_t,apo_title_facet'})['response']['docs']
   end
 end
