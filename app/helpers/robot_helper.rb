fbf58a80fd03f118c2ab665a62c33ca1b23373af
Jonathan Deering <jdeering@stanford.edu>
2013-07-15 16:05:58 -0700

    Label hydrus objects

--- app/helpers/robot_helper.rb
+++ app/helpers/robot_helper.rb
@@ -2,7 +2,7 @@ module RobotHelper
   def render_recent_work docs
     res=''
     docs.each do |doc|
-      res += render(:partial => 'item', :locals => {:title => doc['dc_title_t'], :druid => doc['id'], :apo_title => doc['apo_title_facet']})
+      res += render(:partial => 'item', :locals => {:title => doc['dc_title_t'].first, :druid => doc['id'], :apo_title => doc['apo_title_facet'] ? doc['apo_title_facet'] : "Hydrus Object"})
     end
     res.html_safe
   end
