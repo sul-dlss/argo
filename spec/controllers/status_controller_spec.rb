2aafbfdfb5e2d64514853951fc6b41bcf060052c
Jonathan Deering <jdeering@stanford.edu>
2013-07-16 13:11:14 -0700

    rewriting the status controller tests due to the new solr query for indexer monitoring

--- spec/controllers/status_controller_spec.rb
+++ spec/controllers/status_controller_spec.rb
@@ -3,33 +3,7 @@ require 'spec_helper'
 describe StatusController do
 
   describe "GET 'log'" do
-    it "returns http success" do
-			File.stub(:new){StringIO.new("W, ["+DateTime.now.strftime('%Y-%m-%d %H:%M:%S ')+". #27663]  WARN -- :  updated solr index for druid:gb234tt5124")}
-			get 'log'
-      response.code.should =='200'
-    end
-
-    it "fails if the last log time was more than 15 minutes ago " do
-			#the time has to be incorrectly offset to gmt like it is in the logs, hence the additional -420 minutes
-			File.stub(:new){StringIO.new("W, ["+497.minutes.ago.strftime('%Y-%m-%d %H:%M:%S ')+". #27663]  WARN -- :  updated solr index for druid:gb234tt5124")}
-			get 'log'
-      response.code.should == '500' 
-    end
-    it 'should trigger a Dor:Item.save when nothing has been indexed recently and a test druid was passed in' do
-    	save_count=0
-    	@item=mock(Dor::Item)
-    	@item.should_receive(:save) 
-    	@item.stub(:dirty)
-    	mock_md=mock(Dor::IdentityMetadataDS)
-    	mock_md.stub(:content)
-    	mock_md.stub(:ng_xml).and_return('hello')
-    	mock_md.stub(:dirty=)
-    	@item.stub(:identityMetadata).and_return(mock_md)
-    	Dor::Item.stub(:find).and_return(@item)
-			#the time has to be incorrectly offset to gmt like it is in the logs, hence the additional -420 minutes
-			File.stub(:new){StringIO.new("W, ["+497.minutes.ago.strftime('%Y-%m-%d %H:%M:%S ')+". #27663]  WARN -- :  updated solr index for druid:gb234tt5124")}
-			get 'log',:test_obj=>'druid:bs846vw6460'
-    end
+   
   end
   
 
