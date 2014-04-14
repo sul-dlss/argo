393d3cd0714bdde3b8d0a21617de5e14bb9fcbf9
Carrick Rogers <carrickr@stanford.edu>
2014-04-14 15:53:33 -0700

    Revert "Updating the testing cap deploy to actually use RAILS_ENV=testing"
    This reverts commit fdc4da2bd90e0d0e14cfdc2dc074742c0bacdced.

--- config/deploy/testing.rb
+++ config/deploy/testing.rb
@@ -1,4 +1,4 @@
-set :rails_env, "testing"
+set :rails_env, "production"
 set :activemq_host, "dor-test.stanford.edu"
 set :deployment_host, "argo-test.stanford.edu"
 set :repository,  "https://github.com/sul-dlss/argo.git"
