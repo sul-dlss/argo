1347d561dfc6da8af659bbe94f1f5d111842584a
Johnathan Martin <john.martin@stanford.edu>
2014-08-04 18:31:08 -0700

    ARGO-302:  prior commit added the old manager and viewer workgroup names to the wrong list, unswitched them

--- config/initializers/permissions.rb
+++ config/initializers/permissions.rb
@@ -1,3 +1,3 @@
 ADMIN_GROUPS=["workgroup:sdr:administrator-role", "workgroup:dlss:dor-admin"]
-VIEWER_GROUPS=["workgroup:sdr:viewer-role", "workgroup:dlss:dor-manager"]
-MANAGER_GROUPS=["workgroup:sdr:manager-role", "workgroup:dlss:dor-viewer"]
+VIEWER_GROUPS=["workgroup:sdr:viewer-role", "workgroup:dlss:dor-viewer"]
+MANAGER_GROUPS=["workgroup:sdr:manager-role", "workgroup:dlss:dor-manager"]
