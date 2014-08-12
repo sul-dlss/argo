5472b2059862cc07da513027774abf4a480bac3b
Johnathan Martin <john.martin@stanford.edu>
2014-08-12 16:46:40 -0700

    deployment config fix

--- config/deploy/testing.rb
+++ config/deploy/testing.rb
@@ -1,5 +1,5 @@
 server 'argo-test.stanford.edu', user: 'lyberadmin', roles: %w{web db app}
 
 Capistrano::OneTimeKey.generate_one_time_key!
-set :rails_env, "production"
+set :rails_env, "testing"
 set :branch, :develop
