5d0df3c43e710568acb60e4b3434c96b53fd4a48
Johnathan Martin <john.martin@stanford.edu>
2014-09-15 16:35:32 -0700

    ARGO-336: add link to robots status page from argo workflow grid page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,6 +16,10 @@ module ApplicationHelper
   def inflect(str,num)
     "%d %s" % [num, (num == 1 ? str.singularize : str.pluralize)]
   end
+
+  def robot_status_url
+    return Argo::Config.urls.robot_status
+  end
   
   protected
   
