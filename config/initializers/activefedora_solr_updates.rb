d144755e7da62289727ce24fd2adbd50a5e4e636
Joe Atzberger <ohiocore@gmail.com>
2014-09-22 16:31:10 -0700

    Silence warning about redefining constant

--- config/initializers/activefedora_solr_updates.rb
+++ config/initializers/activefedora_solr_updates.rb
@@ -1,1 +1,7 @@
-::ENABLE_SOLR_UPDATES = false
+# Cannot just do:
+#   ::ENABLE_SOLR_UPDATES = false
+# Because "warning: already initialized constant ENABLE_SOLR_UPDATES".
+# ActiveFedora assigns a default (if unassigned) and we cannot pre-empt it from here.
+
+self.class.send(:remove_const, 'ENABLE_SOLR_UPDATES') if self.class.const_defined?('ENABLE_SOLR_UPDATES')
+self.class.const_set('ENABLE_SOLR_UPDATES', false)
