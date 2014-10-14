ea79466b509c3c23ccb57c43507089fe6912ddc7
Johnathan Martin <john.martin@stanford.edu>
2014-10-14 15:18:53 -0700

    ARGO-360: fix indentation

--- app/controllers/dor_controller.rb
+++ app/controllers/dor_controller.rb
@@ -25,49 +25,48 @@ class DorController < ApplicationController
     respond_with(result)
   end
 
-    def query_by_id
-      unless params[:id]
-        response.status = 400
-        return
-      end
-
-      result = Dor::SearchService.query_by_id(params[:id]).collect do |pid|
-        { :id => pid, :url => url_for(:controller => 'dor/objects', :id => pid) }
-      end
-
-      respond_with(result) do |format|
-        format.any(:json, :xml) { render request.format.to_sym => result }
-        format.text { render :text => result.collect { |v| v[:id] }.join("\n") }
-      end
+  def query_by_id
+    unless params[:id]
+      response.status = 400
+      return
     end
 
-    def label
-      respond_with params.merge('label' => Dor::MetadataService.label_for(params[:source_id]))
+    result = Dor::SearchService.query_by_id(params[:id]).collect do |pid|
+      { :id => pid, :url => url_for(:controller => 'dor/objects', :id => pid) }
     end
 
-    def reindex
-      begin
-        obj = Dor.load_instance params[:pid]
-        solr_doc = obj.to_solr
-        Dor::SearchService.solr.add(solr_doc, :add_attributes => {:commitWithin => 1000}) unless obj.nil?
-        index_logger.info "updated index for #{params[:pid]}"
-        render :text => 'Status:ok<br> Solr Document: '+solr_doc.inspect
-      rescue ActiveFedora::ObjectNotFoundError => e
-        render :status=> 500, :text =>'Object doesnt exist in Fedora.'
-        return
-      end
+    respond_with(result) do |format|
+      format.any(:json, :xml) { render request.format.to_sym => result }
+      format.text { render :text => result.collect { |v| v[:id] }.join("\n") }
     end
-    
+  end
 
-    def delete_from_index
-      Dor::SearchService.solr.delete_by_id(params[:pid])
-      Dor::SearchService.solr.commit
-      render :text => params[:pid]
-    end
+  def label
+    respond_with params.merge('label' => Dor::MetadataService.label_for(params[:source_id]))
+  end
 
-    def republish
-      obj=Dor::Item.find(params[:pid])
-      obj.publish_metadata_remotely
-      render :text => 'Republished! You still need to use the normal versioning process to make sure your changes are preserved.'
+  def reindex
+    begin
+      obj = Dor.load_instance params[:pid]
+      solr_doc = obj.to_solr
+      Dor::SearchService.solr.add(solr_doc, :add_attributes => {:commitWithin => 1000}) unless obj.nil?
+      index_logger.info "updated index for #{params[:pid]}"
+      render :text => 'Status:ok<br> Solr Document: '+solr_doc.inspect
+    rescue ActiveFedora::ObjectNotFoundError => e
+      render :status=> 500, :text =>'Object doesnt exist in Fedora.'
+      return
     end
+  end
+
+  def delete_from_index
+    Dor::SearchService.solr.delete_by_id(params[:pid])
+    Dor::SearchService.solr.commit
+    render :text => params[:pid]
+  end
+
+  def republish
+    obj=Dor::Item.find(params[:pid])
+    obj.publish_metadata_remotely
+    render :text => 'Republished! You still need to use the normal versioning process to make sure your changes are preserved.'
+  end
 end
