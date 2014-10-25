c864ed44bbba40e81a3c830e52b09ddc5c3b7a0e
Johnathan Martin <john.martin@stanford.edu>
2014-10-24 21:58:01 -0700

    ARGO-308: centralize collection creation code

--- app/controllers/apo_controller.rb
+++ app/controllers/apo_controller.rb
@@ -14,12 +14,18 @@ class ApoController < ApplicationController
     'by-nc-nd' => 'Attribution Non-commercial, No Derivatives 3.0 Unported',
   }
 
+  def is_valid_role_name role_name
+    return /^[\w-]+:[\w-]+$/.match(role_name) != nil
+  end
+
   def is_valid_role_list role_list
-    return role_list.find { |role| /^[\w-]+:[\w-]+$/.match(role) == nil } == nil
+    # look for an invalid role name, return true if we don't find one
+    return role_list.find { |role_name| !is_valid_role_name(role_name) } == nil
   end
 
   def is_valid_role_list_endpoint
-    role_list_str = params[:managers] || params[:viewers] || nil
+    # this should only get one of the params at a time
+    role_list_str = params[:managers] || params[:viewers] || params[:role_list] || nil
     if !role_list_str
       ret_val = false
     else
@@ -33,45 +39,48 @@ class ApoController < ApplicationController
     end
   end
 
+  def get_input_params_errors input_params
+    # assume no errors yet
+    err_list = []
+
+    # error if title is empty
+    if input_params[:title].strip.length == 0
+      err_list.push(:title)
+    end
+
+    # error if managers or viewers role list is invalid
+    [:managers, :viewers].each do |roleplayer_list_str|
+      if !is_valid_role_list(split_roleplayer_input_field(input_params[roleplayer_list_str]))
+        err_list.push(role_list)
+      end
+    end
+
+    return err_list
+  end
+
   def register
     param_cleanup params
+
     if params[:title]
       #register a new apo
-      reg_params={:workflow_priority => '70'}
+      
+      input_params_errors = get_input_params_errors params
+      if input_params_errors.length > 0
+        render :status=> :bad_request, :text => input_params_errors
+        return
+      end
+
+      reg_params = {:workflow_priority => '70'}
       reg_params[:label] = params[:title]
       reg_params[:object_type] = 'adminPolicy'
       reg_params[:admin_policy] = 'druid:hv992ry2431'
-      reg_params[:workflow_id]='accessionWF'
+      reg_params[:workflow_id] = 'accessionWF'
       response = Dor::RegistrationService.create_from_request(reg_params)
       pid = response[:pid]
       #register a collection if requested
-      collection_pid=nil
+      collection_pid = nil
       if params[:collection_radio]=='create'
-        reg_params={:workflow_priority => '65'}
-        if params[:collection_title] and params[:collection_title].length>0
-          reg_params[:label] = params[:collection_title]
-        else
-          reg_params[:label]= ':auto'
-        end
-        if reg_params[:label]==':auto'
-          reg_params[:rights]=params[:collection_rights_catkey]
-        else
-          reg_params[:rights]=params[:collection_rights]
-        end
-        if reg_params[:rights]
-          reg_params[:rights]=reg_params[:rights].downcase
-        end
-        reg_params[:object_type] = 'collection'
-        reg_params[:admin_policy] = pid
-        reg_params[:metadata_source]='symphony' if params[:collection_catkey] and params[:collection_catkey].length > 0
-        reg_params[:other_id]='symphony:' + params[:collection_catkey] if params[:collection_catkey] and params[:collection_catkey].length > 0
-        reg_params[:metadata_source]='label' unless params[:collection_catkey] and params[:collection_catkey].length > 0
-        reg_params[:workflow_id]='accessionWF'
-        response = Dor::RegistrationService.create_from_request(reg_params)
-        collection_pid = response[:pid]
-        if params[:collection_abstract] and params[:collection_abstract].length >0
-          set_abstract(collection_pid, params[:collection_abstract])
-        end
+        collection_pid = create_colection pid
       end
       item=Dor.find(pid)
       item.copyright_statement=params[:copyright]
@@ -124,6 +133,13 @@ class ApoController < ApplicationController
   end
 
   def update
+    param_cleanup params
+    input_params_errors = get_input_params_errors params
+    if input_params_errors.length > 0
+      render :status=> :bad_request, :text => input_params_errors
+      return
+    end
+
     @object.copyright_statement = params[:copyright] if params[:copyright] and params[:copyright].length > 0
     @object.use_statement = params[:use] if params[:use] and params[:use].length >0
     @object.mods_title = params[:title]
@@ -131,32 +147,7 @@ class ApoController < ApplicationController
     @object.metadata_source=params[:metadata_source]
     @object.agreement = params[:agreement].to_s
     if params[:collection_radio]=='create'
-      reg_params={:workflow_priority => '65'}
-      if params[:collection_title] and params[:collection_title].length>0
-        reg_params[:label] = params[:collection_title]
-      else
-        reg_params[:label]= ':auto'
-      end
-      if reg_params[:label]==':auto'
-        reg_params[:rights]=params[:collection_rights_catkey]
-      else
-        reg_params[:rights]=params[:collection_rights]
-      end
-      if reg_params[:rights]
-        reg_params[:rights]=reg_params[:rights].downcase
-      end
-      reg_params[:object_type] = 'collection'
-      reg_params[:admin_policy] = @object.pid
-      reg_params[:metadata_source]='symphony' if params[:collection_catkey] and params[:collection_catkey].length > 0
-      reg_params[:other_id]='symphony:' + params[:collection_catkey] if params[:collection_catkey] and params[:collection_catkey].length > 0
-      reg_params[:metadata_source]='label' unless params[:collection_catkey] and params[:collection_catkey].length > 0
-      reg_params[:workflow_id]='accessionWF'
-      response = Dor::RegistrationService.create_from_request(reg_params)
-      collection_pid = response[:pid]
-      if params[:collection_abstract] and params[:collection_abstract].length >0
-        set_abstract(collection_pid, params[:collection_abstract])
-      end
-
+      collection_pid = create_colection @object.pid
     end
 
     if params[:collection_select]='select' and params[:collection] and params[:collection].length > 0
@@ -181,35 +172,40 @@ class ApoController < ApplicationController
 
   def register_collection
     if params[:collection_title] or params[:collection_catkey]
-      reg_params={:workflow_priority => '65'}
-      if params[:collection_title] and params[:collection_title].length>0
-        reg_params[:label] = params[:collection_title]
-      else
-        reg_params[:label]= ':auto'
-      end
-      if reg_params[:label]==':auto'
-        reg_params[:rights]=params[:collection_rights_catkey]
-      else
-        reg_params[:rights]=params[:collection_rights]
-      end
-      if reg_params[:rights]
-        reg_params[:rights]=reg_params[:rights].downcase
-      end
-      reg_params[:object_type] = 'collection'
-      reg_params[:admin_policy] = params[:id]
-      reg_params[:metadata_source]='label'
-      reg_params[:metadata_source]='symphony' if params[:collection_catkey] and params[:collection_catkey].length > 0
-      reg_params[:other_id]='symphony:' + params[:collection_catkey] if params[:collection_catkey] and params[:collection_catkey].length > 0
-      reg_params[:metadata_source]='label' unless params[:collection_catkey] and params[:collection_catkey].length > 0
-      reg_params[:workflow_id]='accessionWF'
-      response = Dor::RegistrationService.create_from_request(reg_params)
-      collection_pid = response[:pid]
-      if params[:collection_abstract] and params[:collection_abstract].length >0
-        set_abstract(collection_pid, params[:collection_abstract])
-      end
+      collection_pid = create_colection params[:id], 'label'
       @object.add_default_collection collection_pid
       redirect_to catalog_path(params[:id]), :notice => "Created collection #{collection_pid}"
     end
+  end
+
+  def create_colection apo_pid, metadata_source_init_value=nil
+    reg_params = {:workflow_priority => '65'}
+    if params[:collection_title] && params[:collection_title].length > 0
+      reg_params[:label] = params[:collection_title]
+    else
+      reg_params[:label] = ':auto'
+    end
+    if reg_params[:label] == ':auto'
+      reg_params[:rights] = params[:collection_rights_catkey]
+    else
+      reg_params[:rights] = params[:collection_rights]
+    end
+    if reg_params[:rights]
+      reg_params[:rights] = reg_params[:rights].downcase
+    end
+    reg_params[:object_type] = 'collection'
+    reg_params[:admin_policy] = apo_pid
+    reg_params[:metadata_source] = metadata_source_init_value if metadata_source_init_value
+    reg_params[:metadata_source] = 'symphony' if params[:collection_catkey] && params[:collection_catkey].length > 0
+    reg_params[:other_id] = 'symphony:' + params[:collection_catkey] if params[:collection_catkey] && params[:collection_catkey].length > 0
+    reg_params[:metadata_source] = 'label' unless params[:collection_catkey] && params[:collection_catkey].length > 0
+    reg_params[:workflow_id] = 'accessionWF'
+    response = Dor::RegistrationService.create_from_request(reg_params)
+    collection_pid = response[:pid]
+    if params[:collection_abstract] && params[:collection_abstract].length > 0
+      set_abstract(collection_pid, params[:collection_abstract])
+    end
+    return collection_pid
   end
 
   def add_roleplayer
