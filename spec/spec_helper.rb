fb7f2c728ea0012091725f4b99e0d940eddaab57
Joe Atzberger <ohiocore@gmail.com>
2014-10-28 16:51:09 -0700

    Add some flexibility to helper method
    Backwards compatible

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -58,32 +58,33 @@ def log_in_as_mock_user(subject)
   subject.stub(:webauth).and_return(double(:webauth_user, :login => 'sunetid', :logged_in? => true))
 end
 
-def item_from_foxml(foxml, item_class = Dor::Base)
+# Highly similar to https://github.com/sul-dlss/dor-services/blob/master/spec/foxml_helper.rb
+def item_from_foxml(foxml, item_class = Dor::Base, other_class = ActiveFedora::NokogiriDatastream)
   foxml = Nokogiri::XML(foxml) unless foxml.is_a?(Nokogiri::XML::Node)
   xml_streams = foxml.xpath('//foxml:datastream')
   properties = Hash[foxml.xpath('//foxml:objectProperties/foxml:property').collect { |node| 
     [node['NAME'].split(/#/).last, node['VALUE']] 
   }]
   result = item_class.new(:pid => foxml.root['PID'])
-  result.label = properties['label']
+  result.label    = properties['label']
   result.owner_id = properties['ownerId']
   xml_streams.each do |stream|
     begin
-    content = stream.xpath('.//foxml:xmlContent/*').first.to_xml
-    dsid = stream['ID']
-    ds = result.datastreams[dsid]
-    if ds.nil?
-      ds = ActiveFedora::NokogiriDatastream.new(result,dsid)
-      result.add_datastream(ds)
-    end
+      content = stream.xpath('.//foxml:xmlContent/*').first.to_xml
+      dsid = stream['ID']
+      ds = result.datastreams[dsid]
+      if ds.nil?
+        ds = other_class.new(result,dsid)
+        result.add_datastream(ds)
+      end
 
-    if ds.is_a?(ActiveFedora::NokogiriDatastream)
-      result.datastreams[dsid] = ds.class.from_xml(Nokogiri::XML(content), ds)
-    elsif ds.is_a?(ActiveFedora::RelsExtDatastream)
-      result.datastreams[dsid] = ds.class.from_xml(content, ds)
-    else
-      result.datastreams[dsid] = ds.class.from_xml(ds, stream)
-    end
+      if ds.is_a?(other_class)
+        result.datastreams[dsid] = ds.class.from_xml(Nokogiri::XML(content), ds)
+      elsif ds.is_a?(ActiveFedora::RelsExtDatastream)
+        result.datastreams[dsid] = ds.class.from_xml(content, ds)
+      else
+        result.datastreams[dsid] = ds.class.from_xml(ds, stream)
+      end
     rescue
       #rescue if 1 datastream failed
     end
@@ -91,7 +92,7 @@ def item_from_foxml(foxml, item_class = Dor::Base)
 
   # stub item and datastream repo access methods
   result.datastreams.each_pair do |dsid,ds|
-    if ds.is_a?(ActiveFedora::NokogiriDatastream) and not ds.is_a?(Dor::WorkflowDs)
+    if ds.is_a?(other_class) and not ds.is_a?(Dor::WorkflowDs)
       ds.instance_eval do
         def content       ; self.ng_xml.to_s                 ; end
         def content=(val) ; self.ng_xml = Nokogiri::XML(val) ; end
