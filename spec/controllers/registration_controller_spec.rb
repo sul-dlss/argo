f019e9691c87d962e4dca17a6ba30e057031b266
Johnathan Martin <john.martin@stanford.edu>
2014-11-18 11:25:15 -0800

    ARGO-227: have rights_list check group tag for 'stanford', leave todo comment about making that more robust.  add a test case with a non-stanford group.  improve test case descriptions, remove a commented out pending.app/controllers/registration_controller.rb

--- spec/controllers/registration_controller_spec.rb
+++ spec/controllers/registration_controller_spec.rb
@@ -10,7 +10,7 @@ describe RegistrationController do
   end
 
   describe 'rights_list' do
-    it 'should show stanford as the default' do
+    it 'should show Stanford as the default if Stanford is the read group and discover is world' do
       content=<<-XML
       <?xml version="1.0"?>
       <rightsMetadata>
@@ -46,7 +46,43 @@ describe RegistrationController do
       expect(response.body.include?("Stanford (APO default)")).to eq(true)
     end
 
-    it 'should show world as the default' do
+    it 'should not show Stanford as the default if the read group is not Stanford' do
+      content=<<-XML
+      <?xml version="1.0"?>
+      <rightsMetadata>
+        <copyright>
+          <human type="copyright">This work is in the Public Domain.</human>
+        </copyright>
+        <access type="discover">
+          <machine>
+            <world/>
+          </machine>
+        </access>
+        <access type="read">
+          <machine>
+            <group>Berkeley</group>
+          </machine>
+        </access>
+        <use>
+          <human type="creativecommons">Attribution Share Alike license</human>
+          <machine type="creativecommons">by-sa</machine>
+        </use>
+      </rightsMetadata>
+      XML
+
+      pid='abc123'
+      @item=double(Dor::Item)
+      xml=Nokogiri::XML(content)
+      Dor.stub(:find).and_return(@item)
+      #using content metadata, but any datastream would do
+      object_rights=double(Dor::ContentMetadataDS)
+      object_rights.stub(:ng_xml).and_return xml
+      @item.stub(:defaultObjectRights).and_return object_rights
+      get 'rights_list', :apo_id => 'abc', :format => :xml
+      expect(response.body.include?("Stanford (APO default)")).to eq(false)
+    end
+
+    it 'should show World as the default if discover and read are both world' do
       content=<<-XML
       <?xml version="1.0"?>
       <rightsMetadata>
@@ -82,7 +118,7 @@ describe RegistrationController do
       expect(response.body.include?("World (APO default)")).to eq(true)
     end
 
-    it 'should show Dark if discover is none' do
+    it 'should show Dark as the default if discover and read are both none' do
       content=<<-XML
       <?xml version="1.0"?>
       <rightsMetadata>
@@ -118,7 +154,7 @@ describe RegistrationController do
       expect(response.body.include?("Dark (APO default)")).to eq(true)
     end
 
-    it 'should show Citation Only as the default' do
+    it 'should show Citation Only as the default if discover is world and read is none' do
       content=<<-XML
       <?xml version="1.0"?>
       <rightsMetadata>
@@ -154,44 +190,7 @@ describe RegistrationController do
       expect(response.body.include?("Citation Only (APO default)")).to eq(true)
     end
 
-    it 'should show Dark as the default' do
-      content=<<-XML
-      <?xml version="1.0"?>
-      <rightsMetadata>
-        <copyright>
-          <human type="copyright">This work is in the Public Domain.</human>
-        </copyright>
-        <access type="discover">
-          <machine>
-            <none/>
-          </machine>
-        </access>
-        <access type="read">
-          <machine>
-            <none/>
-          </machine>
-        </access>
-        <use>
-          <human type="creativecommons">Attribution Share Alike license</human>
-          <machine type="creativecommons">by-sa</machine>
-        </use>
-      </rightsMetadata>
-      XML
-
-      pid='abc123'
-      @item=double(Dor::Item)
-      xml=Nokogiri::XML(content)
-      Dor.stub(:find).and_return(@item)
-      #using content metadata, but any datastream would do
-      object_rights=double(Dor::ContentMetadataDS)
-      object_rights.stub(:ng_xml).and_return xml
-      @item.stub(:defaultObjectRights).and_return object_rights
-      get 'rights_list', :apo_id => 'abc', :format => :xml
-      expect(response.body.include?("Dark (APO default)")).to eq(true)
-    end
-
     it 'should show no default if there is no xml' do
-      # pending 'need to look into how to parse the response further and see how to check for lack of default selection and expected four options'
       content=''
       pid='abc123'
       @item=double(Dor::Item)
