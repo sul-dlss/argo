98811256991aaf26ac60fc1c01596d6d092f5795
Joe Atzberger <ohiocore@gmail.com>
2014-12-04 11:30:20 -0800

    Update debugger version, but remove unused pry require

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,6 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'capybara/rails'
 require 'capybara/rspec'
-require 'pry-debugger'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
