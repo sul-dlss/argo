0614d7854f7a895bd74ff74033df1a05497f004e
Johnathan Martin <john.martin@stanford.edu>
2015-03-16 17:44:15 -0700

    ARGO-367: get rid of deprecated (and effectively unused) attr_accessible method call

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,5 +1,7 @@
 class Robot < ActiveRecord::Base
-  attr_accessible :wf, :process
+  #attr_accessible is deprecated after Rails 3.  also: the only thing that sets these fields 
+  #is a migration, and no other models in argo use this mass assignment prevention approach.
+  # attr_accessible :wf, :process
 
   def status
     ready_docs=Dor::SearchService.query("wf_wps_facet:\"#{wf}:#{process}:ready\"")['response']['docs']
