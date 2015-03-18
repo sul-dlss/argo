1f00c08dc2b6557afd802c9c6c03581d6b6a8174
Johnathan Martin <john.martin@stanford.edu>
2015-03-18 16:57:48 -0700

    ARGO-367: getting rid of this since it's no longer needed

--- config/initializers/time.rb
+++
@@ -1,9 +0,0 @@
-#TODO: get rid of this whole file if it turns out this is as useless/counterproductive as suspected
-# require 'moab'
-# require 'time'
-# class Time
-#   # @return [String] The datetime in ISO 8601 format
-#   def to_s arg=nil
-#     self.utc.iso8601
-#   end
-# end
