426a9b70f13a10fbbcde87a347dbef6e54e728f7
Johnathan Martin <john.martin@stanford.edu>
2015-03-27 20:27:19 -0700

    ARGO-367: solr field name translation

--- app/helpers/robot_helper.rb
+++ app/helpers/robot_helper.rb
@@ -2,7 +2,7 @@ module RobotHelper
   def render_recent_work docs
     res=''
     docs.each do |doc|
-      res += render(:partial => 'item', :locals => {:title => doc['dc_title_t'].first, :druid => doc['id'], :apo_title => doc['apo_title_facet'] ? doc['apo_title_facet'] : "Hydrus Object"})
+      res += render(:partial => 'item', :locals => {:title => doc['dc_title_si'].first, :druid => doc['id'], :apo_title => doc['apo_title_ssm'] ? doc['apo_title_ssm'] : "Hydrus Object"})
     end
     res.html_safe
   end
