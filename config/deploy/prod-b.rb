2e2f1da56a676842275fa054f7a1a02d4f43d93f
Joe Atzberger <ohiocore@gmail.com>
2015-03-30 18:20:58 -0700

    Initial argo-prod-b deploy script

---
+++ config/deploy/prod-b.rb
@@ -0,0 +1,9 @@
+server 'argo-prod-b.stanford.edu', user: 'lyberadmin', roles: %w{web db app}
+
+## argo-prod-b will be used to generate a new (dev) Solr index from PRODUCTION fedora.  
+## Therefore it is not for running tests or poking around the interface!
+
+Capistrano::OneTimeKey.generate_one_time_key!
+set :rails_env, 'production'
+set :branch, :develop
+
