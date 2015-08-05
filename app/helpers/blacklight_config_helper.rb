ab245b8645b95e3eeda6903fa2f1eee2704babca
Johnathan Martin <john.martin@stanford.edu>
2015-08-04 18:35:29 -0700

    ARGO-367:  add facet for last accessioned.  update submitted and published facets to use latest versions of those dates.  slight reordering of date facets, facet field names and labels updated for clarity.

--- app/helpers/blacklight_config_helper.rb
+++ app/helpers/blacklight_config_helper.rb
@@ -10,29 +10,33 @@ module BlacklightConfigHelper
       :days_7  => { :label => 'within 7 days',  :fq => "registered_dttsim:[NOW/DAY-7DAYS TO *]"},
       :days_30 => { :label => 'within 30 days', :fq => "registered_dttsim:[NOW/DAY-30DAYS TO *]"}
     }
-    config.add_facet_field 'submitted_date', :label => 'Submitted', :query => {
-      :days_7  => { :label => 'within 7 days',  :fq => "submitted_dttsim:[NOW/DAY-7DAYS TO *]"},
-      :days_30 => { :label => 'within 30 days', :fq => "submitted_dttsim:[NOW/DAY-30DAYS TO *]"}
+    config.add_facet_field 'accessioned_latest_date', :label => 'Last Accessioned', :query => {
+      :days_7  => { :label => 'within 7 days',  :fq => "accessioned_latest_dttsi:[NOW/DAY-7DAYS TO *]"},
+      :days_30 => { :label => 'within 30 days', :fq => "accessioned_latest_dttsi:[NOW/DAY-30DAYS TO *]"}
     }
-    config.add_facet_field 'published_date', :label => 'Published', :query => {
-      :days_7  => { :label => 'within 7 days',  :fq => "published_dttsim:[NOW/DAY-7DAYS TO *]"},
-      :days_30 => { :label => 'within 30 days', :fq => "published_dttsim:[NOW/DAY-30DAYS TO *]"}
+    config.add_facet_field 'published_latest_date', :label => 'Last Published', :query => {
+      :days_7  => { :label => 'within 7 days',  :fq => "published_latest_dttsi:[NOW/DAY-7DAYS TO *]"},
+      :days_30 => { :label => 'within 30 days', :fq => "published_latest_dttsi:[NOW/DAY-30DAYS TO *]"}
+    }
+    config.add_facet_field 'submitted_latest_date', :label => 'Last Submitted', :query => {
+      :days_7  => { :label => 'within 7 days',  :fq => "submitted_latest_dttsi:[NOW/DAY-7DAYS TO *]"},
+      :days_30 => { :label => 'within 30 days', :fq => "submitted_latest_dttsi:[NOW/DAY-30DAYS TO *]"}
     }
     config.add_facet_field 'deposited_date', :label => 'Deposited', :query => {
       :days_1  => { :label => 'today',          :fq => "deposited_dttsim:[NOW/DAY TO *]"},
       :days_7  => { :label => 'within 7 days',  :fq => "deposited_dttsim:[NOW/DAY-7DAYS TO *]"},
       :days_30 => { :label => 'within 30 days', :fq => "deposited_dttsim:[NOW/DAY-30DAYS TO *]"}
     }
-    config.add_facet_field 'object_modified_day', :label => 'Object Last Modified', :query => {
+    config.add_facet_field 'object_modified_date', :label => 'Object Last Modified', :query => {
       :days_7  => { :label => 'within 7 days',  :fq => "modified_latest_dttsi:[NOW/DAY-7DAYS TO *]"},
       :days_30 => { :label => 'within 30 days', :fq => "modified_latest_dttsi:[NOW/DAY-30DAYS TO *]"}
     }
-    config.add_facet_field 'version_opened', :label => 'Open Version', :query => {
+    config.add_facet_field 'version_opened_date', :label => 'Open Version', :query => {
       :all     => { :label => 'All',               :fq => "opened_latest_dttsi:*"},
       :days_7  => { :label => 'more than 7 days',  :fq => "opened_latest_dttsi:[* TO NOW/DAY-7DAYS]"},
       :days_30 => { :label => 'more than 30 days', :fq => "opened_latest_dttsi:[* TO NOW/DAY-30DAYS]"}
     }
-    config.add_facet_field 'embargo_release', :label => 'Embargo Release Date', :query => {
+    config.add_facet_field 'embargo_release_date', :label => 'Embargo Release Date', :query => {
       :days_7  => { :label => 'up to 7 days',  :fq => "embargo_release_dtsim:[NOW TO NOW/DAY+7DAYS]"},
       :days_30 => { :label => 'up to 30 days', :fq => "embargo_release_dtsim:[NOW TO NOW/DAY+30DAYS]"},
       :all     => { :label => 'All',           :fq => "embargo_release_dtsim:*"}
