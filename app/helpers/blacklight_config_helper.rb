b89870bd672be8ca0d82d27a18a05ce5588bedb5
Johnathan Martin <john.martin@stanford.edu>
2015-08-15 03:40:06 -0700

    ARGO-367: centralized config method for setting up default_solr_params
    also, rename add_common_date_facet_fields_to_config to indicate it has side effects

--- app/helpers/blacklight_config_helper.rb
+++ app/helpers/blacklight_config_helper.rb
@@ -1,5 +1,5 @@
 module BlacklightConfigHelper
-  def self.add_common_date_facet_fields_to_config config
+  def self.add_common_date_facet_fields_to_config! config
     # Be careful using NOW: http://lucidworks.com/blog/date-math-now-and-filter-queries/
     # tl;dr: specify coarsest granularity (/DAY or /HOUR) or lose caching
     #
@@ -40,6 +40,21 @@ module BlacklightConfigHelper
       :days_7  => { :label => 'up to 7 days',  :fq => "embargo_release_dtsim:[NOW TO NOW/DAY+7DAYS]"},
       :days_30 => { :label => 'up to 30 days', :fq => "embargo_release_dtsim:[NOW TO NOW/DAY+30DAYS]"},
       :all     => { :label => 'All',           :fq => "embargo_release_dtsim:*"}
+    }
+  end
+
+  def self.add_common_default_solr_params_to_config! config
+    config.default_solr_params = {
+      :'q.alt' => "*:*",
+      :defType => 'dismax',
+      :qf => %{text^3 creator_tesim dc_creator_tesim dc_identifier_druid_tesim dc_title_tesim dor_id_tesim extent_ssim identifier_tesim lifecycle_ssim obj_label_tesim obj_state_tesim originInfo_place_placeTerm_tesim originInfo_publisher_tesim public_dc_contributor_tesim public_dc_coverage_tesim public_dc_creator_tesim public_dc_date_tesim public_dc_description_tesim public_dc_format_tesim public_dc_identifier_tesim public_dc_language_tesim public_dc_publisher_tesim public_dc_relation_tesim public_dc_rights_tesim public_dc_subject_tesim public_dc_title_tesim public_dc_type_tesim scale_ssim source_id_ssim tag_ssim title_tesim topic_tesim},
+      :facet => true,
+      :'facet.mincount' => 1,
+      :'f.wf_wps_ssim.facet.limit' => -1,
+      :'f.wf_wsp_ssim.facet.limit' => -1,
+      :'f.wf_swp_ssim.facet.limit' => -1,
+      :'f.tag_ssim.facet.limit' => -1,
+      :'f.tag_ssim.facet.sort' => 'index'
     }
   end
 end
