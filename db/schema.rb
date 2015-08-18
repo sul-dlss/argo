933e70a16b10032be732a7ba184ae24c0377e7be
Johnathan Martin <john.martin@stanford.edu>
2015-08-18 12:36:47 -0700

    ran rake db:drop, rake db:create, rake db:migrate to deal w/ a local laptop issue (mysql not using UTF-8).  got some new limits on field sizes, checking in since this is probably how schema.db should look

--- db/schema.rb
+++ db/schema.rb
@@ -14,63 +14,63 @@
 ActiveRecord::Schema.define(version: 20150619200031) do
 
   create_table "bookmarks", force: :cascade do |t|
-    t.integer  "user_id",       null: false
-    t.string   "document_id"
-    t.string   "title"
+    t.integer  "user_id",       limit: 4,   null: false
+    t.string   "document_id",   limit: 255
+    t.string   "title",         limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "user_type"
-    t.string   "document_type"
+    t.string   "user_type",     limit: 255
+    t.string   "document_type", limit: 255
   end
 
-  add_index "bookmarks", ["user_id"], name: "index_bookmarks_on_user_id"
+  add_index "bookmarks", ["user_id"], name: "index_bookmarks_on_user_id", using: :btree
 
   create_table "delayed_jobs", force: :cascade do |t|
-    t.integer  "priority",   default: 0, null: false
-    t.integer  "attempts",   default: 0, null: false
-    t.text     "handler",                null: false
-    t.text     "last_error"
+    t.integer  "priority",   limit: 4,     default: 0, null: false
+    t.integer  "attempts",   limit: 4,     default: 0, null: false
+    t.text     "handler",    limit: 65535,             null: false
+    t.text     "last_error", limit: 65535
     t.datetime "run_at"
     t.datetime "locked_at"
     t.datetime "failed_at"
-    t.string   "locked_by"
-    t.string   "queue"
+    t.string   "locked_by",  limit: 255
+    t.string   "queue",      limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
   create_table "indexing_exceptions", force: :cascade do |t|
-    t.string   "pid"
-    t.text     "solr_document"
-    t.string   "dor_services_version"
-    t.text     "exception"
+    t.string   "pid",                  limit: 255
+    t.text     "solr_document",        limit: 65535
+    t.string   "dor_services_version", limit: 255
+    t.text     "exception",            limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "indexing_exceptions", ["pid"], name: "index_indexing_exceptions_on_pid"
+  add_index "indexing_exceptions", ["pid"], name: "index_indexing_exceptions_on_pid", using: :btree
 
   create_table "robots", force: :cascade do |t|
-    t.string   "wf"
-    t.string   "process"
+    t.string   "wf",         limit: 255
+    t.string   "process",    limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "searches", force: :cascade do |t|
-    t.text     "query_params"
-    t.integer  "user_id"
+    t.text     "query_params", limit: 65535
+    t.integer  "user_id",      limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "user_type"
+    t.string   "user_type",    limit: 255
   end
 
-  add_index "searches", ["user_id"], name: "index_searches_on_user_id"
+  add_index "searches", ["user_id"], name: "index_searches_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "sunetid"
+    t.string   "sunetid",    limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
