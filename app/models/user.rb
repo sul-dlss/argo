9cef17b670310b9f70ce183e0a41c1beb3ddec57
Joe Atzberger <ohiocore@gmail.com>
2015-11-13 15:10:42 -0800

    Cleanup and documentation of User
    Remove extraneous belongs_to_listed_group method.

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,43 +15,45 @@ class User < ActiveRecord::Base
   end
 
   def to_s
-    if webauth
-      webauth.attributes['DISPLAYNAME'] || webauth.login
-    else
-      sunetid
-    end
+    return sunetid unless webauth
+    webauth.attributes['DISPLAYNAME'] || webauth.login
   end
 
   @role_cache = {}
+
+  # Queries Solr for a given record, returning synthesized role strings that are defined. Searches:
+  # (1) for User by sunet id
+  # (2) groups actually contains the sunetid, so it is just looking at different solr fields
+  # NOTE: includes legacy roles that have to be translated
+  # @param [String] DRUID, fully qualified
+  # @return [Array[String]] list of roles
   def roles(pid)
     @role_cache ||= {}
     return @role_cache[pid] if @role_cache[pid]
 
     resp = Dor::SearchService.query('id:"' + pid + '"')['response']['docs'].first || {}
     toret = []
-    # search for group based roles
-    # (1) for User by sunet id
-    # (2) groups actually contains the sunetid, so it is just looking at different solr fields
-    # includes legacy roles that have to be translated
+    my_groups = groups
     %w(apo_role_group_manager_ssim apo_role_person_manager_ssim).each do |key|
-      toret << 'dor-apo-manager' if resp[key] && (resp[key] & groups).length > 0
+      toret << 'dor-apo-manager' if resp[key] && (resp[key] & my_groups).length > 0
     end
 
     known_roles.each do |role|
       ["apo_role_#{role}_ssim", "apo_role_person_#{role}_ssim"].each do |key|
-        toret << role if resp[key] && (resp[key] & groups).length > 0
+        toret << role if resp[key] && (resp[key] & my_groups).length > 0
       end
     end
-    # store this for now, there may be several role related calls
-    @role_cache[pid] = toret
+    @role_cache[pid] = toret # store this for now, there may be several role related calls
     toret
   end
 
-  # array of apos the user is allowed to view
+  # @return [Array<String>] list of apos the user is allowed to view
   def known_roles
     ['dor-administrator', 'sdr-administrator', 'dor-viewer', 'sdr-viewer', 'dor-apo-creator', 'dor-apo-manager', 'dor-apo-depositor', 'dor-apo-reviewer', 'dor-apo-metadata', 'dor-apo-viewer']
   end
 
+  # Also queries Solr based on groups
+  # @return [Array[String]] list of DRUIDs from APOs that this User can view
   def permitted_apos
     query = groups.map{|g| g.gsub(':', '\:')}.join(' OR ')
     q = 'apo_role_group_manager_ssim:(' + query + ') OR apo_role_person_manager_ssim:(' + query + ')'
@@ -63,6 +65,9 @@ class User < ActiveRecord::Base
     resp.map{|doc| doc['id']}
   end
 
+  # Queries Solr yet again! But in a different way! Doesn't use filter query.
+  # FIXME: seems to include display logic
+  # @return [Array<Array<String>>] Sorted array of pairs of strings, each pair like: ["Title (PID)", "PID"]
   def permitted_collections
     q = 'objectType_ssim:collection AND !project_tag_ssim:"Hydrus" '
     q += permitted_apos.map {|pid| 'is_governed_by_ssim:"info:fedora/' + pid + '"'}.join(' OR ') unless is_admin
@@ -72,51 +77,39 @@ class User < ActiveRecord::Base
     result.sort! do |a, b|
       a['dc_title_tesim'].to_s <=> b['dc_title_tesim'].to_s
     end
-    # puts 'qry '+result.first['dc_title_tesim'].encoding.inspect
-    res = [['None', '']]
-    res += result.collect do |doc|
+
+    [['None', '']] + result.collect do |doc|
       [Array(doc['dc_title_tesim']).first + ' (' + doc['id'].to_s + ')', doc['id'].to_s]
     end
-    res
   end
 
   @groups_to_impersonate = nil
-  # create a set of groups in a cookie store to allow a repository admin to see the repository as if they had a different set of permissions
+  # Allow a repository admin to see the repository as if they had a different set of permissions
+  # @param [Array<String>] set of groups
   def set_groups_to_impersonate(grps)
     @groups_to_impersonate = grps
   end
 
   def groups
     return @groups_to_impersonate if @groups_to_impersonate
-
     perm_keys = ["sunetid:#{login}"]
-    if webauth && webauth.privgroup.present?
-      perm_keys += webauth.privgroup.split(/\|/).collect { |g| "workgroup:#{g}" }
-    end
-
-    perm_keys
+    return perm_keys unless webauth && webauth.privgroup.present?
+    perm_keys + webauth.privgroup.split(/\|/).collect { |g| "workgroup:#{g}" }
   end
 
-  def belongs_to_listed_group?(group_list)
-    group_list.each do |group|
-      return true if groups.include? group
-    end
-    false
-  end
-
-  # is the user a repository wide administrator
+  # @return [Boolean] is the user a repository wide administrator
   def is_admin
-    belongs_to_listed_group? ADMIN_GROUPS
+    !(groups & ADMIN_GROUPS).empty?
   end
 
-  # is the user a repository wide viewer
+  # @return [Boolean] is the user a repository wide viewer
   def is_viewer
-    belongs_to_listed_group? VIEWER_GROUPS
+    !(groups & VIEWER_GROUPS).empty?
   end
 
-  # is the user a repo wide manager
+  # @return [Boolean] is the user a repo wide manager
   def is_manager
-    belongs_to_listed_group? MANAGER_GROUPS
+    !(groups & MANAGER_GROUPS).empty?
   end
 
   # The convention is for boolean is_XYZ methods to be interrogative (end in "?")
@@ -125,10 +118,6 @@ class User < ActiveRecord::Base
   alias_method :is_manager?, :is_manager
 
   def login
-    if webauth
-      webauth.login
-    else
-      sunetid
-    end
+    webauth ? webauth.login : sunetid
   end
 end
