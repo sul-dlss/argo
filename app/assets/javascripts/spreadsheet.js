009aadfee07999de8c133a00254d534b2aa0d372
Tommy Ingulfsen <tommyi@stanford.edu>
2015-11-16 14:55:20 -0800

    Rails back to 4.2.4 that we were using before, and adding camel case for JavaScript variables/functions.

--- app/assets/javascripts/spreadsheet.js
+++ app/assets/javascripts/spreadsheet.js
@@ -2,48 +2,47 @@
 
 // An object to prevent us from polluting the global namespace with all these generically named functions.
 var argoSpreadsheet = {
-    
-    enable_submit : function()
+    enableSubmit : function()
     {
-	$("#spreadsheet_submit").prop("disabled", false);
+	$('#spreadsheet_submit').prop('disabled', false);
     },
     
-    disable_control : function(ctrl)
+    disableControl : function(ctrl)
     {
-	ctrl.prop("disabled", true);
+	ctrl.prop('disabled', true);
     },
     
-    enable_control : function(ctrl)
+    enableControl : function(ctrl)
     {
-	ctrl.prop("disabled", false);
+	ctrl.prop('disabled', false);
     },
 
-    load_upload_form : function()
+    loadUploadForm : function()
     {
-	// When the user clicks the "MODS bulk loads" button, a lightbox is opened. The event
-	// "loaded.blacklight.ajax-modal" is fired just before this Blacklight lightbox is shown.
-	var input_controls = [$("#filetypes_1"), $("#filetypes_2"), $("#convert_only"), $("#note_text")];
-	var radio_buttons = input_controls.slice(0, 3);
+	// When the user clicks the 'MODS bulk loads' button, a lightbox is opened. The event
+	// 'loaded.blacklight.ajax-modal' is fired just before this Blacklight lightbox is shown.
+	var inputControls = [$('#filetypes_1'), $('#filetypes_2'), $('#convert_only'), $('#note_text')];
+	var radioButtons = inputControls.slice(0, 3);
 	
 	// Adjust the width of the lightbox when it hosts a metadata spreadsheet upload form.
-	$("#spreadsheet-upload-container").parent().parent().css("width", "650px");
+	$('#spreadsheet-upload-container').parent().parent().css('width', '650px');
 	
 	// Width of the lightbox when hosting a metadata bulk upload log
-	$("#spreadsheet-log-container").parent().parent().css("width", "700");
+	$('#spreadsheet-log-container').parent().parent().css('width', '700');
 	
 	// None of the form controls should be functional until a file has been selected
-	$("#spreadsheet_submit").prop("disabled", true);
-	input_controls.map(argoSpreadsheet.disable_control);
+	$('#spreadsheet_submit').prop('disabled', true);
+	inputControls.map(argoSpreadsheet.disableControl);
 	
 	// Enable everything except for the submit button upon file upload
-	$("#spreadsheet_file").change(function (){
-	    input_controls.map(argoSpreadsheet.enable_control);
+	$('#spreadsheet_file').change(function (){
+	    inputControls.map(argoSpreadsheet.enableControl);
 	});
 	
 	// Only when the user has uploaded a file AND selected one of the radio buttons should it be possible to submit
-	radio_buttons.forEach(function(button) {
+	radioButtons.forEach(function(button) {
 	    button.click(function() {
-		argoSpreadsheet.enable_submit();
+		argoSpreadsheet.enableSubmit();
 	    });
 	});
     },
@@ -52,34 +51,38 @@ var argoSpreadsheet = {
 
 // The Blacklight onLoad event works better than the regular onLoad event if turbolinks is enabled.
 Blacklight.onLoad(function(){
-    // When the user clicks the "MODS bulk loads" button, a lightbox is opened. The event
-    // "loaded.blacklight.ajax-modal" is fired just before this Blacklight lightbox is shown.
-    $("#ajax-modal").on("loaded.blacklight.ajax-modal", function(e){
-	argoSpreadsheet.load_upload_form();
+    'use strict';
+
+    // When the user clicks the 'MODS bulk loads' button, a lightbox is opened. The event
+    // 'loaded.blacklight.ajax-modal' is fired just before this Blacklight lightbox is shown.
+    $('#ajax-modal').on('loaded.blacklight.ajax-modal', function(e){
+	argoSpreadsheet.loadUploadForm();
     });
 });
 
 
 // Confirmation modal dialog for when the user presses the delete button in the spreadsheet bulk upload table.
 $(document).ready(function() {
+    'use strict';
+
     // The form we want to submit has both ':' and '/' in its ID, which need to be escaped
-    function escape_characters(identifier) {
-	return "#" + identifier.replace( /(:|\.|\[|\]|,|\/)/g, "\\$1" );
+    function escapeCharacters(identifier) {
+	return '#' + identifier.replace( /(:|\.|\[|\]|,|\/)/g, '\\$1' );
     }
 
     
-    $(".job-delete-button").click(function() {
-	var form_parent_id = $(this).parent().attr('id');
+    $('.job-delete-button').click(function() {
+	var formParentId = $(this).parent().attr('id');
 	
 	$('#confirm-delete-job').click(function(){
 	    
 	    // Submit the form when the Delete button in the modal is clicked
-	    $(escape_characters(form_parent_id)).submit();
+	    $(escapeCharacters(formParentId)).submit();
 	});
     });
 
     // The bulk upload form can also be reached directly without opening a modal
-    if($("#spreadsheet-upload-container").length) {
-	argoSpreadsheet.load_upload_form();
+    if($('#spreadsheet-upload-container').length) {
+	argoSpreadsheet.loadUploadForm();
     }
 });
