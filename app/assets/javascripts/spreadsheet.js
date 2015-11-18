7f021aae7f9492b6f0af8365188553979a7f973a
Tommy Ingulfsen <tommyi@stanford.edu>
2015-11-18 11:38:04 -0800

    Fix JS to make Hound less unhappy.

--- app/assets/javascripts/spreadsheet.js
+++ app/assets/javascripts/spreadsheet.js
@@ -1,60 +1,66 @@
-// This file contains JavaScript specific functionality for the bulk loading of spreadsheets.
+'use strict';
 
-console.log('loading ssheet file');
+(function ($) {
+    // jQuery plugin with utility functions for spreadsheet bulk upload
+    // submission
 
-(function($) {
-    /*
-      jQuery plugin with utility functions for spreadsheet bulk upload submission
-    */
-
-    $.fn.argoSpreadsheet = function() {
-	// var enableSubmit = function() {
-	//     $('#spreadsheet_submit').prop('disabled', false);
-	// };
+    $.fn.argoSpreadsheet = function () {
+        var $el;             // Use dollar sign for JQuery objects
+        var $submitButton;
 	
-	// var disableControl = function(ctrl) {
-	//     ctrl.prop('disabled', true);
-	// };
+	var enableSubmit = function() {
+	    $submitButton.prop('disabled', false);
+	};
 	
-	// var enableControl = function(ctrl) {
-	//     ctrl.prop('disabled', false);
-	// };
+	var disableControl = function(ctrl) {
+	    ctrl.prop('disabled', true);
+	};
+	
+	var enableControl = function(ctrl) {
+	    ctrl.prop('disabled', false);
+	};
 
 	return this.each(function() {
-	    console.log('start of call');
+	    $el = $(this);     // $el is now spreadsheet-upload-container
+	    $submitButton = $el.find('#spreadsheet_submit');
 	    
-	    // When the user clicks the 'MODS bulk loads' button, a lightbox is opened. The event
-	    // 'loaded.blacklight.ajax-modal' is fired just before this Blacklight lightbox is shown.
-	    var inputControls = [$('#filetypes_1'), $('#filetypes_2'), $('#convert_only'), $('#note_text')];
+	    // When the user clicks the 'MODS bulk loads' button, a lightbox is
+	    // opened. The event 'loaded.blacklight.ajax-modal' is fired just
+	    // before this Blacklight lightbox is shown.
+	    var inputControls = [$('#filetypes_1'),
+				 $('#filetypes_2'),
+				 $('#convert_only'),
+				 $('#note_text')];
 	    var radioButtons = inputControls.slice(0, 3);
 	    
-	    // Adjust the width of the lightbox when it hosts a metadata spreadsheet upload form.
-	    $('#spreadsheet-upload-container').parent().parent().css('width', '650px');
+	    // Adjust the width of the lightbox when it hosts a metadata
+	    // spreadsheet upload form.
+	    $el.parent().parent().css('width', '650px');
 	    
 	    // Width of the lightbox when hosting a metadata bulk upload log
-	    $('#spreadsheet-log-container').parent().parent().css('width', '700');
+	    $el.parent().parent().css('width', '700');
 	    
-	    // None of the form controls should be functional until a file has been selected
-	    $('#spreadsheet_submit').prop('disabled', true);
-	    inputControls.map(argoSpreadsheet.disableControl);
+	    // None of the form controls should be functional until a file has been
+	    // selected
+	    $submitButton.prop('disabled', true);
+	    inputControls.map(disableControl);
 	    
 	    // Enable everything except for the submit button upon file upload
-	    $('#spreadsheet_file').change(function (){
-		inputControls.map(argoSpreadsheet.enableControl);
+	    $el.find('#spreadsheet_file').change(function (){
+		inputControls.map(enableControl);
 	    });
 	    
-	    // Only when the user has uploaded a file AND selected one of the radio buttons should it be possible to submit
+	    // Only when the user has uploaded a file AND selected one of the
+	    // radio buttons should it be possible to submit
 	    radioButtons.forEach(function(button) {
 		button.click(function() {
-		    argoSpreadsheet.enableSubmit();
+		    enableSubmit();
 		});
 	    });
 	});
     };
 })(jQuery);	
 	
-
-
 
 // The Blacklight onLoad event works better than the regular onLoad event if turbolinks is enabled.
 Blacklight.onLoad(function(){
@@ -65,11 +71,18 @@ Blacklight.onLoad(function(){
     // 'loaded.blacklight.ajax-modal' is fired just before this Blacklight lightbox is shown.
     //$('#ajax-modal').on('loaded.blacklight.ajax-modal', function(e){
     $('#spreadsheet-upload-container').argoSpreadsheet();
-    //});
+    
+    // When the user clicks the 'MODS bulk loads' button, a lightbox is opened.
+    // The event 'loaded.blacklight.ajax-modal' is fired just before this
+    // Blacklight lightbox is shown.
+    $('#ajax-modal').on('loaded.blacklight.ajax-modal', function(e){
+	$('#spreadsheet-upload-container').argoSpreadsheet();
+    });
 });
 
 
-// Confirmation modal dialog for when the user presses the delete button in the spreadsheet bulk upload table.
+// Confirmation modal dialog for when the user presses the delete button in the
+// spreadsheet bulk upload table.
 $(document).ready(function() {
     console.log('doc ready executing');
 
@@ -78,7 +91,6 @@ $(document).ready(function() {
 	return '#' + identifier.replace( /(:|\.|\[|\]|,|\/)/g, '\\$1' );
     }
 
-    
     $('.job-delete-button').click(function() {
 	var formParentId = $(this).parent().attr('id');
 	
