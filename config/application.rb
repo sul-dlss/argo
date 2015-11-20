685d039bc42dfcc0d7130194b2f462f350aad822
Jack Reed <pjreed@stanford.edu>
2015-11-20 10:52:32 -0500

    Set ActiveJobs to use delayed_jobs, needed for Rails 4.2 https://github.com/collectiveidea/delayed_job#rails-42

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module Argo
     # -- all .rb files in that directory are automatically loaded.
     require 'indexer'
 
+    config.active_job.queue_adapter = :delayed_job
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
