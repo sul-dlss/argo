4a0d517cf53af479e44574c161532ce25bd5e548
Jack Reed <pjreed@stanford.edu>
2015-11-24 17:40:34 -0500

    Add ViewSwitcher to be used in multiple views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,4 +20,47 @@ module ApplicationHelper
   def robot_status_url
     Argo::Config.urls.robot_status
   end
+
+  ##
+  # Views used in report view toggle
+  # @return [Array<ViewSwitcher>]
+  def views_to_switch
+    [
+      ViewSwitcher.new(:bulk_update, :report_bulk_url),
+      ViewSwitcher.new(:catalog, :catalog_index_url),
+      ViewSwitcher.new(:report, :report_url),
+      ViewSwitcher.new(:discovery, :discovery_url),
+      ViewSwitcher.new(:workflow_grid, :report_workflow_grid_url)
+    ]
+  end
+
+  ##
+  # @return [Boolean]
+  def bulk_update_view?(params)
+    params['controller'] == 'report' && params['action'] == 'bulk'
+  end
+
+  ##
+  # @return [Boolean]
+  def catalog_view?(params)
+    params['controller'] == 'catalog'
+  end
+
+  ##
+  # @return [Boolean]
+  def report_view?(params)
+    params['controller'] == 'report' && params['action'] == 'index'
+  end
+
+  ##
+  # @return [Boolean]
+  def discovery_view?(params)
+    params['controller'] == 'discovery'
+  end
+
+  ##
+  # @return [Boolean]
+  def workflow_grid_view?(params)
+    params['controller'] == 'report' && params['action'] == 'workflow_grid'
+  end
 end
