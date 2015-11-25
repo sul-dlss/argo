7cfa0a970435f5a279fe9bc533c7f94753e87a7e
Joe Atzberger <ohiocore@gmail.com>
2015-11-24 18:13:10 -0800

    Begin to break down RegistrationHelper PDF/reindex
    Maybe it is broken in this state, but who would know?  There's no test.
    Added a stup spec file.  Trying to separate data and presentation.

--- app/helpers/registration_helper.rb
+++ app/helpers/registration_helper.rb
@@ -4,13 +4,13 @@ module RegistrationHelper
   def apo_list(*permission_keys)
     q = 'objectType_ssim:adminPolicy AND !tag_ssim:"Project : Hydrus"'
     unless permission_keys.empty?
-      q += '(' + permission_keys.flatten.collect { |key| %(apo_register_permissions_ssim:"#{key}") }.join(' OR ') + ')'
+      q += '(' + permission_keys.flatten.map { |key| %(apo_register_permissions_ssim:"#{key}") }.join(' OR ') + ')'
     end
     result = Dor::SearchService.query(q, :rows => 99999, :fl => 'id,tag_ssim,dc_title_tesim').docs
     result.sort! do |a, b|
       Array(a['tag_ssim']).include?('AdminPolicy : default') ? -1 : a['dc_title_tesim'].to_s <=> b['dc_title_tesim'].to_s
     end
-    result.collect do |doc|
+    result.map do |doc|
       [Array(doc['dc_title_tesim']).first, doc['id'].to_s]
     end
   end
@@ -56,15 +56,28 @@ module RegistrationHelper
     ]
   end
 
+  # @param [String] druid unqualified DRUID identifier
+  # @return [Hash] doc from Solr or to_solr
+  # @note To the extent we use Solr input filters or copyField(s), the Solr version will differ from the to_solr hash.
+  # @note That difference shouldn't be important for the few known fields we use here.
+  def find_or_create_in_solr_by_id(druid)
+    doc = Dor::SearchService.query(%(id:"druid:#{druid}"), :rows => 1).docs.first
+    return doc unless doc.nil?
+    obj = Dor.load_instance "druid:#{druid}"
+    solr_doc = obj.to_solr
+    Dor::SearchService.solr.add(solr_doc)
+    # Dor::SearchService.solr.add(solr_doc, :add_attributes => {:commitWithin => 1000})
+    solr_doc
+  end
+
+  # @FIXME: This code needs to separate data and query logic from presentation.
+
+  # @param [Array<String>] druids unqualified DRUID identifiers
+  # @return [Prawn::Document] PDF document for the DRUIDs
   def generate_tracking_pdf(druids)
     # FIXME: why not search for all druids in one query?
-    druids.each do |druid|
-      doc = Dor::SearchService.query(%(id:"druid:#{druid}"), :rows => 1).docs.first
-      next unless doc.nil?
-      obj = Dor.load_instance 'druid:' + druid
-      solr_doc = obj.to_solr
-      Dor::SearchService.solr.add(solr_doc, :add_attributes => {:commitWithin => 1000}) unless obj.nil?
-    end
+    # FIXME: how about a MAX number of druids? And/Or a chunky use of enumeration?
+    druids.each { |druid| find_or_create_in_solr_by_id(druid) }
     pdf = Prawn::Document.new(:page_size => [5.5.in, 8.5.in])
     pdf.font('Courier')
     druids.each_with_index do |druid, i|
@@ -74,52 +87,39 @@ module RegistrationHelper
     pdf
   end
 
+  # Adds one DRUID page to the PDF document
+  # @param [String] druid unqualified DRUID identifier
+  # @param [Prawn::Document] pdf document being built (document is modified)
+  # @return [Prawn::Document] the same document
   def generate_tracking_sheet(druid, pdf)
     bc_width  = 2.25.in
     bc_height = 0.75.in
 
-    top_margin = (pdf.page.size[1] - pdf.bounds.absolute_top)
+    top_margin = pdf.page.size[1] - pdf.bounds.absolute_top
 
-    doc = Dor::SearchService.query(%(id:"druid:#{druid}"), :rows => 1).docs.first
-
-    if doc.nil?
-      begin
-        obj = Dor.load_instance "druid:#{druid}"
-        solr_doc = obj.to_solr
-        Dor::SearchService.solr.add(solr_doc, :add_attributes => {:commitWithin => 1000}) unless obj.nil?
-        doc = Dor::SearchService.query(%(id:"druid:#{druid}"), :rows => 1).docs.first
-      rescue
-        pdf.text "DRUID #{druid} not found in index", :size => 15, :style => :bold, :align => :center
-      end
-      return
+    doc = nil
+    begin
+      doc = find_or_create_in_solr_by_id(druid)
+    rescue
+      pdf.text "DRUID #{druid} not found in index", :size => 15, :style => :bold, :align => :center
+      return pdf
     end
 
     barcode = Barby::Code128B.new(druid)
-    barcode.annotate_pdf(pdf, :width => bc_width, :height => bc_height,
-    :x => ((pdf.bounds.width / 2) - (bc_width / 2)), :y => (pdf.bounds.height - bc_height))
+    barcode.annotate_pdf(
+      pdf,
+      :width  => bc_width,
+      :height => bc_height,
+      :x      => (pdf.bounds.width / 2) - (bc_width / 2),
+      :y      => (pdf.bounds.height - bc_height)
+    )
 
     pdf.y -= (bc_height + 0.25.in)
     pdf.text druid, :size => 15, :style => :bold, :align => :center
     pdf.y -= 0.5.in
 
     pdf.font('Courier', :size => 10)
-
-    table_data = []
-
-    labels = doc['obj_label_ssim']
-    label = (labels.nil? || labels.empty?) ? '' : labels.first
-    label = label[0..110] + '...' if label.length > 110
-    table_data.push(['Object Label:', label])
-    table_data.push(['Project Name:', doc['project_tag_ssim'].to_s]) if doc['project_tag_ssim']
-
-    tags = Array(doc['tag_ssim']).collect { |tag| tag =~ /^Project\s*:/ ? nil : tag.gsub(/\s+/,  Prawn::Text::NBSP) }.compact
-    table_data.push(['Tags:', tags.join("\n")]) if tags.length > 0
-    table_data.push(['Catkey:', Array(doc['catkey_id_ssim']).join(', ')]) if doc['catkey_id_ssim'].present?
-    table_data.push(['Source ID:', Array(doc['source_id_ssim']).first]) if doc['source_id_ssim'].present?
-    table_data.push(['Barcode:', Array(doc['barcode_id_ssim']).first]) if doc['barcode_id_ssim'].present?
-    table_data.push(['Date Printed:', Time.now.strftime('%c')])
-
-    pdf.table(table_data, :column_widths => [100, 224], :cell_style => { :borders => [], :padding => 0.pt })
+    pdf.table(doc_to_table(doc), :column_widths => [100, 224], :cell_style => { :borders => [], :padding => 0.pt })
 
     pdf.y -= 0.5.in
 
@@ -146,14 +146,26 @@ module RegistrationHelper
         pdf.y -= pdf.font.height * 1.5
       end
     end
+    pdf
   end
 
-  def extract_identifiers(xml)
-    xml.search('/mods:mods/mods:identifier | /msDesc/msIdentifier/*', { 'mods' => 'http://www.loc.gov/mods/v3' }).collect do |elem|
-      unless elem.text.empty?
-        ["#{(elem['displayLabel'] || elem.name).titleize}:", elem.text]
-      end
-    end.compact
+  # @param [Hash] doc Solr document or to_solr Hash
+  # @return [Array<Array<String>>] Complex array suitable for pdf.table()
+  def doc_to_table(doc)
+    table_data = []
+    labels = doc['obj_label_ssim']
+    label = (labels.nil? || labels.empty?) ? '' : labels.first
+    label = label[0..110] + '...' if label.length > 110
+    table_data.push(['Object Label:', label])
+    table_data.push(['Project Name:', doc['project_tag_ssim'].to_s]) if doc['project_tag_ssim']
+
+    tags = Array(doc['tag_ssim']).collect { |tag| tag =~ /^Project\s*:/ ? nil : tag.gsub(/\s+/, Prawn::Text::NBSP) }.compact
+    table_data.push(['Tags:', tags.join("\n")]) if tags.length > 0
+    table_data.push(['Catkey:',    Array(doc['catkey_id_ssim']).join(', ')]) if doc['catkey_id_ssim'].present?
+    table_data.push(['Source ID:', Array(doc['source_id_ssim']).first     ]) if doc['source_id_ssim'].present?
+    table_data.push(['Barcode:',   Array(doc['barcode_id_ssim']).first    ]) if doc['barcode_id_ssim'].present?
+    table_data.push(['Date Printed:', Time.now.strftime('%c')])
+    table_data
   end
 
 end
---
+++ spec/helpers/registration_helper_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe RegistrationHelper do
+  it 'does something' do
+    
+  end
+end
