76fea20fce5acedad0f937e82a2a88a3bb5ec740
Darren Hardy <drh@stanford.edu>
2015-12-01 11:51:37 -0800

    fix Nil exception with @object, not @obj

---
+++ spec/views/items/workflow_view.html.erb_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe 'items/workflow_view.html.erb' do
+  it 'should render' do
+    assign(:object, double('object', pid: 'druid:aa111bb2222'))
+    assign(:workflow_id, 'accessionWF')
+    render
+    expect(rendered).to have_css 'table.detail'
+  end
+end
