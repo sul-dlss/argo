7b9d669a5c56161b3098f1f191c99fbfb6a6196b
Jack Reed <pjreed@stanford.edu>
2015-12-02 06:24:43 -0800

    Move Blacklight date configs to SolrDocument concern pattern. Useful for defining used Solr fields in one place only. Also adds hidden facet fields for labeling direct date field queries (not using predefined queries)

---
+++ spec/models/concerns/document_date_concern_spec.rb
@@ -0,0 +1,70 @@
+require 'spec_helper'
+
+RSpec.describe DocumentDateConcern do
+  let(:document) { SolrDocument.new(document_attributes) }
+  let(:single_date) { ['2012-04-05T01:00:04.148Z'] }
+  describe '#registered_date' do
+    let(:document_attributes) do
+      { SolrDocument::FIELD_REGISTERED_DATE => single_date }
+    end
+    it 'returns date' do
+      expect(document.registered_date).to match_array(single_date)
+    end
+  end
+  describe '#accessioned_date' do
+    let(:document_attributes) do
+      { SolrDocument::FIELD_LAST_ACCESSIONED_DATE => single_date }
+    end
+    it 'returns date' do
+      expect(document.accessioned_date).to match_array(single_date)
+    end
+  end
+  describe '#published_date' do
+    let(:document_attributes) do
+      { SolrDocument::FIELD_LAST_PUBLISHED_DATE => single_date }
+    end
+    it 'returns date' do
+      expect(document.published_date).to match_array(single_date)
+    end
+  end
+  describe '#submitted_date' do
+    let(:document_attributes) do
+      { SolrDocument::FIELD_LAST_SUBMITTED_DATE => single_date }
+    end
+    it 'returns date' do
+      expect(document.submitted_date).to match_array(single_date)
+    end
+  end
+  describe '#deposited_date' do
+    let(:document_attributes) do
+      { SolrDocument::FIELD_LAST_DEPOSITED_DATE => single_date }
+    end
+    it 'returns date' do
+      expect(document.deposited_date).to match_array(single_date)
+    end
+  end
+  describe '#modified_date' do
+    let(:document_attributes) do
+      { SolrDocument::FIELD_LAST_MODIFIED_DATE => single_date }
+    end
+    it 'returns date' do
+      expect(document.modified_date).to match_array(single_date)
+    end
+  end
+  describe '#opened_date' do
+    let(:document_attributes) do
+      { SolrDocument::FIELD_LAST_OPENED_DATE => single_date }
+    end
+    it 'returns date' do
+      expect(document.opened_date).to match_array(single_date)
+    end
+  end
+  describe '#embargo_release_date' do
+    let(:document_attributes) do
+      { SolrDocument::FIELD_EMBARGO_RELEASE_DATE => single_date }
+    end
+    it 'returns date' do
+      expect(document.embargo_release_date).to match_array(single_date)
+    end
+  end
+end
