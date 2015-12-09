3e378980f8f279f4c269c1481fbb30684685d13d
Jack Reed <pjreed@stanford.edu>
2015-12-08 20:48:17 -0800

    rip out old non used auth stuff, there is no webauth login route so we should just return a 401

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,17 +1,4 @@
 class AuthController < ApplicationController
-
-  def login
-    session[:webauth_env] = Hash[webauth.env.select { |k, v| k.to_s =~ /^WEBAUTH_/ }].to_json
-    session[:privgroup_hash] = htaccess_hash
-    redirect_to params[:return] || root_path
-  end
-
-  def logout
-    session.delete(:webauth_env)
-    session.delete(:privgroup_hash)
-    redirect_to root_path
-  end
-
   def remember_impersonated_groups
     groups = params[:groups].split(',')
     session[:groups] = groups
@@ -22,5 +9,4 @@ class AuthController < ApplicationController
     session[:groups] = nil
     redirect_to root_path
   end
-
 end
