180b6672d4f028da7aa796eba6817b9b08ca1f66
Jack Reed <pjreed@stanford.edu>
2015-12-11 11:09:44 -0800

    enable squash revision

--- config/initializers/squash_exceptions.rb
+++ config/initializers/squash_exceptions.rb
@@ -4,4 +4,5 @@
 Squash::Ruby.configure api_host: Settings.SQUASH.API_HOST,
                        api_key: Settings.SQUASH.API_KEY,
                        environment: Settings.SQUASH_ENVIRONMENT || Rails.env,
-                       disabled: Settings.SQUASH.DISABLE
+                       disabled: Settings.SQUASH.DISABLE,
+                       revision_file: File.join(Rails.root, 'REVISION')
