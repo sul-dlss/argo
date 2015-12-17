6955de8b9a6c7fd047bc8694956cc09b899ee8f1
Jack Reed <pjreed@stanford.edu>
2015-12-17 15:01:54 -0500

    remove defunct "Robot" resource

--- app/controllers/robot_controller.rb
+++
@@ -1,9 +0,0 @@
-class RobotController < ApplicationController
-  def index
-    @robots = Robot.all
-  end
-
-  def show
-    @robot = Robot.find_by_id(params[:id])
-  end
-end
--- db/migrate/20130709234901_create_robots.rb
+++
@@ -1,9 +0,0 @@
-class CreateRobots < ActiveRecord::Migration
-  def change
-    create_table :robots do |t|
-      t.string :wf
-      t.string :process
-      t.timestamps
-    end
-  end
-end
