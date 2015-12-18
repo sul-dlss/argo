74e42210c7c0c18782caa453ca906171c005560e
Tommy Ingulfsen <tommyi@stanford.edu>
2015-12-18 13:58:52 -0800

    Adding a few notes to self.

--- app/models/bulk_descmetadata_download.rb
+++ app/models/bulk_descmetadata_download.rb
@@ -1,3 +1,6 @@
 class BulkDescmetadataDownload < ActiveRecord::Base
   has_one :bulk_action, as: :bulk_actionable
+
+  # Base model for downloading descriptive metadata in bulk. Should start a delayed job given a user selection of
+  # druids to work on. Will need another class in app/jobs/ that implements perform_later().
 end
