0956909c37a7c888de32722dcd2b4fea5d799aa5
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2016-02-21 10:12:42 -0800

    Move spec/integration/report_spec.rb to feature specs

--- spec/features/report_view_spec.rb
+++ spec/features/report_view_spec.rb
@@ -12,4 +12,14 @@ feature 'Report view' do
     visit report_path f: { objectType_ssim: ['item'] }
     expect(page).to have_css 'table#report_grid'
   end
+
+  context 'bulk' do
+    it 'should return a page with the expected elements' do
+      visit '/report/bulk'
+      expect(page).to have_content('Bulk update operations')
+      expect(page).to have_css('.bulk_button', text: 'Get druids from search')
+      expect(page).to have_css('.bulk_button', text: 'Paste a druid list')
+      # expect(page).to have_css('.bulk_button', text: 'Reindex')
+    end
+  end
 end
