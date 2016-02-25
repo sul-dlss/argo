5eb13af2783c4a61d07b9355f798e8aed3398ac9
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2016-02-24 18:05:23 -0800

    Modify specs slightly in spec/features/workflow_service_creation_spec.rb

--- spec/features/workflow_service_creation_spec.rb
+++ spec/features/workflow_service_creation_spec.rb
@@ -1,15 +1,20 @@
 require 'spec_helper'
 
 feature 'Workflow Service Creation' do
+  let(:item) do
+    object = instantiate_fixture('hj185vb7593', Dor::Item)
+    allow(Dor::Item).to receive(:find).with(object.pid).and_return(object)
+    object
+  end
   before :each do
-    admin_user # see spec_helper
+    @current_user = admin_user # see spec_helper
   end
   scenario 'redirect and display on show page' do
-    visit add_workflow_item_path 'druid:qq613vj0238'
+    expect(item).to receive(:create_workflow).and_return(true)
+    visit add_workflow_item_path item.pid
     click_button 'Add'
     within '.flash_messages' do
       expect(page).to have_css '.alert.alert-info', text: 'Added accessionWF'
     end
-    expect(page).to have_css 'tr td a', text: 'accessionWF'
   end
 end
