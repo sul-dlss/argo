5cbc7661783f18c0cf55bc31a069a7c91c87b5b1
Darren Hardy <drh@stanford.edu>
2016-02-26 15:16:52 -0800

    add explicit bundle_without

--- config/deploy/stage-a.rb
+++ config/deploy/stage-a.rb
@@ -2,6 +2,7 @@ server 'argo-stage-a.stanford.edu', user: 'lyberadmin', roles: %w(web db app)
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'staging'
+set :bundle_without, %w(test development).join(' ')
 
 set :deploy_to, '/opt/app/lyberadmin/argo'
 
