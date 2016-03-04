22f60233841be338cc4ec8844eba713b6610f465
Jack Reed <phillipjreed@gmail.com>
2016-03-04 14:28:35 -0500

    Consolidates WorkflowService method logic into the DorObjectWorkflowService model

--- app/controllers/workflow_service_controller.rb
+++ app/controllers/workflow_service_controller.rb
@@ -2,88 +2,41 @@ class WorkflowServiceController < ApplicationController
   ##
   # Is a document closeable?
   def closeable
-    @status = check_if_can_close_version
+    @status = workflow_status.can_close_version?
     render json: @status
   end
 
   ##
   # Is a document openable?
   def openable
-    @status = check_if_can_open_version
+    @status = workflow_status.can_open_version?
     render json: @status
   end
 
   ##
   # Has an object been published?
   def published
-    @status = check_if_published
+    @status = workflow_status.published?
     render json: @status
   end
 
   ##
   # Has an object been submitted?
   def submitted
-    @status = check_if_submitted
+    @status = workflow_status.submitted?
     render json: @status
   end
 
   ##
   # Has an object been accessioned?
   def accessioned
-    @status = check_if_accessioned
+    @status = workflow_status.accessioned?
     render json: @status
   end
 
   private
 
-  def get_lifecycle(task)
-    Dor::Config.workflow.client.get_lifecycle('dor', params[:pid], task)
-  end
-
-  def get_active_lifecycle(task)
-    Dor::Config.workflow.client.get_active_lifecycle('dor', params[:pid], task)
-  end
-
-  ##
-  # Ported over logic from app/helpers/dor_object_helper.rb#LN119
-  # @return [Boolean]
-  def check_if_published
-    return true if get_lifecycle('published')
-    false
-  end
-
-  ##
-  # Ported over logic from app/helpers/dor_object_helper.rb#LN126
-  # @return [Boolean]
-  def check_if_submitted
-    return true if get_lifecycle('submitted')
-    false
-  end
-
-  ##
-  # Ported over logic from app/helpers/dor_object_helper.rb#LN133
-  # @return [Boolean]
-  def check_if_accessioned
-    return true if get_lifecycle('accessioned')
-    false
-  end
-
-  ##
-  # Ported over logic from app/helpers/dor_object_helper.rb#LN167
-  # @return [Boolean]
-  def check_if_can_close_version
-    return true if get_active_lifecycle('opened') &&
-                  !get_active_lifecycle('submitted')
-    false
-  end
-
-  ##
-  # Ported over logic from app/helpers/dor_object_helper.rb#LN160
-  # @return [Boolean]
-  def check_if_can_open_version
-    return false unless check_if_accessioned
-    return false if get_active_lifecycle('submitted')
-    return false if get_active_lifecycle('opened')
-    true
+  def workflow_status
+    DorObjectWorkflowStatus.new(params[:pid])
   end
 end
--- app/models/dor_object_workflow_status.rb
+++ app/models/dor_object_workflow_status.rb
@@ -10,15 +10,52 @@ class DorObjectWorkflowStatus
   ##
   # @return [Boolean]
   def can_open_version?
-    return false unless workflow.get_lifecycle('dor', pid, 'accessioned')
-    return false if workflow.get_active_lifecycle('dor', pid, 'submitted')
-    return false if workflow.get_active_lifecycle('dor', pid, 'opened')
+    return false unless accessioned?
+    return false if get_active_lifecycle('submitted')
+    return false if get_active_lifecycle('opened')
     true
+  end
+
+  ##
+  # @return [Boolean]
+  def can_close_version?
+    return true if get_active_lifecycle('opened') &&
+                  !get_active_lifecycle('submitted')
+    false
+  end
+
+  ##
+  # @return [Boolean]
+  def published?
+    return true if get_lifecycle('published')
+    false
+  end
+
+  ##
+  # @return [Boolean]
+  def submitted?
+    return true if get_lifecycle('submitted')
+    false
+  end
+
+  ##
+  # @return [Boolean]
+  def accessioned?
+    return true if get_lifecycle('accessioned')
+    false
   end
 
   private
 
   def workflow
     Dor::Config.workflow.client
+  end
+
+  def get_lifecycle(task)
+    workflow.get_lifecycle('dor', pid, task)
+  end
+
+  def get_active_lifecycle(task)
+    workflow.get_active_lifecycle('dor', pid, task)
   end
 end
