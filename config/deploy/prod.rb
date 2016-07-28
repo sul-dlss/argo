9295be7e1c860c2da9e010669bb5a7baf23e1ca8
Naomi Dushay <ndushay@stanford.edu>
2016-07-27 17:14:59 -0700

    remove argo-prod deploy script;  VM no longer exists

--- config/deploy/prod.rb
+++
@@ -1,6 +0,0 @@
-server 'argo-prod.stanford.edu', user: 'lyberadmin', roles: %w(web db app)
-
-Capistrano::OneTimeKey.generate_one_time_key!
-set :rails_env, 'production'
-
-set :delayed_job_workers, 12
