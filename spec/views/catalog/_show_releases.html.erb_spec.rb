39821b326f4b5d2527a84d2c175de0273ac032f3
Jack Reed <phillipjreed@gmail.com>
2016-09-28 10:15:34 -0400

    Provide a more robust way of parsing and displaying all release tags
    The methods in dor_services were insufficient to display all release
    tag information. This commit adds useful modeling to show all of these
    tags.

--- spec/views/catalog/_show_releases.html.erb_spec.rb
+++ spec/views/catalog/_show_releases.html.erb_spec.rb
@@ -2,8 +2,7 @@ require 'spec_helper'
 
 describe 'catalog/_show_releases.html.erb' do
   describe 'creates a table of release information' do
-    let(:release_tags) { [['SEARCHWORKS', [{'what' => 'self', 'when' => '2016-09-13 20:00:00 UTC', 'who' => 'pjreed', 'release' => true}]]] }
-    let(:object) { double(pid: 'druid:qq613vj0238', release_tags: release_tags) }
+    let(:object) { instantiate_fixture('druid:qq613vj0238') }
     before do
       allow(view).to receive(:object).and_return(object)
     end
@@ -11,10 +10,10 @@ describe 'catalog/_show_releases.html.erb' do
       render
       expect(rendered).to have_css 'table.table'
       expect(rendered).to have_css 'tbody td:nth-child(1)', text: 'true'
-      expect(rendered).to have_css 'tbody td:nth-child(2)', text: 'self'
-      expect(rendered).to have_css 'tbody td:nth-child(3)', text: 'SEARCHWORKS'
-      expect(rendered).to have_css 'tbody td:nth-child(4)', text: 'pjreed'
-      expect(rendered).to have_css 'tbody td:nth-child(5)', text: '2016-09-13 20:00:00 UTC'
+      expect(rendered).to have_css 'tbody td:nth-child(2)', text: 'self', count: 2
+      expect(rendered).to have_css 'tbody td:nth-child(3)', text: 'SEARCHWORKS', count: 2
+      expect(rendered).to have_css 'tbody td:nth-child(4)', text: 'pjreed', count: 2
+      expect(rendered).to have_css 'tbody td:nth-child(5)', text: '2016-09-13T20:00:00.000Z'
     end
   end
 end
