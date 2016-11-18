191f95d9249b816bd52b830aad09c570a8ed6f02
Chris Beer <chris@cbeer.info>
2016-11-18 08:22:08 -0800

    Fix capybara selector invokation

--- spec/features/bulk_screen_spec.rb
+++ spec/features/bulk_screen_spec.rb
@@ -11,7 +11,7 @@ feature 'Bulk actions view', js: true do
   scenario 'basic page renders ok' do
     visit report_bulk_path
 
-    expect(page).to have_css('h1', 'Bulk update operations')
+    expect(page).to have_css('h1', text: 'Bulk update operations')
     expect(find(:xpath, "//span[@class='bulk_button'][text()='Get druids from search'][not(@disabled)]")).to be_truthy
     expect(find(:xpath, "//span[@class='bulk_button'][text()='Paste a druid list'][not(@disabled)]")).to be_truthy
 
