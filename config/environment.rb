6f041cc5321cdcbfcdfa1430cfb77c8da25d5a6f
Chris Beer <chris@cbeer.info>
2016-11-30 17:14:45 -0800

    Unify the various dor configs into a single initializer

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,5 @@
 # Load the Rails application.
 require_relative 'application'
 
-require File.join(Rails.root, 'config', 'environments', "dor_#{Rails.env}")
 # Initialize the Rails application.
 Rails.application.initialize!
