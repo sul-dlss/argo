692c298c3abe16f9d489fbf98e1a8c3a628afc5b
Chris Beer <chris@cbeer.info>
2017-01-19 09:41:07 -0800

    Allow certain fields (e.g. collections and APOs) to display the full list of facet values after clicking 'more'

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -5,5 +5,36 @@ class SearchBuilder < Blacklight::SearchBuilder
   include Argo::DateFieldQueries
   include Argo::ProfileQueries
 
-  self.default_processor_chain += [:add_access_controls_to_solr_params, :pids_only, :add_date_field_queries, :add_profile_queries]
+  self.default_processor_chain += [
+    :add_access_controls_to_solr_params,
+    :pids_only,
+    :add_date_field_queries,
+    :add_profile_queries
+  ]
+
+  # customize facet paging behavior to allow a separately configurable limit for
+  # the full list of facets
+  def add_facet_paging_to_solr(solr_params)
+    return unless facet.present?
+
+    super
+
+    facet_config = blacklight_config.facet_fields[facet]
+
+    if facet_config.more_limit
+      limit = if scope.respond_to?(:facet_list_limit)
+                scope.facet_list_limit.to_s.to_i
+              elsif solr_params['facet.limit']
+                solr_params['facet.limit'].to_i
+              else
+                facet_config[:more_limit]
+              end
+
+      page = blacklight_params.fetch(request_keys[:page], 1).to_i
+      offset = (page - 1) * limit
+
+      solr_params[:"f.#{facet}.facet.limit"] = limit + 1
+      solr_params[:"f.#{facet}.facet.offset"] = offset
+    end
+  end
 end
