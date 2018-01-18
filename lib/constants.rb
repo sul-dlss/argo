8a51c2aa600bb04d5fbcb0936a0e06a89b38ca7f
Peter Mangiafico <peter@mangiafico.org>
2018-01-18 12:49:28 -0800

    remove goobi workflow drop-down menu and auto-tagging

--- lib/constants.rb
+++ lib/constants.rb
@@ -5,12 +5,6 @@ module Constants
     [human_readable, type_code]
   end
 
-  # the name of the workflow that when selected from the initial workflow triggers an external workflow dropdown with options configured in registration_helper.rb
-  DOR_EXTERNAL_WORKFLOW_NAME = 'goobiWF'
-
-  # the name of the tag prefix that will be used to create the tag with the selected external workflow name (e.g. DPG : Workflow : Goobi_process_name)
-  DOR_EXTERNAL_WORKFLOW_TAG_PREFIX = 'DPG : Workflow'
-
   CONTENT_TYPES = %w(image book file manuscript map media).freeze
 
   RESOURCE_TYPES = %w(image page file audio video).freeze
