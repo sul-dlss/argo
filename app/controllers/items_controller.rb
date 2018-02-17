69f6b93247f430ed2e7ad226321591a0dc361b2f
Justin Coyne <jcoyne@justincoyne.com>
2018-02-16 22:43:20 -0600

    Remove content_type method as it's already defined
    Previous defintion is in `ActionController::Metal#content_type`

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -756,12 +756,6 @@ class ItemsController < ApplicationController
     end
   end
 
-  def content_type
-    respond_to do |format|
-      format.html { render layout: !request.xhr? }
-    end
-  end
-
   private
 
   def reindex(item)
