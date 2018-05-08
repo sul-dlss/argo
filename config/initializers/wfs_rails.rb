edda1aaba80df7e47f87371de3713388614e8533
Justin Coyne <jcoyne@justincoyne.com>
2018-05-08 15:21:07 -0700

    Add wfs as docker on travis

--- config/initializers/wfs_rails.rb
+++ config/initializers/wfs_rails.rb
@@ -1,6 +1,6 @@
-if Settings.WFS_RAILS.ENABLE
-  booted_url = Capybara::Discoball::Runner.new(OpenStruct.new(new: WfsRails::Engine.app)).boot
-  # Reconfigure Dor::Config.workflow.client with booted_url of WfsRails
-  Dor::Config.workflow.url = booted_url
-  Settings.WORKFLOW_URL = booted_url
-end
+# if Settings.WFS_RAILS.ENABLE
+#   booted_url = Capybara::Discoball::Runner.new(OpenStruct.new(new: WfsRails::Engine.app)).boot
+#   # Reconfigure Dor::Config.workflow.client with booted_url of WfsRails
+#   Dor::Config.workflow.url = booted_url
+#   Settings.WORKFLOW_URL = booted_url
+# end
