sul-dlss/argo

commit c3f55a597c9d3c7e97b7e2cbdc641c1010955026

Remove webapp context from the jobs

app/controllers/bulk_actions_controller.rb
@@ -16,8 +16,7 @@ class BulkActionsController < ApplicationController
 
   # POST /bulk_actions
   def create
-    @bulk_action = BulkAction.new(bulk_action_params)
-    @bulk_action.user = current_user
+    @bulk_action = BulkAction.new(bulk_action_params.merge(user: current_user))
 
     if @bulk_action.save
       redirect_to action: :index, notice: 'Bulk action was successfully created.'
@@ -52,6 +51,6 @@ class BulkActionsController < ApplicationController
       :pids,
       manage_release: [:tag, :what, :who, :to],
       set_governing_apo: [:new_apo_id]
-    ).merge(webauth: { privgroup: webauth.privgroup, login: webauth.login })
+    )
   end
 end

app/jobs/release_object_job.rb
@@ -3,7 +3,7 @@
 class ReleaseObjectJob < GenericJob
   queue_as :release_object
 
-  attr_reader :manage_release, :pids, :webauth
+  attr_reader :manage_release, :pids
   ##
   # This is a shameless green approach to a job that calls release from dor
   # services app and then kicks off release WF.
@@ -15,10 +15,8 @@ class ReleaseObjectJob < GenericJob
   # @option manage_release [String] :who required username of releaser
   # @option manage_release [String] :what required type of release (self, collection)
   # @option manage_release [String] :tag required (true, false)
-  # @option webauth [Hash] required for permissions check
   def perform(bulk_action_id, params)
     @manage_release = params[:manage_release]
-    @webauth = OpenStruct.new params[:webauth]
     @pids = params[:pids]
     with_bulk_action_log do |log|
       log.puts("#{Time.current} Starting ReleaseObjectJob for BulkAction #{bulk_action_id}")
@@ -71,7 +69,7 @@ class ReleaseObjectJob < GenericJob
   private
 
   def ability
-    @ability ||= Ability.new(User.find_or_create_by_webauth(webauth))
+    @ability ||= Ability.new(bulk_action.user)
   end
 
   def params_to_body(params)

app/jobs/set_governing_apo_job.rb
@@ -3,11 +3,10 @@
 class SetGoverningApoJob < GenericJob
   queue_as :set_governing_apo
 
-  attr_reader :pids, :new_apo_id, :webauth
+  attr_reader :pids, :new_apo_id
 
   def perform(bulk_action_id, params)
     @new_apo_id = params[:set_governing_apo]['new_apo_id']
-    @webauth = OpenStruct.new params[:webauth]
     @pids = params[:pids]
 
     with_bulk_action_log do |log|
@@ -50,7 +49,7 @@ class SetGoverningApoJob < GenericJob
   end
 
   def ability
-    @ability ||= Ability.new(User.find_or_create_by_webauth(webauth))
+    @ability ||= Ability.new(bulk_action.user)
   end
 
   def update_druid_count

app/models/bulk_action.rb
@@ -9,7 +9,7 @@ class BulkAction < ActiveRecord::Base
   before_destroy :remove_output_directory
 
   # A virtual attribute used for job creation but not persisted
-  attr_accessor :pids, :manage_release, :set_governing_apo, :webauth
+  attr_accessor :pids, :manage_release, :set_governing_apo
 
   def file(filename)
     File.join(output_directory, filename)
@@ -55,7 +55,6 @@ class BulkAction < ActiveRecord::Base
       output_directory: output_directory,
       manage_release: manage_release,
       set_governing_apo: set_governing_apo,
-      webauth: webauth
     }
   end
 

spec/controllers/bulk_actions_controller_spec.rb
@@ -4,10 +4,8 @@ RSpec.describe BulkActionsController do
   let(:current_user) do
     create(:user)
   end
-  let(:webauth) do
-    double('webauth', privgroup: '', login: '')
-  end
-  before(:each) do
+
+  before do
     expect_any_instance_of(BulkActionsController).to receive(:current_user)
       .at_least(:once).and_return(current_user)
   end
@@ -56,10 +54,6 @@ RSpec.describe BulkActionsController do
   end
   describe 'POST create' do
     context 'with correct parameters' do
-      before(:each) do
-        expect_any_instance_of(BulkActionsController).to receive(:webauth)
-          .at_least(:once).and_return(webauth)
-      end
       it 'assigns @bulk_action to current_user' do
         post :create, params: { bulk_action: { action_type: 'GenericJob', pids: '' } }
         expect(assigns(:bulk_action)).to be_an BulkAction

spec/jobs/release_object_job_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe ReleaseObjectJob do
+RSpec.describe ReleaseObjectJob do
   describe '#perform' do
     let(:bulk_action_no_process_callback) do
       bulk_action = build(
@@ -117,6 +117,7 @@ describe ReleaseObjectJob do
         expect(buffer.string).to include 'Trying to start release workflow'
       end
     end
+
     context 'when not authorized' do
       before do
         expect(subject).to receive(:bulk_action).and_return(bulk_action_no_process_callback).at_least(:once)
@@ -139,34 +140,24 @@ describe ReleaseObjectJob do
         expect(buffer.string).to include 'Finished ReleaseObjectJob for BulkAction'
       end
     end
-    describe 'receives and sets params' do
-      before do
-        expect(subject).to receive(:bulk_action).and_return(bulk_action_no_process_callback).at_least(:once)
-        expect(subject).to receive(:can_manage?).and_return(true).exactly(pids.length).times
-        pids.each do |pid|
-          stub_release_tags(pid)
-          stub_release_wf(pid)
-        end
-      end
-      it 'webauth converted to OpenStruct' do
-        subject.perform(bulk_action_no_process_callback.id, params)
-        expect(subject.webauth).to be_an OpenStruct
-        expect(subject.webauth.privgroup).to eq 'dorstuff'
-        expect(subject.webauth.login).to eq 'esnowden'
-      end
-    end
   end
+
   describe '#can_manage?' do
     let(:ability_instance) { instance_double(Ability) }
-    let(:dor_double) { double('Dor') }
+    let(:item) { instance_double(Dor::Item) }
+    let(:job) { described_class.new }
+    let(:bulk_action) { instance_double(BulkAction, user: user) }
+    let(:user) { instance_double(User) }
+
     before do
       expect(Ability).to receive(:new).and_return(ability_instance)
-      expect(Dor).to receive(:find).with('druid:abc123').and_return(dor_double)
-      expect(subject).to receive(:webauth).and_return(double(privgroup: 'dorstuff', login: 'esnowden'))
+      expect(Dor).to receive(:find).with('druid:abc123').and_return(item)
+      expect(job).to receive(:bulk_action).and_return(bulk_action)
     end
+
     it 'calls parent ability manage' do
-      expect(ability_instance).to receive(:can?).with(:manage_item, dor_double).and_return false
-      expect(subject.can_manage?('druid:abc123')).to be false
+      expect(ability_instance).to receive(:can?).with(:manage_item, item).and_return false
+      expect(job.can_manage?('druid:abc123')).to be false
     end
   end
 end

spec/jobs/set_governing_apo_job_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe SetGoverningApoJob do
+RSpec.describe SetGoverningApoJob do
   let(:bulk_action_no_process_callback) do
     bulk_action = build(
       :bulk_action,
@@ -133,13 +133,9 @@ describe SetGoverningApoJob do
   end
 
   describe '#ability' do
-    before { subject.instance_variable_set(:@webauth, webauth) }
-
     it 'caches the result' do
-      user = double(User)
-      expect(User).to receive(:find_or_create_by_webauth).with(webauth).and_return(user).exactly(:once)
       ability = double(Ability)
-      expect(Ability).to receive(:new).with(user).and_return(ability).exactly(:once)
+      expect(Ability).to receive(:new).with(bulk_action_no_process_callback.user).and_return(ability).exactly(:once)
 
       expect(subject.send(:ability)).to be(ability)
       expect(subject.send(:ability)).to be(ability)

spec/models/bulk_action_spec.rb
@@ -52,7 +52,7 @@ RSpec.describe BulkAction do
   end
   it 'makes sure BulkAction job was kicked off' do
     @bulk_action = BulkAction.create(
-      action_type: 'GenericJob', pids: 'a b c', webauth: {}, manage_release: {}
+      action_type: 'GenericJob', pids: 'a b c', manage_release: {}
     )
     expect(GenericJob).to receive(:perform_later)
       .with(
@@ -61,8 +61,7 @@ RSpec.describe BulkAction do
           pids: %w(a b c),
           output_directory: Settings.BULK_METADATA.DIRECTORY +
             "#{@bulk_action.action_type}_#{@bulk_action.id}",
-          manage_release: {},
-          webauth: {}
+          manage_release: {}
         )
       )
     @bulk_action.run_callbacks(:create) { true }

commit 00159760a3489ab73fe8c28969510e30a2a9e6bb

Pass the users groups to the backend jobs that need to check Ability
This should fix https://github.com/sul-dlss/item-release/issues/91
This was caused by https://github.com/sul-dlss/argo/commit/c3f55a597c9d3c7e97b7e2cbdc641c1010955026

app/controllers/bulk_actions_controller.rb
@@ -16,7 +16,10 @@ class BulkActionsController < ApplicationController
 
   # POST /bulk_actions
   def create
-    @bulk_action = BulkAction.new(bulk_action_params.merge(user: current_user))
+    # Since the groups aren't persisted, we need to pass them here.
+    @bulk_action = BulkAction.new(
+      bulk_action_params.merge(user: current_user, groups: current_user.groups)
+    )
 
     if @bulk_action.save
       redirect_to action: :index, notice: 'Bulk action was successfully created.'

app/jobs/release_object_job.rb
@@ -3,7 +3,7 @@
 class ReleaseObjectJob < GenericJob
   queue_as :release_object
 
-  attr_reader :manage_release, :pids
+  attr_reader :manage_release, :pids, :groups
   ##
   # This is a shameless green approach to a job that calls release from dor
   # services app and then kicks off release WF.
@@ -15,8 +15,10 @@ class ReleaseObjectJob < GenericJob
   # @option manage_release [String] :who required username of releaser
   # @option manage_release [String] :what required type of release (self, collection)
   # @option manage_release [String] :tag required (true, false)
+  # @option params [Array] :groups the groups the user belonged to when the started the job. Required for permissions check
   def perform(bulk_action_id, params)
     @manage_release = params[:manage_release]
+    @groups = params[:groups]
     @pids = params[:pids]
     with_bulk_action_log do |log|
       log.puts("#{Time.current} Starting ReleaseObjectJob for BulkAction #{bulk_action_id}")
@@ -69,7 +71,12 @@ class ReleaseObjectJob < GenericJob
   private
 
   def ability
-    @ability ||= Ability.new(bulk_action.user)
+    @ability ||= begin
+      user = bulk_action.user
+      # Since a user doesn't persist its groups, we need to pass the groups in here.
+      user.set_groups_to_impersonate(groups)
+      Ability.new(user)
+    end
   end
 
   def params_to_body(params)

app/jobs/set_governing_apo_job.rb
@@ -3,11 +3,17 @@
 class SetGoverningApoJob < GenericJob
   queue_as :set_governing_apo
 
-  attr_reader :pids, :new_apo_id
+  attr_reader :pids, :new_apo_id, :groups
 
+  # @param [Integer] bulk_action_id GlobalID for a BulkAction object
+  # @param [Hash] params additional parameters that an Argo job may need
+  # @option params [Array] :pids required list of pids
+  # @option params [Hash] :set_governing_apo
+  # @option params [Array] :groups the groups the user belonged to when the started the job. Required for permissions check
   def perform(bulk_action_id, params)
     @new_apo_id = params[:set_governing_apo]['new_apo_id']
     @pids = params[:pids]
+    @groups = params[:groups]
 
     with_bulk_action_log do |log|
       log.puts("#{Time.current} Starting SetGoverningApoJob for BulkAction #{bulk_action_id}")
@@ -49,7 +55,12 @@ class SetGoverningApoJob < GenericJob
   end
 
   def ability
-    @ability ||= Ability.new(bulk_action.user)
+    @ability ||= begin
+      user = bulk_action.user
+      # Since a user doesn't persist its groups, we need to pass the groups in here.
+      user.set_groups_to_impersonate(groups)
+      Ability.new(user)
+    end
   end
 
   def update_druid_count

app/models/bulk_action.rb
@@ -10,6 +10,7 @@ class BulkAction < ActiveRecord::Base
 
   # A virtual attribute used for job creation but not persisted
   attr_accessor :pids, :manage_release, :set_governing_apo
+  attr_accessor :groups # the groups the user was a member of when they launched the job
 
   def file(filename)
     File.join(output_directory, filename)
@@ -55,6 +56,7 @@ class BulkAction < ActiveRecord::Base
       output_directory: output_directory,
       manage_release: manage_release,
       set_governing_apo: set_governing_apo,
+      groups: groups
     }
   end
 

spec/controllers/bulk_actions_controller_spec.rb
@@ -6,8 +6,7 @@ RSpec.describe BulkActionsController do
   end
 
   before do
-    expect_any_instance_of(BulkActionsController).to receive(:current_user)
-      .at_least(:once).and_return(current_user)
+    sign_in current_user
   end
 
   describe 'GET index' do
@@ -54,11 +53,19 @@ RSpec.describe BulkActionsController do
   end
   describe 'POST create' do
     context 'with correct parameters' do
-      it 'assigns @bulk_action to current_user' do
+      let(:groups) { [User::ADMIN_GROUPS.first, 'sunetid:person9'] }
+
+      before do
+        allow(current_user).to receive(:groups).and_return(groups)
+      end
+
+      it 'assigns @bulk_action to current_user and passes current groups' do
         post :create, params: { bulk_action: { action_type: 'GenericJob', pids: '' } }
         expect(assigns(:bulk_action)).to be_an BulkAction
         expect(assigns(:bulk_action).user).to eq current_user
+        expect(assigns(:bulk_action).groups).to eq groups
       end
+
       it 'creates a new BulkAction' do
         expect do
           post :create, params: { bulk_action: { action_type: 'GenericJob', pids: '' } }

spec/jobs/release_object_job_spec.rb
@@ -146,16 +146,19 @@ RSpec.describe ReleaseObjectJob do
     let(:ability_instance) { instance_double(Ability) }
     let(:item) { instance_double(Dor::Item) }
     let(:job) { described_class.new }
-    let(:bulk_action) { instance_double(BulkAction, user: user) }
-    let(:user) { instance_double(User) }
+    let(:bulk_action) { create(:bulk_action) }
 
     before do
       expect(Ability).to receive(:new).and_return(ability_instance)
       expect(Dor).to receive(:find).with('druid:abc123').and_return(item)
-      expect(job).to receive(:bulk_action).and_return(bulk_action)
+
+      # In this test, perform() is not being called. This is what sets these attributes
+      allow(job).to receive(:bulk_action).and_return(bulk_action)
+      allow(job).to receive(:groups).and_return(['admin'])
     end
 
     it 'calls parent ability manage' do
+      expect_any_instance_of(User).to receive(:set_groups_to_impersonate).with(['admin'])
       expect(ability_instance).to receive(:can?).with(:manage_item, item).and_return false
       expect(job.can_manage?('druid:abc123')).to be false
     end