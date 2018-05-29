sul-dlss/argo

commit 9c2c78c7bd84eb52f7c63ba4c7666b8d20b04a5b

Extract batch upload methods from the catalog_controller
Codeclimate says:
```
Class CatalogController has 25 methods (exceeds 20 allowed). Consider refactoring.
```

This refactor gets us slightly closer to that goal.

.rubocop_todo.yml
@@ -1,6 +1,6 @@
 # This configuration was generated by
 # `rubocop --auto-gen-config`
-# on 2018-05-23 11:23:31 -0500 using RuboCop version 0.56.0.
+# on 2018-05-23 13:42:27 -0500 using RuboCop version 0.56.0.
 # The point is for the user to remove these configuration records
 # one by one as the offenses are removed from the code base.
 # Note that changes in the inspected code, or installation of new
@@ -604,7 +604,7 @@ Style/StringLiterals:
     - 'config/environments/staging.rb'
     - 'config/puma.rb'
 
-# Offense count: 49
+# Offense count: 50
 # Cop supports --auto-correct.
 # Configuration parameters: MinSize.
 # SupportedStyles: percent, brackets

app/controllers/catalog_controller.rb
@@ -198,26 +198,6 @@ class CatalogController < ApplicationController
     end
   end
 
-  def bulk_upload_form
-    @obj = Dor.find params[:id]
-  end
-
-  # Lets the user start a bulk metadata job (i.e. upload a metadata spreadsheet/XML file).
-  def upload
-    @apo = Dor.find params[:id]
-
-    directory_name = Time.zone.now.strftime('%Y_%m_%d_%H_%M_%S_%L')
-    output_directory = File.join(Settings.BULK_METADATA.DIRECTORY, params[:druid], directory_name)
-    temp_spreadsheet_filename = params[:spreadsheet_file].original_filename + '.' + directory_name
-
-    # Temporary files are sometimes garbage collected before the Delayed Job is run, so make a copy and let the job delete it when it's done.
-    temp_filename = make_tmp_filename(temp_spreadsheet_filename)
-    FileUtils.copy(params[:spreadsheet_file].path, temp_filename)
-    ModsulatorJob.perform_later(@apo.id, temp_filename.to_s, output_directory, current_user.login, params[:filetypes], params[:xml_only], params[:note])
-
-    redirect_to bulk_jobs_index_path(@apo.id)
-  end
-
   # Generates the index page for a given DRUID's past bulk metadata upload jobs.
   def bulk_jobs_index
     params[:id] = 'druid:' + params[:id] unless params[:id].include? 'druid'

app/controllers/uploads_controller.rb
@@ -1,0 +1,24 @@
+# Handles the HTTP interaction for creating bulk metadata uploads
+class UploadsController < ApplicationController
+  # GET /items/:item_id/uploads/new
+  def new
+    @obj = Dor.find params[:item_id]
+  end
+
+  # Lets the user start a bulk metadata job (i.e. upload a metadata spreadsheet/XML file).
+  # POST /items/:item_id/uploads
+  def create
+    apo = Dor.find params[:item_id]
+
+    directory_name = Time.zone.now.strftime('%Y_%m_%d_%H_%M_%S_%L')
+    output_directory = File.join(Settings.BULK_METADATA.DIRECTORY, params[:druid], directory_name)
+    temp_spreadsheet_filename = params[:spreadsheet_file].original_filename + '.' + directory_name
+
+    # Temporary files are sometimes garbage collected before the Delayed Job is run, so make a copy and let the job delete it when it's done.
+    temp_filename = make_tmp_filename(temp_spreadsheet_filename)
+    FileUtils.copy(params[:spreadsheet_file].path, temp_filename)
+    ModsulatorJob.perform_later(apo.id, temp_filename.to_s, output_directory, current_user.login, params[:filetypes], params[:xml_only], params[:note])
+
+    redirect_to bulk_jobs_index_path(apo.id)
+  end
+end

app/views/catalog/_bulk_start.html.erb
@@ -12,6 +12,6 @@
 </div>
 <div class="row spreadsheet-row">
     <div class="col-md-12">
-	<%= link_to("Submit new file ...", bulk_upload_form_path(@obj.id), :class => "button btn btn-primary", data: { ajax_modal: 'trigger', ajax_modal_title: 'MODS Bulk Loads' }) %>
+      <%= link_to("Submit new file ...", new_item_upload_path(@obj.id), class: "button btn btn-primary", data: { ajax_modal: 'trigger', ajax_modal_title: 'MODS Bulk Loads' }) %>
     </div>
 </div>

app/views/uploads/new.html.erb
@@ -1,6 +1,6 @@
 <div data-ajax-modal="container">
     <div id="spreadsheet-upload-container">
-	<%= form_tag(upload_path(@obj.id), multipart: true) do %>
+	<%= form_tag(item_uploads_path(@obj.id), multipart: true) do %>
         <div id="bulk-upload-form" class="container-fluid">
 	    <div class="row spreadsheet-row">
 		<div class="col-md-10">

config/routes.rb
@@ -30,14 +30,12 @@ Argo::Application.routes.draw do
     end
   end
 
-  get 'catalog/:id/bulk_upload_form',    to: 'catalog#bulk_upload_form',  as: 'bulk_upload_form'
   get 'catalog/:id/bulk_jobs_index',     to: 'catalog#bulk_jobs_index',   as: 'bulk_jobs_index'
   get 'catalog/:id/bulk_status_help',    to: 'catalog#bulk_status_help',  as: 'bulk_status_help'
   get 'catalog/:id/:time/bulk_jobs_xml', to: 'catalog#bulk_jobs_xml',     as: 'bulk_jobs_xml'
   get 'catalog/:id/:time/bulk_jobs_log', to: 'catalog#bulk_jobs_log',     as: 'bulk_jobs_log'
   get 'catalog/:id/bulk_jobs_help',      to: 'catalog#bulk_jobs_help',    as: 'bulk_jobs_help'
   get 'catalog/:id/:time/bulk_jobs_csv', to: 'catalog#bulk_jobs_csv',     as: 'bulk_jobs_csv'
-  post 'catalog/:id/upload',             to: 'catalog#upload',            as: 'upload'
   delete 'catalog/:id/bulk_jobs_delete', to: 'catalog#bulk_jobs_delete',  as: 'bulk_jobs_delete'
   match 'catalog',      via: [:get, :post], to: redirect { |params, req| req.fullpath.sub(%r{^/catalog}, '/view') }, as: 'search_catalog_redirect'
   match 'catalog/*all', via: [:get, :post], to: redirect { |params, req| req.fullpath.sub(%r{^/catalog}, '/view') }, as: 'catalog_redirect'
@@ -84,6 +82,8 @@ Argo::Application.routes.draw do
   end
 
   resources :items do
+    resources 'uploads', only: [:new, :create]
+
     get :register, on: :collection
     resource :content_type, only: [:show, :update]
     member do

spec/features/bulk_jobs_spec.rb
@@ -8,17 +8,17 @@ RSpec.feature 'Bulk jobs view', js: true do
 
   scenario 'the submit button exists' do
     visit bulk_jobs_index_path('druid:hv992ry2431')
-    expect(page).to have_link('Submit new file ...', href: '/catalog/druid:hv992ry2431/bulk_upload_form')
+    expect(page).to have_link('Submit new file ...', href: new_item_upload_path(item_id: 'druid:hv992ry2431'))
   end
 
   scenario 'click submit button opens bulk upload form' do
-    visit bulk_upload_form_path(id: 'druid:hv992ry2431')
+    visit new_item_upload_path(item_id: 'druid:hv992ry2431')
     expect(page).to have_css('div#spreadsheet-upload-container form div#bulk-upload-form')
     expect(page).to have_css('input#spreadsheet_file')
   end
 
   scenario 'bulk upload form buttons are disabled upon first page visit' do
-    visit bulk_upload_form_path(id: 'druid:hv992ry2431')
+    visit new_item_upload_path(item_id: 'druid:hv992ry2431')
     expect(find('input#filetypes_1').disabled?).to be_truthy
     expect(find('input#filetypes_2').disabled?).to be_truthy
     expect(find('input#convert_only').disabled?).to be_truthy
@@ -28,7 +28,7 @@ RSpec.feature 'Bulk jobs view', js: true do
   end
 
   scenario 'selecting a file to upload and selecting one of the radio buttons enables the submit button' do
-    visit bulk_upload_form_path(id: 'druid:hv992ry2431')
+    visit new_item_upload_path(item_id: 'druid:hv992ry2431')
     expect(page).to have_css('#spreadsheet_file')
     expect(find('input#filetypes_1').disabled?).to be_truthy
 
@@ -46,7 +46,7 @@ RSpec.feature 'Bulk jobs view', js: true do
   end
 
   scenario 'uploading a file with an invalid extension displays a warning' do
-    visit bulk_upload_form_path(id: 'druid:hv992ry2431')
+    visit new_item_upload_path(item_id: 'druid:hv992ry2431')
     expect(page).to have_css('#spreadsheet_file')
     attach_file('spreadsheet_file', File.absolute_path(__FILE__))
 

spec/views/uploads/new.html.erb_spec.rb
@@ -1,19 +1,16 @@
 require 'spec_helper'
 
-RSpec.describe 'catalog/bulk_upload_form.html.erb', type: :view do
-  before :each do
+RSpec.describe 'uploads/new.html.erb', type: :view do
+  before do
     @obj = double('dor_object', id: 'druid:hv992ry2431')
+    render
   end
 
   it 'has the correct title' do
-    assign(:object, @obj)
-    render
     expect(rendered).to have_css('strong', text: 'Submit MODS descriptive metadata for bulk processing')
   end
 
   it 'has the correct overall structure' do
-    assign(:object, @obj)
-    render
     expect(rendered).to have_css('div#bulk-upload-form')
     expect(rendered).to have_css('div#spreadsheet-upload-container form div#bulk-upload-form')
     expect(rendered).to have_css('div.row.spreadsheet-row', count: 5)
@@ -21,8 +18,6 @@ RSpec.describe 'catalog/bulk_upload_form.html.erb', type: :view do
   end
 
   it 'has Browse, Submit and Cancel buttons' do
-    assign(:object, @obj)
-    render
     expect(rendered).to have_css('input#spreadsheet_file')
     expect(rendered).to have_css('button#spreadsheet_submit')
     expect(rendered).to have_css('button#spreadsheet_cancel')

commit 5b5f23bd00dea7e494f1b2612cb645f8596e2e19

Move make_tmp_filename to UploadsController
This bug was introduced in 9c2c78c7bd84eb52f7c63ba4c7666b8d20b04a5b
when the UploadsController was extracted from the CatalogController

.rubocop_todo.yml
@@ -1,6 +1,6 @@
 # This configuration was generated by
 # `rubocop --auto-gen-config`
-# on 2018-05-23 13:42:27 -0500 using RuboCop version 0.56.0.
+# on 2018-05-29 08:20:19 -0500 using RuboCop version 0.56.0.
 # The point is for the user to remove these configuration records
 # one by one as the offenses are removed from the code base.
 # Note that changes in the inspected code, or installation of new
@@ -78,7 +78,7 @@ Lint/UselessAssignment:
 Metrics/AbcSize:
   Max: 83
 
-# Offense count: 162
+# Offense count: 165
 # Configuration parameters: CountComments, ExcludedMethods.
 Metrics/BlockLength:
   Max: 710
@@ -459,13 +459,12 @@ Style/MutableConstant:
     - 'app/models/concerns/apo_concern.rb'
     - 'lib/tasks/argo_testing.rake'
 
-# Offense count: 2
+# Offense count: 1
 # Cop supports --auto-correct.
 # Configuration parameters: EnforcedStyle.
 # SupportedStyles: both, prefix, postfix
 Style/NegatedIf:
   Exclude:
-    - 'app/controllers/catalog_controller.rb'
     - 'app/jobs/modsulator_job.rb'
 
 # Offense count: 8

app/controllers/catalog_controller.rb
@@ -337,11 +337,6 @@ class CatalogController < ApplicationController
     File.join(job_output_directory, bulk_job_metadata(job_output_directory)['argo.bulk_metadata.bulk_log_xml_filename'])
   end
 
-  def make_tmp_filename(temp_spreadsheet_filename)
-    FileUtils.mkdir_p(Settings.BULK_METADATA.TEMPORARY_DIRECTORY) if !File.exist?(Settings.BULK_METADATA.TEMPORARY_DIRECTORY)
-    File.join(Settings.BULK_METADATA.TEMPORARY_DIRECTORY, temp_spreadsheet_filename)
-  end
-
   # Sorts the Blacklight collection actions buttons so that the "Bulk Action" and "Bulk Update View" buttons appear
   # at the front of the list.
   def sort_collection_actions_buttons

app/controllers/uploads_controller.rb
@@ -21,4 +21,11 @@ class UploadsController < ApplicationController
 
     redirect_to bulk_jobs_index_path(apo.id)
   end
+
+  private
+
+  def make_tmp_filename(temp_spreadsheet_filename)
+    FileUtils.mkdir_p(Settings.BULK_METADATA.TEMPORARY_DIRECTORY) unless File.exist?(Settings.BULK_METADATA.TEMPORARY_DIRECTORY)
+    File.join(Settings.BULK_METADATA.TEMPORARY_DIRECTORY, temp_spreadsheet_filename)
+  end
 end