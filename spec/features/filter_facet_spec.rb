393720773ed45f6093d8d948b2e8212e5ac595df
Justin Littman <justinlittman@stanford.edu>
2018-12-05 12:02:04 -0500

    Added filtering of more facets.
    closes #201

---
+++ spec/features/filter_facet_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.feature 'More facet view', js: true do
+  before do
+    sign_in create(:user), groups: ['sdr:administrator-role']
+  end
+
+  scenario 'filter works correctly' do
+    visit '/catalog/facet/nonhydrus_collection_title_ssim'
+
+    filter_field = find_field(id: 'filterInput')
+    expect(filter_field).not_to be_nil
+
+    expect(page).to have_content('druid:pb873ty1662')
+    filter_field.fill_in(with: 'foo')
+    expect(page).not_to have_content('druid:pb873ty1662')
+    filter_field.fill_in(with: 'druid:')
+    expect(page).to have_content('druid:pb873ty1662')
+    filter_field.fill_in(with: 'druid:foo')
+    expect(page).not_to have_content('druid:pb873ty1662')
+    filter_field.fill_in(with: 'druid:pb873ty1662')
+    expect(page).to have_content('druid:pb873ty1662')
+  end
+end
