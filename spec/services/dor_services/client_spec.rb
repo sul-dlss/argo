0082be688c050489e6da28382de9d93969352ca9
Michael J. Giarlo <mjgiarlo@stanford.edu>
2018-12-14 15:17:41 -0800

    Add spec coverage when API call fails

--- spec/services/dor_services/client_spec.rb
+++ spec/services/dor_services/client_spec.rb
@@ -23,6 +23,7 @@ RSpec.describe DorServices::Client do
   describe '#register' do
     before do
       allow(client).to receive(:connection).and_return(fake_connection)
+      allow(fake_connection).to receive(:post).and_yield(fake_request).and_return(fake_response)
     end
 
     let(:fake_connection) { double }
@@ -30,15 +31,35 @@ RSpec.describe DorServices::Client do
       double(url: nil, headers: fake_headers, 'body=' => nil)
     end
     let(:fake_headers) { {} }
-    let(:fake_body) { double(body: '{"pid":"druid:123"}') }
 
-    it 'posts params as json' do
-      expect(fake_connection).to receive(:post).and_yield(fake_request).and_return(fake_body)
-      expect(fake_request).to receive(:url).with('v1/objects')
-      expect(fake_request).to receive(:headers).and_return(fake_headers)
-      expect(fake_request).to receive(:body=).with('{"foo":"bar"}')
-      expect(fake_headers).to receive(:[]=).with('Content-Type', 'application/json')
-      expect(client.register[:pid]).to eq('druid:123')
+    context 'when API request succeeds' do
+      let(:fake_response) do
+        double(body: '{"pid":"druid:123"}',
+               success?: true)
+      end
+
+      it 'posts params as json' do
+        expect(fake_request).to receive(:url).with('v1/objects')
+        expect(fake_request).to receive(:headers).and_return(fake_headers)
+        expect(fake_request).to receive(:body=).with('{"foo":"bar"}')
+        expect(fake_headers).to receive(:[]=).with('Content-Type', 'application/json')
+        expect(client.register[:pid]).to eq('druid:123')
+        expect(fake_connection).to have_received(:post).once
+      end
+    end
+
+    context 'when API request fails' do
+      let(:fake_response) do
+        double(body: '',
+               success?: false,
+               reason_phrase: 'object already exists',
+               status: '409')
+      end
+
+      it 'raises an error' do
+        expect { client.register[:pid] }.to raise_error('object already exists: 409 ()')
+        expect(fake_connection).to have_received(:post).once
+      end
     end
   end
 end
