c2c95fb697597d71e4a54141b3db1aacec2ea52b
Justin Coyne <jcoyne@justincoyne.com>
2018-12-14 13:46:54 -0600

    This prevents warnings about SQLite3Adapter.represent_boolean_as_integer

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,9 @@ Bundler.require(*Rails.groups)
 
 module Argo
   class Application < Rails::Application
+    # Initialize configuration defaults
+    config.load_defaults 5.2
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
