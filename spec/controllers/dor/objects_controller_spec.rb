6ff27b6f9c91a5f3d0e9a2f2bf373ff59a0da247
Justin Coyne <jcoyne@justincoyne.com>
2019-05-01 21:59:34 -0500

    Show a conflict to the user when dor-services-app reports a conflict

--- spec/controllers/dor/objects_controller_spec.rb
+++ spec/controllers/dor/objects_controller_spec.rb
@@ -13,12 +13,30 @@ RSpec.describe Dor::ObjectsController, type: :controller do
   let(:dor_registration) { { pid: 'abc' } }
 
   describe '#create' do
-    it 'registers the object and reindexes the pid list' do
-      expect(Dor::Services::Client.objects)
-        .to receive(:register)
-        .and_return(dor_registration)
-      post :create
-      expect(mock_object).to have_received(:update_index)
+    context 'when register is successful' do
+      it 'registers the object' do
+        expect(Dor::Services::Client.objects)
+          .to receive(:register)
+          .and_return(dor_registration)
+        post :create
+        expect(response).to be_redirect
+      end
+    end
+
+    context 'when register is a conflict' do
+      let(:message) { "Conflict: 409 (An object with the source ID 'sul:36105226711146' has already been registered" }
+
+      before do
+        allow(Dor::Services::Client.objects)
+          .to receive(:register)
+          .and_raise(Dor::Services::Client::UnexpectedResponse, message)
+      end
+
+      it 'shows an error' do
+        post :create
+        expect(response.status).to eq 409
+        expect(response.body).to eq message
+      end
     end
   end
 end
