d50aecd570e100128d3666152a9fe977b37c5ef4
Michael J. Giarlo <mjgiarlo@stanford.edu>
2019-08-13 20:10:42 -0700

    Remove unsupported dor-services-client configuration options (#1468)
    * Remove unsupported dor-services-client configuration options
    
    * Remove unused DSC configuration options
    
    * Amend docker-compose with related changes
    
    * Revert formatting change to docker-compose config
    
    * Try generating a JWT in a spec helper using the same hmac secret as the service
    
    Note that this secret is a dummy secret that is already public; we do not use this in our deployed instances.
    
    * Use dummy token value
    
    * Remove JWT stuff in spec/
    
    * Set the token in the settings.yml

--- config/initializers/dor_services_client.rb
+++ config/initializers/dor_services_client.rb
@@ -2,7 +2,5 @@
 
 # Configure dor-services-client to use the dor-services URL
 Dor::Services::Client.configure(url: Settings.DOR_SERVICES.URL,
-                                username: Settings.DOR_SERVICES.USER,
-                                password: Settings.DOR_SERVICES.PASS,
                                 token: Settings.DOR_SERVICES.TOKEN,
                                 token_header: Settings.DOR_SERVICES.TOKEN_HEADER)
