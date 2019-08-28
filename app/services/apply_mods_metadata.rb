4023c05f365a3a89f6eb326fbb24c2ef70d07a94
Justin Coyne <jcoyne@justincoyne.com>
2019-08-28 13:41:26 -0500

    Extract ApplyModsMetadata from ModsulatorJob
    This encapsulates the knowledge of how to apply the metadata from the job.

---
+++ app/services/apply_mods_metadata.rb
@@ -0,0 +1,113 @@
+# frozen_string_literal: true
+
+# Updates the metadata of an object with the given MODS
+class ApplyModsMetadata
+  # @param [Nokogiri::XML::Element] mods_node A MODS XML node.
+  # @param [Dor::Item] item the item to be updated
+  def initialize(apo_druid:, mods_node:, item:, original_filename:, user_login:, log:)
+    @apo_druid = apo_druid
+    @mods_node = mods_node
+    @item = item
+    @original_filename = original_filename
+    @user_login = user_login
+    @log = log
+  end
+
+  def apply
+    return unless item
+
+    # Only update objects that are governed by the correct APO
+    unless item.admin_policy_object_id == apo_druid
+      log.puts("argo.bulk_metadata.bulk_log_apo_fail #{item_druid}")
+      return
+    end
+    if in_accessioning?
+      log.puts("argo.bulk_metadata.bulk_log_skipped_accession #{item_druid}")
+      return
+    end
+
+    return unless status_ok?
+
+    # We only update objects if the descMetadata XML is different
+    current_metadata = item.descMetadata.content
+    if equivalent_nodes(Nokogiri::XML(current_metadata).root, mods_node)
+      log.puts("argo.bulk_metadata.bulk_log_skipped_mods #{item_druid}")
+      return
+    end
+
+    version_object(original_filename, user_login, log)
+
+    item.descMetadata.content = mods_node.to_s
+    item.save!
+    log.puts("argo.bulk_metadata.bulk_log_job_save_success #{item_druid}")
+  rescue StandardError => e
+    log.puts("argo.bulk_metadata.bulk_log_error_exception #{item_druid}")
+    log.puts(e.message.to_s)
+    log.puts(e.backtrace.to_s)
+  end
+
+  private
+
+  attr_reader :apo_druid, :mods_node, :item, :original_filename, :user_login, :log
+
+  # Open a new version for the given object if it is in the accessioned state.
+  def version_object
+    return unless accessioned?
+
+    unless DorObjectWorkflowStatus.new(item.pid).can_open_version?
+      log.puts("argo.bulk_metadata.bulk_log_unable_to_version #{item.pid}") # totally unexpected
+      return
+    end
+    commit_new_version
+  end
+
+  # Open a new version for the given object.
+  def commit_new_version
+    vers_md_upd_info = {
+      significance: 'minor',
+      description: "Descriptive metadata upload from #{original_filename}",
+      opening_user_name: user_login
+    }
+    Dor::Services::Client.object(item.pid).version.open(vers_md_upd_info: vers_md_upd_info)
+  end
+
+  # Check if two MODS XML nodes are equivalent.
+  #
+  # @param [Nokogiri::XML::Element] node1 A MODS XML node.
+  # @param [Nokogiri::XML::Element] node2 A MODS XML node.
+  # @return [Boolean] true if the given nodes are equivalent, false otherwise.
+  def equivalent_nodes(node1, node2)
+    EquivalentXml.equivalent?(node1,
+                              node2,
+                              element_order: false,
+                              normalize_whitespace: true,
+                              ignore_attr_values: ['version', 'xmlns', 'xmlns:xsi', 'schemaLocation'])
+  end
+
+  # Returns true if the given object is accessioned, false otherwise.
+  def accessioned?
+    (6..8).cover?(status)
+  end
+
+  # Checks whether or not a DOR object is in accessioning or not.
+  #
+  # @return [Boolean]     true if the object is currently being accessioned, false otherwise
+  def in_accessioning?
+    (2..5).cover?(status)
+  end
+
+  # Checks whether or not a DOR object's status is OK for a descMetadata update. Basically, the only times we are
+  # not OK to update is if the object is currently being accessioned and if the object has status unknown.
+  #
+  # @return [Boolean]     true if the object's status allows us to update the descMetadata datastream, false otherwise
+  def status_ok?
+    [1, 6, 7, 8, 9].include?(status)
+  end
+
+  # Returns the status_info for a DOR object from the StatusService
+  #
+  # @return [Integer]     value cooresponding to the status info list
+  def status
+    @status ||= Dor::StatusService.new(item).status_info[:status_code]
+  end
+end
---
+++ spec/services/apply_mods_metadata_spec.rb
@@ -0,0 +1,124 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ApplyModsMetadata do
+  let(:apo_druid) {}
+  let(:mods_node) {}
+  let(:item) { instance_double(Dor::Item, pid: 'druid:123abc') }
+  let(:log) { instance_double(File, puts: true) }
+  let(:action) do
+    described_class.new(apo_druid: apo_druid,
+                        mods_node: mods_node,
+                        item: item,
+                        original_filename: 'testfile.xlsx',
+                        user_login: 'username',
+                        log: log)
+  end
+
+  describe 'version_object' do
+    before do
+      allow(Dor::StatusService).to receive(:new).and_return(stub_service)
+    end
+
+    let(:stub_service) { instance_double(Dor::StatusService, status_info: { status_code: status_code }) }
+    let(:status_code) { 6 }
+    let(:workflow) { instance_double(DorObjectWorkflowStatus) }
+
+    it 'writes a log error message if a version cannot be opened' do
+      expect(DorObjectWorkflowStatus).to receive(:new).with(item.pid).and_return(workflow)
+      expect(workflow).to receive(:can_open_version?).and_return(false)
+
+      action.send(:version_object)
+      expect(log).to have_received(:puts).with("argo.bulk_metadata.bulk_log_unable_to_version #{item.pid}")
+    end
+
+    context 'the object is in the registered state' do
+      let(:status_code) { 1 }
+
+      it 'does not update the version' do
+        expect(action).not_to receive(:commit_new_version)
+
+        action.send(:version_object)
+      end
+    end
+
+    context 'the object is in the opened state' do
+      let(:status_code) { 9 }
+
+      it 'does not update the version ' do
+        expect(action).not_to receive(:commit_new_version)
+
+        action.send(:version_object)
+      end
+    end
+
+    it 'updates the version if the object is past the registered state' do
+      expect(DorObjectWorkflowStatus).to receive(:new).with(item.pid).and_return(workflow)
+      expect(workflow).to receive(:can_open_version?).and_return(true)
+      expect(action).to receive(:commit_new_version)
+
+      action.send(:version_object)
+    end
+  end
+
+  describe '#commit_new_version' do
+    let(:client) { instance_double(Dor::Services::Client::Object, version: version_client) }
+    let(:version_client) { instance_double(Dor::Services::Client::ObjectVersion, open: true) }
+
+    before do
+      allow(Dor::Services::Client).to receive(:object).and_return(client)
+    end
+
+    it 'opens a new minor version with filename and username' do
+      action.send(:commit_new_version)
+
+      expect(version_client).to have_received(:open).with(
+        vers_md_upd_info: {
+          significance: 'minor',
+          description: 'Descriptive metadata upload from testfile.xlsx',
+          opening_user_name: 'username'
+        }
+      )
+    end
+  end
+
+  describe 'status_ok?' do
+    (0..9).each do |i|
+      it "correctly queries the status of DOR objects (:status_code #{i})" do
+        allow(action).to receive(:status).and_return(i)
+        if [1, 6, 7, 8, 9].include?(i)
+          expect(action.send(:status_ok?)).to be_truthy
+        else
+          expect(action.send(:status_ok?)).to be_falsy
+        end
+      end
+    end
+  end
+
+  describe 'in_accessioning?' do
+    (0..9).each do |i|
+      it "returns true for DOR objects that are currently in acccessioning, false otherwise (:status_code #{i})" do
+        allow(action).to receive(:status).and_return(i)
+        if [2, 3, 4, 5].include?(i)
+          expect(action.send(:in_accessioning?)).to be_truthy
+        else
+          expect(action.send(:in_accessioning?)).to be_falsy
+        end
+      end
+    end
+  end
+
+  describe 'accessioned?' do
+    (0..9).each do |i|
+      it "returns true for DOR objects that are acccessioned, false otherwise (:status_code #{i})" do
+        allow(action).to receive(:status).and_return(i)
+        if [6, 7, 8].include?(i)
+          expect(action.send(:accessioned?)).to be_truthy
+        else
+          expect(action.send(:accessioned?)).to be_falsy
+        end
+      end
+    end
+  end
+end
