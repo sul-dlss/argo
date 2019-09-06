c3e83ae71f880893b3a11550e72adf6799357eec
Justin Coyne <jcoyne@justincoyne.com>
2019-09-06 09:35:05 -0500

    Remove unused javascript function

--- app/assets/javascripts/argo.js
+++ app/assets/javascripts/argo.js
@@ -1,11 +1,3 @@
-// Put your application scripts here
-
-$.fn.preload = function() {
-    this.each(function(){
-        $('<img/>')[0].src = this;
-    });
-}
-
 function pathTo(path) {
   var root = $('body').attr('data-application-root') || '';
   return(root + path);
