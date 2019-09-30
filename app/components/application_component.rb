f4784c45de068d8e3fb46c2f3c4899a12ea2a094
Justin Coyne <jcoyne@justincoyne.com>
2019-09-30 15:37:05 -0500

    Stop monkeypatching actionview-component
    These features are now built into the 1.3.0 version

--- app/components/application_component.rb
+++ app/components/application_component.rb
@@ -1,19 +1,5 @@
 # frozen_string_literal: true
 
-# See https://github.com/github/actionview-component/issues/15#issuecomment-523433555
-# https://github.com/github/actionview-component/pull/28/files
 class ApplicationComponent < ActionView::Component::Base
-  include ActiveSupport::Configurable
-  include ActionController::RequestForgeryProtection
-
-  def render_in(view_context, *args, &block)
-    # Enable button_to/forms in components
-    self.controller = view_context.controller
-    # Enable partials to be rendered in components
-    @view_renderer ||= view_context.view_renderer
-    @lookup_context ||= view_context.lookup_context
-    super
-  end
-
   delegate :can?, to: :controller
 end
