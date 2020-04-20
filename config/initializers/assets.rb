b82bcd312a5cdc9e5cb2aaafc55acb6b8e4a7f5b
Justin Coyne <jcoyne@justincoyne.com>
2020-04-20 07:33:07 -0500

    Add manifest

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += ['registration.css', 'report.css', '*.js']
+# Rails.application.config.assets.precompile += ['registration.css']
 
 Rails.application.config.assets.compress = true
