17905aa3244f44a60db332c2218028273fba7b28
Justin Coyne <jcoyne@justincoyne.com>
2020-04-24 12:19:46 -0500

    Test what was sent to registration
    Handle Document type, and reading direction.

--- spec/controllers/dor/objects_controller_spec.rb
+++ spec/controllers/dor/objects_controller_spec.rb
@@ -5,14 +5,12 @@ require 'rails_helper'
 RSpec.describe Dor::ObjectsController, type: :controller do
   before do
     sign_in(create(:user))
-
     allow(Dor).to receive(:find).with(pid).and_return(mock_object)
   end
 
   let(:pid) { 'druid:abc' }
   let(:mock_object) { instance_double(Dor::Item) }
   let(:workflow_service) { instance_double(Dor::Workflow::Client, create_workflow_by_name: nil) }
-  let(:objects_client) { instance_double(Dor::Services::Client::Objects, register: dor_registration) }
   let(:object_client) { instance_double(Dor::Services::Client::Object, administrative_tags: administrative_tags) }
   let(:administrative_tags) { instance_double(Dor::Services::Client::AdministrativeTags, create: true) }
   let(:dor_registration) { instance_double(Cocina::Models::DRO, externalIdentifier: pid) }
@@ -20,7 +18,6 @@ RSpec.describe Dor::ObjectsController, type: :controller do
   describe '#create' do
     before do
       allow(Dor::Services::Client).to receive(:object).and_return(object_client)
-      allow(Dor::Services::Client).to receive(:objects).and_return(objects_client)
       allow(Dor::Workflow::Client).to receive(:new).and_return(workflow_service)
     end
 
@@ -44,7 +41,6 @@ RSpec.describe Dor::ObjectsController, type: :controller do
         # this exception is handled by the Rails exception wrapper middleware in production
         expect { post :create, params: submitted }.to raise_error ActionController::ParameterMissing
 
-        expect(objects_client).not_to have_received(:register)
         expect(workflow_service).not_to have_received(:create_workflow_by_name)
       end
     end
@@ -58,7 +54,7 @@ RSpec.describe Dor::ObjectsController, type: :controller do
           workflow_id: 'registrationWF',
           metadata_source: 'label',
           label: 'test parameters for registration',
-          tag: ['Process : Content Type : Book (ltr)',
+          tag: ['Process : Content Type : Document',
                 'Registered By : jcoyne85'],
           seed_datastream: ['descMetadata'],
           rights: 'default',
@@ -67,14 +63,33 @@ RSpec.describe Dor::ObjectsController, type: :controller do
         }
       end
 
+      let(:json) do
+        Cocina::Models::DRO.new(externalIdentifier: 'druid:bc234fg5678',
+                                type: Cocina::Models::Vocab.document,
+                                label: '',
+                                version: 1,
+                                access: {
+                                  access: 'location-based'
+                                }).to_json
+      end
+
+      before do
+        stub_request(:post, 'http://localhost:3003/v1/objects')
+          .with(
+            body: '{"type":"http://cocina.sul.stanford.edu/models/document.jsonld",' \
+            '"label":"test parameters for registration","version":1,' \
+            '"administrative":{"hasAdminPolicy":"druid:hv992ry2431"},' \
+            '"identification":{"sourceId":"foo:bar","catalogLinks":[]},' \
+            '"structural":{"isMemberOf":"druid:hv992ry7777"}}'
+          )
+          .to_return(status: 200, body: json, headers: {})
+      end
+
       it 'registers the object' do
         post :create, params: submitted
         expect(response).to be_created
-        expect(objects_client).to have_received(:register).with(
-          params: Cocina::Models::RequestDRO
-        )
         expect(workflow_service).to have_received(:create_workflow_by_name)
-          .with('druid:abc', 'registrationWF', version: '1')
+          .with('druid:bc234fg5678', 'registrationWF', version: '1')
       end
     end
 
@@ -96,14 +111,35 @@ RSpec.describe Dor::ObjectsController, type: :controller do
         }
       end
 
+      let(:json) do
+        Cocina::Models::DRO.new(externalIdentifier: 'druid:bc234fg5678',
+                                type: Cocina::Models::Vocab.book,
+                                label: '',
+                                version: 1,
+                                access: {
+                                  access: 'location-based'
+                                }).to_json
+      end
+
+      before do
+        stub_request(:post, 'http://localhost:3003/v1/objects')
+          .with(
+            body: '{"type":"http://cocina.sul.stanford.edu/models/book.jsonld",' \
+            '"label":"test parameters for registration","version":1,' \
+            '"access":{"access":"location-based","download":"none","readLocation":"music"},' \
+            '"administrative":{"hasAdminPolicy":"druid:hv992ry2431"},' \
+            '"identification":{"sourceId":"foo:bar","catalogLinks":[]},' \
+            '"structural":{"hasMemberOrders":[{"viewingDirection":"left-to-right"}],' \
+            '"isMemberOf":"druid:hv992ry7777"}}'
+          )
+          .to_return(status: 200, body: json, headers: {})
+      end
+
       it 'registers the object' do
         post :create, params: submitted
         expect(response).to be_created
-        expect(objects_client).to have_received(:register).with(
-          params: Cocina::Models::RequestDRO
-        )
         expect(workflow_service).to have_received(:create_workflow_by_name)
-          .with('druid:abc', 'registrationWF', version: '1')
+          .with('druid:bc234fg5678', 'registrationWF', version: '1')
       end
     end
 
