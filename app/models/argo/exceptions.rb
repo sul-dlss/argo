e9fe072951544e1d478227294d575051bb7b8165
Justin Coyne <jcoyne@justincoyne.com>
2020-05-14 07:46:09 -0500

    Remove Indexer backlog from header
    Ref https://github.com/sul-dlss/argo/issues/924#issuecomment-628336154

--- app/models/argo/exceptions.rb
+++ app/models/argo/exceptions.rb
@@ -2,11 +2,6 @@
 
 module Argo
   module Exceptions
-    class IndexQueueRequestFailed < StandardError
-    end
-    class IndexQueueInvalidResponse < StandardError
-    end
-
     # Raised if there is a problem communicating with dor_indexing_app
     class ReindexError < RuntimeError; end
   end
--- spec/support/mock_index_depth.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.configure do |config|
-  config.before(:example, type: :feature) do
-    # This stubs out the call that IndexQueue makes
-    stub_request(:get, 'https://dor-indexing-app:3000/dor/queue_size.json')
-      .to_return(status: 200, body: '1', headers: {})
-  end
-end
