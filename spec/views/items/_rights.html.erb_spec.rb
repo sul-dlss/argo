a03bb9eba5af26948c64cd49d30ee186c480cb6b
aaron-collier <aaron.collier@stanford.edu>
2020-05-27 09:47:49 -0700

    Add rights_list to view spec

--- spec/views/items/_rights.html.erb_spec.rb
+++ spec/views/items/_rights.html.erb_spec.rb
@@ -5,6 +5,7 @@ require 'rails_helper'
 RSpec.describe 'items/_rights.html.erb' do
   before do
     @object = instance_double(Dor::Item, pid: 'druid:abc123')
+    @rights_list = Constants::REGISTRATION_RIGHTS_OPTIONS
     @form = instance_double(AccessForm, model_name: instance_double(ActiveModel::Name, param_key: 'pid'))
     render
   end
