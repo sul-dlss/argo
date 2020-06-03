c9eb5ba332af0f4df5f895548f1d1583ffe8e0e7
aaron-collier <aaron.collier@stanford.edu>
2020-06-03 15:04:04 -0700

    Update test for datastream modal

--- spec/views/catalog/ds.html.erb_spec.rb
+++ spec/views/catalog/ds.html.erb_spec.rb
@@ -3,19 +3,35 @@
 require 'rails_helper'
 
 RSpec.describe 'catalog/ds.html.erb' do
-  let(:dsid) { 'identityMetadata' }
-  let(:stub_ds) { instance_double(Dor::IdentityMetadataDS, content: '<xml />') }
+  let(:stub_ds) { instance_double(Dor::IdentityMetadataDS, content: '<xml />', ng_xml: true) }
 
   before do
     params[:dsid] = dsid
+    params[:id] = 'druid:abc123'
     allow(view).to receive(:can?).and_return(true)
     allow(view).to receive(:render_ds_profile_header)
     @obj = instance_double(Dor::Item, datastreams: { dsid => stub_ds })
   end
 
-  it 'renders the modal' do
-    render
-    expect(rendered).to have_css '.modal-header h3.modal-title'
-    expect(rendered).to have_css '.modal-body', text: '<xml/>'
+  context 'with an editable datastream' do
+    let(:dsid) { 'identityMetadata' }
+
+    it 'renders the modal with edit link' do
+      render
+      expect(rendered).to have_css '.modal-header h3.modal-title'
+      expect(rendered).to have_css '.modal-body'
+      expect(rendered).to include 'Edit identityMetadata'
+    end
+  end
+
+  context 'with an non-editable datastream' do
+    let(:dsid) { 'fakeMetadata' }
+
+    it 'renders the modal without the edit link' do
+      render
+      expect(rendered).to have_css '.modal-header h3.modal-title'
+      expect(rendered).to have_css '.modal-body'
+      expect(rendered).not_to include 'Edit fakeMetadata'
+    end
   end
 end
