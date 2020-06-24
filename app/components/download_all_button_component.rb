41c4b087879b88de4f290754e92442cc344441f3
Justin Coyne <jcoyne@justincoyne.com>
2020-06-24 14:20:59 -0500

    Move the Download All button to the right
    Fixes #2134

---
+++ app/components/download_all_button_component.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class DownloadAllButtonComponent < ViewComponent::Base
+  # @param [SolrDocument] document
+  def initialize(document:)
+    @document = document
+  end
+
+  def link_options
+    # This onclick handler prevents the accordion which contains this element from folding.
+    options = { onclick: 'event.stopPropagation()' }
+    return options if !document.preservation_size || document.preservation_size < 1_000_000_000
+
+    options.merge(data: { confirm: 'This will be a large download. Are you sure?' })
+  end
+
+  attr_reader :document
+end
---
+++ spec/components/download_all_button_component_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe DownloadAllButtonComponent, type: :component do
+  include Rails.application.routes.url_helpers
+  subject { page }
+
+  let(:document) { instance_double(SolrDocument, preservation_size: 0) }
+  let(:component) { described_class.new(document: document) }
+
+  before do
+    render_inline(component)
+  end
+
+  it { is_expected.to have_link 'Download all files', href: download_item_files_path(document) }
+  it { is_expected.to have_selector 'a[onclick="event.stopPropagation()"]' }
+
+  context 'with a large file' do
+    let(:document) { instance_double(SolrDocument, preservation_size: 1_000_000_000) }
+
+    it { is_expected.to have_selector 'a[data-confirm="This will be a large download. Are you sure?"]' }
+  end
+end
