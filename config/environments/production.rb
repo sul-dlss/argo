955c1f679934e2d82567109a7c54120254f33137
Justin Coyne <jcoyne@justincoyne.com>
2020-06-26 14:42:03 -0500

    Setup staging to use sidekiq

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   config.active_job.queue_adapter = :sidekiq
-  config.active_job.queue_name_prefix = 'foo_production'
+  # config.active_job.queue_name_prefix = 'argo_production'
 
   config.action_mailer.perform_caching = false
 
