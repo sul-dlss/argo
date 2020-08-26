cb385e2ee21edcfe48f4a88f4258388b769e6259
Justin Coyne <jcoyne@justincoyne.com>
2020-08-26 08:21:35 -0500

    Add documentation of which forms have the None case

--- app/services/cocina_access.rb
+++ app/services/cocina_access.rb
@@ -6,6 +6,7 @@ class CocinaAccess
   # @param [String] the rights representation from the form (must be one of the keys in Dor::RightsMetadataDS::RIGHTS_TYPE_CODES, or 'default')
   # @return [Maybe<Hash<Symbol,String>>] a hash representing a subset of the Access subschema of the Cocina model
   def self.from_form_value(rights)
+    # Default only appears on the registration form, not the update form.
     return None() if rights == 'default'
 
     data = if rights.end_with?('-nd') || %w[dark citation-only].include?(rights)
