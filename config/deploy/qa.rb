405cf396e857aa5cd769cdbd6d0ff74bd1135b62
Michael J. Giarlo <mjgiarlo@stanford.edu>
2020-09-17 15:45:47 -0700

    Allow capistrano to manage sidekiq workers in QA

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-server 'argo-qa-a.stanford.edu', user: 'lyberadmin', roles: %w[web db app]
-server 'argo-qa-b.stanford.edu', user: 'lyberadmin', roles: %w[web db app]
+server 'argo-qa-a.stanford.edu', user: 'lyberadmin', roles: %w[web db app worker]
+server 'argo-qa-b.stanford.edu', user: 'lyberadmin', roles: %w[web db app worker]
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
