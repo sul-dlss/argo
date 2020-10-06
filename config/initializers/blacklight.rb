95c9bacc3e506c33cef15075e6d30d62d42aaa61
Justin Coyne <jcoyne@justincoyne.com>
2020-10-06 12:41:46 -0500

    Upgrade to Blacklight 7.11

--- config/initializers/blacklight.rb
+++ config/initializers/blacklight.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# Change the order of the load paths so that Blacklight::Document gets loaded first.
+# See https://github.com/projectblacklight/blacklight/issues/2346
+Blacklight::Engine.config.eager_load_paths = [File.join(Blacklight.root, 'app', 'models', 'concerns')] + Blacklight::Engine.config.eager_load_paths
 ##
 # Configure Blacklight to use POST as the default HTTP method
 Blacklight::Configuration.default_values[:http_method] = :post
