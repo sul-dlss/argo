b935600f38c104ca0fa226898a18620bc4dfea2b
Justin Coyne <jcoyne@justincoyne.com>
2021-01-07 15:19:23 -0600

    There is no reason to override the thumbnail presenter
    This functionality is now in blacklight

--- app/presenters/argo_show_presenter.rb
+++ app/presenters/argo_show_presenter.rb
@@ -1,18 +1,11 @@
 # frozen_string_literal: true
 
 class ArgoShowPresenter < Blacklight::ShowPresenter
-  class_attribute :thumbnail_presenter
-  self.thumbnail_presenter = Blacklight::ThumbnailPresenter
-
   ##
   # Override the default Blacklight presenter method, to provide citation when
   # rendering the document_heading. Used in show page `render_document_heading`
   # @see https://github.com/projectblacklight/blacklight/blob/c04e80b690bdbd71482d3d91cc168d194d0b6a51/app/presenters/blacklight/document_presenter.rb#L22
   def heading
     CitationPresenter.new(@document).render
-  end
-
-  def thumbnail
-    @thumbnail ||= thumbnail_presenter.new(document, view_context, view_config)
   end
 end
