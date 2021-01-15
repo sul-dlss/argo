704a4ff55ef527872ce59fba47ba5efe18e11ac3
Justin Littman <justinlittman@stanford.edu>
2021-01-15 08:54:45 -0500

    Support removing catKeys with manage catKey bulk action.
    closes #2358

--- app/services/catkey_service.rb
+++ app/services/catkey_service.rb
@@ -3,9 +3,9 @@
 # Updates the catkey on an object by calling the dor-services-app api
 class CatkeyService
   def self.update(cocina_model, object_client, new_catkey)
-    updated_identification = cocina_model.identification.new(
-      catalogLinks: [{ catalog: 'symphony', catalogRecordId: new_catkey }]
-    )
+    catalog_links = []
+    catalog_links << { catalog: 'symphony', catalogRecordId: new_catkey } unless new_catkey.nil?
+    updated_identification = cocina_model.identification.new(catalogLinks: catalog_links)
     updated = cocina_model.new(identification: updated_identification)
     object_client.update(params: updated)
   end
