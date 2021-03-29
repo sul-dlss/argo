4bd030891ee5d844ddb7c26e5c1f0b7167cf2813
Justin Coyne <jcoyne@justincoyne.com>
2021-03-29 00:13:31 -0500

    Update factories to use the image where dor-services-app does the bootstrapping
    See https://github.com/sul-dlss/dor-services-app/pull/2616

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -22,7 +22,7 @@ FactoryBot.define do
       Dor::Services::Client.objects.register(params: builder.cocina_model)
     end
 
-    admin_policy_id { FactoryBot.create_for_repository(:ur_apo).pid }
+    admin_policy_id { FactoryBot.create_for_repository(:ur_apo).id }
     label { 'test object' }
     type { Cocina::Models::Vocab.object }
 
