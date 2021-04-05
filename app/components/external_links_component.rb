b54898de7930dc288aa446643f146266d9f2fd2f
Justin Coyne <jcoyne@justincoyne.com>
2021-04-05 16:14:33 -0500

    [refactor] rename safeurl to fedora_url_without_credentials

--- app/components/external_links_component.rb
+++ app/components/external_links_component.rb
@@ -13,7 +13,7 @@ class ExternalLinksComponent < ViewComponent::Base
   end
 
   def dor_link
-    link_to 'Fedora UI', File.join(safeurl, "objects/#{document.id}"),
+    link_to 'Fedora UI', File.join(fedora_url_without_credentials, "objects/#{document.id}"),
             target: '_blank', rel: 'noopener', class: 'nav-link'
   end
 
@@ -24,7 +24,7 @@ class ExternalLinksComponent < ViewComponent::Base
   end
 
   def foxml_link
-    url = File.join(safeurl, "objects/#{document.id}/export?context=archive")
+    url = File.join(fedora_url_without_credentials, "objects/#{document.id}/export?context=archive")
     link_to 'FoXML', url, target: '_blank', rel: 'noopener', class: 'nav-link'
   end
 
@@ -45,7 +45,7 @@ class ExternalLinksComponent < ViewComponent::Base
 
   attr_reader :document
 
-  def safeurl
+  def fedora_url_without_credentials
     fedora_uri = URI.parse(Settings.fedora_url)
     fedora_uri.user = fedora_uri.password = nil
     fedora_uri.to_s
