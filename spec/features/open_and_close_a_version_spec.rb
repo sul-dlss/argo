19a1ec74f6cd729d006430ae8da879f2dd642184
Justin Coyne <jcoyne@justincoyne.com>
2021-04-14 12:11:16 -0500

    Add a feature test for open and close version

---
+++ spec/features/open_and_close_a_version_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Open and close a version' do
+  let(:item) do
+    FactoryBot.create_for_repository(:item)
+  end
+
+  let(:dsc) { Dor::Services::Client.object(item.externalIdentifier) }
+
+  before do
+    sign_in create(:user), groups: ['sdr:administrator-role']
+    dsc.accession.start(workflow: 'accessionWF')
+  end
+
+  it 'opens an object', js: true do
+    visit solr_document_path item.externalIdentifier
+    12.times do
+      # Ensure every step of accessionWF is completed, this will allow us to open a new version.
+      click_link 'accessionWF'
+      click_button 'Set to completed', match: :first
+    end
+
+    sleep 1 # Give time for a reindex to occur.
+    visit solr_document_path item.externalIdentifier
+    click_link 'Open for modification'
+    select 'Minor', from: 'Type'
+    fill_in 'Version description', with: 'Test a change'
+
+    click_button 'Open Version'
+    expect(page).to have_content "#{item.externalIdentifier} is open for modification!"
+
+    click_link 'Close Version'
+    expect(page).to have_content 'Test a change'
+
+    click_button 'Close Version'
+
+    expect(page).to have_content "Version 2 of #{item.externalIdentifier} has been closed!"
+  end
+end
