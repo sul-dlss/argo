ca828ec444fe836336fc1720dd61fe416911a892
Justin Coyne <jcoyne@justincoyne.com>
2021-04-19 14:17:25 -0500

    Add a mechanism for registering an agreement

---
+++ app/forms/agreement_form.rb
@@ -0,0 +1,40 @@
+class AgreementForm < Reform::Form
+  property :title
+  property :source_id
+
+  validates :source_id, format: { with: /\A\w+:\w+\z/,
+    message: "must have a colon" }
+
+  def persisted?
+    false
+  end
+
+  def to_key
+    []
+  end
+
+  def setup_properties!(cocina_item)
+    # Since this is only for create, this is a no-op
+  end
+
+  def sync!(props)
+    @model = new_resource(title: title, source_id: source_id)
+  end
+
+  def save_model
+    @model = Dor::Services::Client.objects.register(params: model)
+  end
+
+  def new_resource(title:, source_id:)
+    Cocina::Models.build_request(
+      'type' => Cocina::Models::Vocab.agreement,
+      'label' => title,
+      'version' => 1,
+      'access' => { 'access' => 'dark' },
+      'description' => { 'title' => [{ 'value' => title }] },
+      'administrative' => { 'hasAdminPolicy' => ApoConcern::UBER_APO_ID },
+      'identification' => { 'sourceId' => source_id },
+      'structural' => {}
+    )
+  end
+end
