sul-dlss/argo

commit 9ec63b6fae8df19413c55f12881f3bb659ed9c96

Group bulk actions into sections

app/views/bulk_actions/forms/_manage_release_form.html.erb
@@ -1,5 +1,5 @@
 <div>
-  <%= f.fields_for :manage_release do |manage_release_fields| %>
+  <% f.fields_for :manage_release do |manage_release_fields| %>
     <div class='form-group'>
       <div class='radio'>
         <label class='checkbox-inline'>

app/components/bulk_actions_form_component.html.erb
@@ -1,0 +1,158 @@
+<div class='container'>
+  <h1>New Bulk Action</h1>
+
+  <%= form_for(@form, html: { 'data-controller' => 'bulk_actions' }) do |f| %>
+    <% if @form.errors.any? %>
+      <div id="error_explanation">
+        <h2><%= pluralize(@form.errors.count, 'error') %> prohibited this bulk_action from being saved:</h2>
+
+        <ul>
+        <% @form.errors.full_messages.each do |message| %>
+          <li><%= message %></li>
+        <% end %>
+        </ul>
+      </div>
+    <% end %>
+
+    <div class='form-group'>
+        <%= f.select(:action_type, action_types,
+                     {},
+                     class: 'form-control',
+                     data: { action: 'change->bulk_actions#showTab' }) %>
+
+      <div class='tab-content'>
+        <div role='tabpanel' class='tab-pane' id='DescmetadataDownloadJob'>
+          <span class='help-block'>
+            Download descriptive metadata for objects.
+          </span>
+        </div>
+
+        <div role='tabpanel' class='tab-pane active' id='ReleaseObjectJob'>
+          <span class='help-block'>
+            Adds release tags to individual objects.
+          </span>
+          <%= render 'bulk_actions/forms/manage_release_form', f: f %>
+        </div>
+
+        <div role='tabpanel' class='tab-pane' id='SetGoverningApoJob'>
+          <span class='help-block'>
+            Moves the object to a new governing APO.
+          </span>
+          <%= render 'bulk_actions/forms/set_governing_apo_form', f: f %>
+        </div>
+
+        <div role='tabpanel' class='tab-pane' id='RemoteIndexingJob'>
+          <span class='help-block'>
+            Reindexes the DOR object to Solr
+          </span>
+        </div>
+
+        <div role='tabpanel' class='tab-pane' id='PurgeJob'>
+          <span class='help-block'>
+            Deletes unpublished DOR objects
+          </span>
+        </div>
+
+        <div role='tabpanel' class='tab-pane' id='SetCatkeysAndBarcodesJob'>
+          <span class='help-block'>
+            Adds or updates catkeys and/or barcodes associated with objects.
+          </span>
+          <p>You must have a druid list and a catkey and/or a barcode list. Lists of catkeys/barcodes (if provided) must be the same length as the list of druids and in the same order. Use a blank line to indicate that a catkey/barcode should be deleted.</p>
+          <%= render 'bulk_actions/forms/set_catkeys_and_barcodes_form', f: f %>
+        </div>
+
+        <div role='tabpanel' class='tab-pane' id='SetCatkeysAndBarcodesCsvJob'>
+          <span class='help-block'>
+            Adds or updates catkeys and/or barcodes associated with objects.
+          </span>
+          <%= render 'bulk_actions/forms/set_catkeys_and_barcodes_csv_form', f: f %>
+        </div>
+
+        <div role='tabpanel' class='tab-pane' id='PrepareJob'>
+          <span class='help-block'>
+            Open items not yet open for versioning.
+          </span>
+          <%= render 'bulk_actions/forms/prepare_form', f: f %>
+        </div>
+
+        <div role='tabpanel' class='tab-pane' id='RepublishJob'>
+          <span class='help-block'>
+            Republish DOR objects. You still need to use the normal versioning process to make sure your changes are preserved.
+          </span>
+        </div>
+
+        <div role='tabpanel' class='tab-pane' id='CloseVersionJob'>
+          <span class='help-block'>
+            Close a version of the items so the changes can be accessioned. The items will retain the version type (Major, Minor, or Admin) and version description as entered when the item was opened for versioning.
+          </span>
+        </div>
+
+        <div role='tabpanel' class='tab-pane' id='ChecksumReportJob'>
+          <span class='help-block'>
+            Download checksums of files in objects (as csv).
+          </span>
+        </div>
+
+        <div role='tabpanel' class='tab-pane' id='CreateVirtualObjectsJob'>
+          <div class="form-group">
+            <span class='help-block'>
+              Create one or more virtual objects.
+            </span>
+          </div>
+          <%= render 'bulk_actions/forms/create_virtual_objects_form', f: f %>
+        </div>
+
+        <div role='tabpanel' class='tab-pane' id='ExportTagsJob'>
+          <span class='help-block'>
+            Download tags as CSV (comma-separated values) for druids specified below
+          </span>
+        </div>
+
+        <div role='tabpanel' class='tab-pane' id='ImportTagsJob'>
+          <span class='help-block'>
+            Upload tags as CSV (comma-separated values)
+          </span>
+          <%= render 'bulk_actions/forms/import_tags_form', f: f %>
+        </div>
+
+        <div role='tabpanel' class='tab-pane' id='RegisterDruidsJob'>
+          <div class="form-group">
+            <span class='help-block'>
+              Register druids
+            </span>
+          </div>
+          <%= render 'bulk_actions/forms/register_druids_form', f: f %>
+        </div>
+
+        <div role='tabpanel' class='tab-pane' id='SetLicenseAndRightsStatementsJob'>
+          <div class="form-group">
+            <span class='help-block'>
+              Edit license, copyright statement, and/or use &amp; reproduction statements
+            </span>
+          </div>
+          <%= render 'bulk_actions/forms/set_license_and_rights_statements_form', f: f %>
+        </div>
+      </div>
+    </div>
+
+    <div data-bulk_actions-target="commonFields" >
+      <button class='btn btn-primary' data-populate-druids="<%= search_catalog_path(search_of_pids) %>" data-target='#pids'>
+        Populate with previous search
+      </button>
+      <div class='form-group'>
+        <label>Druids to perform bulk action on</label>
+        <textarea id='pids' name='bulk_action[pids]' class='form-control' rows='10'></textarea>
+      </div>
+    </div>
+
+    <div class='form-group'>
+      <%= f.label :description, 'Describe this bulk action' %>
+      <%= f.text_area :description, class: 'form-control' %>
+    </div>
+    <div class=''>
+      <button type='submit' class='btn btn-primary'>Submit</button>
+    </div>
+  <% end %>
+
+  <%= link_to 'Back', bulk_actions_path %>
+</div>

app/components/bulk_actions_form_component.rb
@@ -1,0 +1,49 @@
+# frozen_string_literal: true
+
+class BulkActionsFormComponent < ApplicationComponent
+  def initialize(form:, last_search:)
+    @form = form
+    @last_search = last_search
+  end
+
+  ##
+  # Add a pids_only=true parameter to create a "search of pids" to an existing
+  # Blacklight::Search
+  # This is used by the "Populate with previous search" feature of bulk actions
+  # @param [Blacklight::Search, nil]
+  # @return [Hash]
+  def search_of_pids
+    return '' if @last_search.blank?
+
+    @last_search.query_params.merge('pids_only' => true)
+  end
+
+  def action_types
+    grouped_options_for_select [
+      ['Perform actions', [
+        ['Manage release', 'ReleaseObjectJob'],
+        %w[Reindex RemoteIndexingJob],
+        ['Republish objects', 'RepublishJob'],
+        ['Purge object', 'PurgeJob']
+      ]],
+      ['Modify objects (via form)', [
+        ['Open new object versions', 'PrepareJob'],
+        ['Close versions', 'CloseVersionJob'],
+        ['Update governing APO', 'SetGoverningApoJob'],
+        ['Edit Catkeys and Barcodes', 'SetCatkeysAndBarcodesJob'],
+        ['Edit License and Rights Statements', 'SetLicenseAndRightsStatementsJob']
+      ]],
+      ['Modify objects (via CSV)', [
+        ['Create virtual object(s)', 'CreateVirtualObjectsJob'],
+        ['Edit Catkeys and Barcodes from CSV', 'SetCatkeysAndBarcodesCsvJob'],
+        ['Export tags to CSV', 'ExportTagsJob'],
+        ['Import tags from CSV', 'ImportTagsJob'],
+        ['Register Druids', 'RegisterDruidsJob']
+      ]],
+      ['Generate reports', [
+        ['Download descriptive metadata', 'DescmetadataDownloadJob'],
+        ['Checksum report', 'ChecksumReportJob']
+      ]]
+    ]
+  end
+end

app/helpers/bulk_action_helper.rb
@@ -8,16 +8,4 @@ module BulkActionHelper
   rescue ActionView::MissingTemplate
     render partial: 'default'
   end
-
-  ##
-  # Add a pids_only=true parameter to create a "search of pids" to an existing
-  # Blacklight::Search
-  # This is used by the "Populate with previous search" feature of bulk actions
-  # @param [Blacklight::Search, nil]
-  # @return [Hash]
-  def search_of_pids(search)
-    return '' unless search.present?
-
-    search.query_params.merge('pids_only' => true)
-  end
 end

app/views/bulk_actions/forms/_create_virtual_objects_form.html.erb
@@ -1,4 +1,4 @@
-<%= f.fields_for :create_virtual_objects do |create_virtual_objects_fields| %>
+<% f.fields_for :create_virtual_objects do |create_virtual_objects_fields| %>
   <div class='form-group'>
     <%= create_virtual_objects_fields.label :csv_file, 'Upload a CSV file' %>
       <small><em>Please note that each row should contain the parent druid as the first field and child druids as all the other fields. Child druids <strong>must</strong> be entered in the order they should appear in the catalog.</em></small>

app/views/bulk_actions/forms/_import_tags_form.html.erb
@@ -1,4 +1,4 @@
-<%= f.fields_for :import_tags do |import_tags_fields| %>
+<% f.fields_for :import_tags do |import_tags_fields| %>
   <div class='form-group' id="import-csv">
     <%= import_tags_fields.label :csv_file, 'Upload a CSV file' %>
       <br>

app/views/bulk_actions/forms/_prepare_form.html.erb
@@ -1,4 +1,4 @@
-<%= f.fields_for :prepare do |prepare_fields| %>
+<% f.fields_for :prepare do |prepare_fields| %>
   <div class='form-group'>
     <%= prepare_fields.label :significance, 'Change type' %>
     <%= prepare_fields.select :significance,

app/views/bulk_actions/forms/_register_druids_form.html.erb
@@ -1,4 +1,4 @@
-<%= f.fields_for :register_druids do |register_druids_fields| %>
+<% f.fields_for :register_druids do |register_druids_fields| %>
   <div class='form-group'>
     <%= register_druids_fields.label :csv_file, 'Upload a CSV file' %>
       <p><small><em>Please note that each row should have headers for:

commit b06a2398c4055d21c8cec1365415e8b8a65bf7bf

Ensure release options display on the manage release form
This fixes a regression introduced in https://github.com/sul-dlss/argo/commit/9ec63b6fae8df19413c55f12881f3bb659ed9c96 where an erb was rendered both from within and without a component and the component way doesn't support capture blocks

app/components/manage_release/collection_form_component.html.erb
@@ -1,4 +1,4 @@
-<%= f.fields_for :manage_release do |manage_release_fields| %>
+<%= form.fields_for :manage_release do |manage_release_fields| %>
   <div class='form-group'>
     <label>What should be released/withdrawn?</label>
     <div class='radio'>

app/components/manage_release/collection_form_component.rb
@@ -1,0 +1,12 @@
+# frozen_string_literal: true
+
+module ManageRelease
+  class CollectionFormComponent < ApplicationComponent
+    def initialize(form:, current_user:)
+      @form = form
+      @current_user = current_user
+    end
+
+    attr_reader :form, :current_user
+  end
+end

app/components/manage_release/form_component.html.erb
@@ -8,7 +8,7 @@
   <div class='form-group'>
     <label>Released for:</label> <%= @document.released_to.to_sentence %>
   </div>
-  <%= render "#{@document.object_type}_manage_release_bulk_action_form", f: f %>
+  <%= child_form(f) %>
   <div class='form-group'>
     <button type='submit' class='btn btn-primary'>Submit</button>
   </div>

app/components/manage_release/form_component.rb
@@ -1,0 +1,19 @@
+# frozen_string_literal: true
+
+module ManageRelease
+  class FormComponent < ApplicationComponent
+    def initialize(bulk_action:, document:)
+      @bulk_action = bulk_action
+      @document = document
+    end
+
+    def child_form(form)
+      case @document.object_type
+      when 'collection'
+        render CollectionFormComponent.new(form: form, current_user: helpers.current_user)
+      else
+        render ItemFormComponent.new(form: form, current_user: helpers.current_user)
+      end
+    end
+  end
+end

app/components/manage_release/item_form_component.html.erb
@@ -1,0 +1,1 @@
+<%= render 'bulk_actions/forms/manage_release_form', f: form, current_user: current_user %>

app/components/manage_release/item_form_component.rb
@@ -1,0 +1,12 @@
+# frozen_string_literal: true
+
+module ManageRelease
+  class ItemFormComponent < ApplicationComponent
+    def initialize(form:, current_user:)
+      @form = form
+      @current_user = current_user
+    end
+
+    attr_reader :form, :current_user
+  end
+end

app/views/bulk_actions/forms/_manage_release_form.html.erb
@@ -1,23 +1,21 @@
-<div>
-  <% f.fields_for :manage_release do |manage_release_fields| %>
-    <div class='form-group'>
-      <div class='radio'>
-        <label class='checkbox-inline'>
-          <%= manage_release_fields.radio_button(:tag, true, checked: true) %>
-          Release this object*
-        </label>
-        <label class='checkbox-inline'>
-          <%= manage_release_fields.radio_button(:tag, false) %>
-          Do not release this object*
-        </label>
-      </div>
+<% f.fields_for :manage_release do |manage_release_fields| %>
+  <div class='form-group'>
+    <div class='radio'>
+      <label class='checkbox-inline'>
+        <%= manage_release_fields.radio_button(:tag, true, checked: true) %>
+        Release this object*
+      </label>
+      <label class='checkbox-inline'>
+        <%= manage_release_fields.radio_button(:tag, false) %>
+        Do not release this object*
+      </label>
     </div>
-    <div class='form-group'>
-      <label for='bulk_action_manage_release_to'>to</label>
-      <%= manage_release_fields.select :to, Constants::RELEASE_TARGETS, {}, class: 'form-control' %>
-    </div>
-    <%= manage_release_fields.hidden_field('what', value: 'self') %>
-    <%= manage_release_fields.hidden_field('who', value: current_user.sunetid) %>
-  <% end %>
-</div>
+  </div>
+  <div class='form-group'>
+    <label for='bulk_action_manage_release_to'>to</label>
+    <%= manage_release_fields.select :to, Constants::RELEASE_TARGETS, {}, class: 'form-control' %>
+  </div>
+  <%= manage_release_fields.hidden_field('what', value: 'self') %>
+  <%= manage_release_fields.hidden_field('who', value: current_user.sunetid) %>
+<% end %>
 <span class='help-block'>* for items this overrides any collection release instructions; for a collection this releases the collection object itself, not its members</span>

app/views/manage_releases/_item_manage_release_bulk_action_form.html.erb
@@ -1,1 +1,0 @@
-<%= render 'bulk_actions/forms/manage_release_form', f: f %>

app/views/manage_releases/show.html.erb
@@ -1,4 +1,6 @@
 <%= render BlacklightModalComponent.new do |component| %>
   <% component.header { 'Manage release' } %>
-  <% component.body { render 'show' } %>
+  <% component.body {
+    render ManageRelease::FormComponent.new(bulk_action: @bulk_action, document: @document)
+  } %>
 <% end %>

spec/components/manage_release/collection_form_component_spec.rb
@@ -1,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ManageRelease::CollectionFormComponent, type: :component do
+  subject(:component) { described_class.new(form: form, current_user: build(:user)) }
+
+  let(:form) { ActionView::Helpers::FormBuilder.new(nil, nil, controller.view_context, {}) }
+  let(:rendered) { render_inline(component) }
+
+  it 'renders the options' do
+    expect(rendered.css('label').to_html).to include(
+      'This collection and all its members*',
+      'Only this collection description but not any of its members',
+      'Release it',
+      'Do not release it (withdraw)'
+    )
+  end
+end

spec/components/manage_release/form_component_spec.rb
@@ -1,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ManageRelease::FormComponent, type: :component do
+  subject(:component) { described_class.new(bulk_action: bulk_action, document: document) }
+
+  let(:document) { SolrDocument.new(id: 'druid:123', objectType_ssim: 'collection') }
+  let(:bulk_action) { BulkAction.new }
+  let(:rendered) { render_inline(component) }
+
+  before do
+    allow(controller).to receive_messages(
+      current_user: build(:user)
+    )
+  end
+
+  it 'renders the form' do
+    expect(rendered.to_html).to include(
+      'Manage release to discovery applications for collection druid:123'
+    )
+
+    expect(rendered.css('button').inner_html).to eq 'Submit'
+  end
+end

spec/components/manage_release/item_form_component_spec.rb
@@ -1,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ManageRelease::ItemFormComponent, type: :component do
+  subject(:component) { described_class.new(form: form, current_user: build(:user)) }
+
+  let(:form) { ActionView::Helpers::FormBuilder.new(nil, nil, controller.view_context, {}) }
+  let(:rendered) { render_inline(component) }
+
+  it 'renders the options' do
+    expect(rendered.css('label').to_html).to include(
+      'Release this object*',
+      'Do not release this object*'
+    )
+
+    expect(rendered.css('select option').to_html).to include(
+      'Searchworks',
+      'Earthworks'
+    )
+  end
+end