f33e01ded46f7596933d4f79fc750b8f6e51c79e
Justin Littman <justinlittman@stanford.edu>
2021-06-29 12:57:56 -0400

    Fix

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
+# Make Zeitwerks happy.
+Rails.autoloaders.main.ignore(Rails.root.join('app/packs'))
+
 require_relative 'boot'
 
 require 'rails/all'
-
-# Make Zeitwerks happy.
-Rails.autoloaders.main.ignore(Rails.root.join('app/packs'))
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
