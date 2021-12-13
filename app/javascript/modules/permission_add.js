c8f0fdda20835e0b0637b48ed83257fe6b3311ba
Justin Coyne <jcoyne@justincoyne.com>
2021-12-13 13:45:14 -0600

    Update APO registration form to use bootstrap 5 form layout

--- app/javascript/modules/permission_add.js
+++ app/javascript/modules/permission_add.js
@@ -18,19 +18,22 @@ export default class {
       newEl.innerHTML = '<legend class="col-sm-3">Add group<legend>'
 
       var div = document.createElement('div')
-      div.className = "col-sm-9 form-inline"
-      div.innerHTML = '<input id="permissionName" class="form-control" placeholder="Group name"/>' +
-          '<select id="permissionRole" class="form-control"><option value="manage">Manage</option><option value="view">View</option></select>'
+      div.className = "col-sm-9 row"
+      div.innerHTML = `<div class="col-lg-6"><input id="permissionName" class="form-control" placeholder="Group name"></div>
+          <div class="col-lg-5">
+            <select id="permissionRole" class="form-control"><option value="manage">Manage</option><option value="view">View</option></select>
+          </div>
+          <div class="col-lg-1">
+            <button class="btn btn-secondary">Add</button>
+          </div>
+          `
 
-      var button = document.createElement('button')
-      button.innerHTML = 'Add'
-      button.className = 'btn btn-secondary'
+      var button = div.querySelector('button')
       button.addEventListener('click', (event) => {
           event.preventDefault()
           this.parent.add({name: document.getElementById('permissionName').value, type: "group", access: document.getElementById('permissionRole').value}, )
       })
 
-      div.appendChild(button)
       newEl.appendChild(div)
       return newEl
     }
