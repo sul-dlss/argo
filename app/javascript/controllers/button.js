a456c5a6b6c2a2b00db63973b30b79dba0d7b4fd
Justin Coyne <jcoyne@justincoyne.com>
2021-12-14 10:22:14 -0600

    Use the bootstrap 5 API for managing the modal

--- app/javascript/controllers/button.js
+++ app/javascript/controllers/button.js
@@ -4,7 +4,9 @@ export default class extends Controller {
   open(event) {
     event.preventDefault()
     const href = this.element.getAttribute('href')
-    $('#edit-modal').modal('show')
+    const element = document.getElementById('edit-modal')
+    const instance = bootstrap.Modal.getOrCreateInstance(element)
+    instance.show()
     const modal = document.querySelector('#edit-modal .modal-content')
     // Target is _top so that we can navigate to a new page when the form submission is successful
     modal.innerHTML = `<turbo-frame id="modal-frame" src="${href}" target="_top"></turbo-frame>`
