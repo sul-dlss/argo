sul-dlss/argo

commit 50914bfe579b5212e9a0a92a3aaa1ff0c2db2a0a

Start using the new show layout

app/helpers/argo_helper.rb
@@ -9,7 +9,7 @@ module ArgoHelper
   def container_classes
     return super if controller_name == 'apo'
 
-    'container-fluid'
+    action_name == 'show' ? 'container' : 'container-fluid'
   end
 
   def render_thumbnail_helper(doc, thumb_class = '', thumb_alt = '', thumb_style = 'max-width:240px;max-height:240px;')

app/assets/stylesheets/show.scss
@@ -1,13 +1,33 @@
-.argo-show-btn-group {
-  width: 65%;
+.show-document {
+  $button-width: 11rem;
 
-  .btn {
-    text-align: left;
-    padding: 0.3rem 1.3rem;
-    margin-bottom: 3px;
+  .argo-show-btn-group {
+    .btn {
+      width: $button-width;
+      margin-right: .5rem;
+      margin-bottom: .5rem;
+    }
   }
-}
 
-.full-view-links {
-  padding: 20px;
+  .full-view-links {
+    padding: 20px;
+  }
+
+
+  .button-header {
+    font-size: 12pt;
+    font-weight: bold;
+  }
+
+  .external-link-button {
+    margin-right: .5rem;
+    margin-bottom: .5rem;
+    width: $button-width;
+    @extend .btn;
+    @extend .btn-outline-primary;
+  }
+
+  .blacklight-item {
+    margin-top: 1rem;
+  }
 }

app/components/action_button.html.erb
@@ -1,1 +1,1 @@
-<%= link_to label, url, class: "btn button btn-primary #{'disabled' if disabled?}", data: button_data %>
+<%= link_to label, url, method: method, class: "btn button btn-primary #{'disabled' if disabled?}", data: button_data %>

app/components/external_links_component.html.erb
@@ -1,5 +1,8 @@
-<h3 class="start-open">View in new window</h3>
-<ul class='nav flex-column'>
+<h3 class="button-header">View in new window</h3>
+<ul class="nav flex">
+  <li class="nav-item">
+    <%= link_to 'MODS', descriptive_item_metadata_path(@document.id), class: 'external-link-button', data: { blacklight_modal: 'trigger' } %>
+  </li>
   <li class="nav-item"><%= purl_link %></li>
   <% if released_to_searchworks? %>
     <li class="nav-item"><%= searchworks_link %></li>
@@ -8,4 +11,7 @@
   <li class="nav-item"><%= foxml_link %></li>
   <li class="nav-item"><%= solr_link %></li>
   <li class="nav-item"><%= cocina_link %></li>
+  <li class="nav-item">
+    <%= link_to 'Dublin Core', full_dc_item_metadata_path(@document.id), title: 'Dublin Core (derived from MODS)', class: 'external-link-button', data: { blacklight_modal: 'trigger' } %>
+  </li>
 </ul>

app/components/external_links_component.rb
@@ -9,33 +9,33 @@ class ExternalLinksComponent < ViewComponent::Base
 
   def purl_link
     link_to 'PURL', File.join(Settings.purl_url, document.druid),
-            target: '_blank', rel: 'noopener', class: 'nav-link'
+            target: '_blank', rel: 'noopener', class: 'external-link-button'
   end
 
   def dor_link
     link_to 'Fedora UI', File.join(fedora_url_without_credentials, "objects/#{document.id}"),
-            target: '_blank', rel: 'noopener', class: 'nav-link'
+            target: '_blank', rel: 'noopener', class: 'external-link-button'
   end
 
   def searchworks_link
     id = document.catkey.presence || document.druid
     url = Kernel.format(Settings.searchworks_url, id: id)
-    link_to 'SearchWorks', url, target: '_blank', rel: 'noopener', class: 'nav-link'
+    link_to 'SearchWorks', url, target: '_blank', rel: 'noopener', class: 'external-link-button'
   end
 
   def foxml_link
     url = File.join(fedora_url_without_credentials, "objects/#{document.id}/export?context=archive")
-    link_to 'FoXML', url, target: '_blank', rel: 'noopener', class: 'nav-link'
+    link_to 'FoXML', url, target: '_blank', rel: 'noopener', class: 'external-link-button'
   end
 
   def solr_link
     link_to 'Solr document', solr_document_path(document, format: :json),
-            target: '_blank', rel: 'noopener', class: 'nav-link'
+            target: '_blank', rel: 'noopener', class: 'external-link-button'
   end
 
   def cocina_link
     link_to 'Cocina model', item_path(document, format: :json),
-            target: '_blank', rel: 'noopener', class: 'nav-link'
+            target: '_blank', rel: 'noopener', class: 'external-link-button'
   end
 
   def released_to_searchworks?

app/components/sidebar_controls_component.html.erb
@@ -1,4 +1,5 @@
-<div class='btn-group-vertical argo-show-btn-group' role='group' aria-label='Argo action buttons'>
+<div class='argo-show-btn-group'>
+  <h3 class="button-header">Actions</h3>
   <%= close_button %>
   <%= open_button %>
 

app/views/catalog/_full_view_links_default.html.erb
@@ -1,4 +1,0 @@
-<div class='btn-group full-view-links' role='group'>
-  <%= link_to 'View full Dublin Core', full_dc_item_metadata_path(document.id), title: 'Dublin Core (derived from MODS)', class: 'btn btn-secondary btn-sm', data: { blacklight_modal: 'trigger' } %>
-  <%= link_to 'View MODS', descriptive_item_metadata_path(document.id), title: 'MODS View', class: 'btn btn-secondary btn-sm', data: { blacklight_modal: 'trigger' } %>
-</div>

app/views/catalog/_show_sidebar.html.erb
@@ -1,7 +1,0 @@
-<nav class="show_sidebar">
-  <%= render ExternalLinksComponent.new(document: @document) %>
-  <hr>
-  <%= render SidebarControlsComponent.new(solr_document: @document, manager: can?(:manage_item, @cocina)) %>
-</nav>
-
-<%= render 'catalog/show_embargo_sidebar', document: @document %>

commit 1f3e70e71d23d96d9f140937a67db72f3dba2d5f

Only the catalog controller show page should have a container class
All others (like registration) should remain container-fluid. This was caused by a regression in https://github.com/sul-dlss/argo/commit/50914bfe579b5212e9a0a92a3aaa1ff0c2db2a0a#diff-f7572e813105da402ff889a50f513c883352daa760f926b589f93cf93dc75eefR12

app/helpers/argo_helper.rb
@@ -9,7 +9,7 @@ module ArgoHelper
   def container_classes
     return super if controller_name == 'apo'
 
-    action_name == 'show' ? 'container' : 'container-fluid'
+    action_name == 'show' && controller_name == 'catalog' ? 'container' : 'container-fluid'
   end
 
   def render_thumbnail_helper(doc, thumb_class = '', thumb_alt = '', thumb_style = 'max-width:240px;max-height:240px;')