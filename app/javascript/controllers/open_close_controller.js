bcce0ab608d2db26970606b9fcc2e2f376ae1e0b
Justin Coyne <jcoyne@justincoyne.com>
2022-01-11 20:44:23 -0600

    The open/close widget opens a modal
    This fixes a regression introduced when the open_close_controller was created

--- app/javascript/controllers/open_close_controller.js
+++ app/javascript/controllers/open_close_controller.js
@@ -10,4 +10,16 @@ export default class extends Controller {
            this.element.hidden = value == 'false'
         })
     }
+
+    // TODO: This same code is in button.js Perhaps it can be extracted?
+    open(event) {
+      event.preventDefault()
+      const href = this.element.getAttribute('href')
+      const element = document.getElementById('edit-modal')
+      const instance = bootstrap.Modal.getOrCreateInstance(element)
+      instance.show()
+      const modal = document.querySelector('#edit-modal .modal-content')
+      // Target is _top so that we can navigate to a new page when the form submission is successful
+      modal.innerHTML = `<turbo-frame id="modal-frame" src="${href}" target="_top"></turbo-frame>`
+    }
 }
