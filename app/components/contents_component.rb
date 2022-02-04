70ad26115e043d13cda330bdfa74bba603a6ca5e
Peter Mangiafico <peter@mangiafico.org>
2022-02-04 15:27:43 -0800

    hide upload CSV button if the object is locked

--- app/components/contents_component.rb
+++ app/components/contents_component.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
 class ContentsComponent < ApplicationComponent
-  def initialize(cocina:, document:)
+  def initialize(cocina:, document:, state_service:)
     @cocina = cocina
     @document = document
+    @state_service = state_service
   end
 
   def render?
     @cocina.respond_to?(:structural)
   end
+
+  delegate :allows_modification?, to: :@state_service
 end
