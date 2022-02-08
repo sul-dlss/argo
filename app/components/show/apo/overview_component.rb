07d8a5ea0df1781c14b460ac594246f3785deecc
Justin Coyne <jcoyne@justincoyne.com>
2022-02-07 19:41:14 -0600

    Extract the overview and details components for admin policies

---
+++ app/components/show/apo/overview_component.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Show
+  module Apo
+    class OverviewComponent < ApplicationComponent
+      # @param [ArgoShowPresenter] presenter
+      def initialize(presenter:)
+        @presenter = presenter
+        @solr_document = presenter.document
+      end
+
+      delegate :id, :status, to: :@solr_document
+      delegate :state_service, to: :@presenter
+    end
+  end
+end
