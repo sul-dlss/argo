04e8d5dfa73946dac4f6b93c81ea0c52e6901251
Justin Coyne <jcoyne@justincoyne.com>
2022-02-07 21:32:51 -0600

    Try to fix flaky feature spec

--- spec/features/bulk_reindex_spec.rb
+++ spec/features/bulk_reindex_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe 'Bulk Reindex of DOR Objects' do
 
   before do
     sign_in current_user
+    allow(RemoteIndexingJob).to receive(:perform_later)
   end
 
   it 'Creates a new job' do
@@ -15,9 +16,8 @@ RSpec.describe 'Bulk Reindex of DOR Objects' do
     fill_in 'pids', with: 'druid:ab123gg7777'
     click_button 'Submit'
     expect(page).to have_css 'h1', text: 'Bulk Actions'
-    reload_page_until_timeout do
-      page.has_css?('td', text: 'RemoteIndexingJob') &&
-        page.has_css?('td', text: 'Processing')
-    end
+    page.has_css?('td', text: 'RemoteIndexingJob') &&
+      page.has_css?('td', text: 'Scheduled Action')
+    expect(RemoteIndexingJob).to have_received(:perform_later)
   end
 end
