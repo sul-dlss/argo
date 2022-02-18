effc97690a92875447d07303a9bf3acc8496dd54
Peter Mangiafico <peter@mangiafico.org>
2022-02-18 11:32:38 -0800

    added component spec for default object rights component

--- spec/components/show/apo/default_object_rights_component_spec.rb
+++ spec/components/show/apo/default_object_rights_component_spec.rb
@@ -3,13 +3,40 @@
 require 'rails_helper'
 
 RSpec.describe Show::Apo::DefaultObjectRightsComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:component) { described_class.new(presenter: presenter) }
+  let(:presenter) { instance_double(ArgoShowPresenter, document: doc, cocina: cocina) }
+  let(:cocina) do
+    Cocina::Models::AdminPolicy.new(externalIdentifier: 'druid:bb663yf7144',
+                                    type: Cocina::Models::Vocab.admin_policy,
+                                    label: 'An APO',
+                                    version: 1,
+                                    administrative: {
+                                      hasAdminPolicy: 'druid:hv992ry2431',
+                                      hasAgreement: 'druid:hp308wm0436',
+                                      defaultAccess: {
+                                        access: 'location-based',
+                                        download: 'location-based',
+                                        readLocation: 'spec',
+                                        useAndReproductionStatement: 'Use and reproduction statement.',
+                                        copyright: 'This is the copyright.',
+                                        license: 'A license goes here.'
+                                      }
+                                    })
+  end
+  let(:doc) do
+    SolrDocument.new('id' => 'druid:bb663yf7144',
+                     SolrDocument::FIELD_OBJECT_TYPE => 'adminPolicy',
+                     SolrDocument::FIELD_DEFAULT_ACCESS_RIGHTS => 'location - spec')
+  end
+  let(:rendered) { render_inline(component) }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  it 'shows the copyright, license, use statement and default access rights' do
+    # these come from the cocina model:
+    expect(rendered.to_html).to include 'Use and reproduction statement.'
+    expect(rendered.to_html).to include 'This is the copyright.'
+    expect(rendered.to_html).to include 'A license goes here.'
+
+    # this comes from the solr document:
+    expect(rendered.to_html).to include 'location - spec'
+  end
 end
