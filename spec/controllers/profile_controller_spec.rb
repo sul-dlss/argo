5be176ed85f4873f968489a2461834cbcff8f8aa
Justin Coyne <jcoyne@justincoyne.com>
2022-03-15 15:30:27 -0500

    Remove a unhelpful test
    Tests should describe behaviors, not the code itself

--- spec/controllers/profile_controller_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-RSpec.describe ProfileController do
-  it 'is a subclass of CatalogController' do
-    expect(described_class.superclass).to eq CatalogController
-  end
-end
