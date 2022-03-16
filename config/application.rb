950f7b48b827589c8109629d7f726169b88a2e10
Justin Coyne <jcoyne@justincoyne.com>
2022-03-16 18:22:53 -0500

    Flip application defaults to 7.0

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ Bundler.require(*Rails.groups)
 module Argo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 7.0
 
     require 'indexer'
     require 'constants'
