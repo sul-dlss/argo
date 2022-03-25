7d095aa9289de0eb3bfe22fb701990716fa1185b
Peter Mangiafico <peter@mangiafico.org>
2022-03-25 11:07:37 -0700

    rename bulk edit to bulk action (#3318)

--- spec/features/bulk_desc_metadata_download_spec.rb
+++ spec/features/bulk_desc_metadata_download_spec.rb
@@ -11,12 +11,25 @@ RSpec.describe 'Bulk Descriptive Metadata Download', js: true do
 
   it 'New page has a populate druids button and div with last search' do
     visit search_catalog_path q: 'stanford'
-    click_link 'Bulk Actions'
+    within '.search-widgets' do
+      click_link 'Bulk Actions'
+    end
     expect(page).to have_css 'h1', text: 'Bulk Actions'
     click_link 'New Bulk Action'
     expect(page).to have_css 'h1', text: 'New Bulk Action'
     expect(page).to have_button 'Populate with previous search'
     expect(page).to have_css 'div[data-bulk-actions-populate-url-value="/catalog?druids_only=true&q=stanford"]'
+  end
+
+  it 'New page does not have a populate druids button if no search params provided' do
+    visit root_path
+    within '.navbar-right' do
+      click_link 'Bulk Actions'
+    end
+    expect(page).to have_css 'h1', text: 'Bulk Actions'
+    click_link 'New Bulk Action'
+    expect(page).to have_css 'h1', text: 'New Bulk Action'
+    expect(page).not_to have_button 'Populate with previous search'
   end
 
   it 'Creates a new jobs' do
