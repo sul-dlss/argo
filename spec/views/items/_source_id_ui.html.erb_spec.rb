80e2cd7aaac25b7b373c46f79024a63eae24f021
Justin Coyne <jcoyne@justincoyne.com>
2022-04-06 13:08:14 -0500

    Use factories in view specs

--- spec/views/items/_source_id_ui.html.erb_spec.rb
+++ spec/views/items/_source_id_ui.html.erb_spec.rb
@@ -3,18 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe 'items/_source_id_ui' do
-  let(:identification) do
-    instance_double(Cocina::Models::Identification, sourceId: 'source id')
-  end
-
   before do
-    @cocina = instance_double(Cocina::Models::DRO, externalIdentifier: 'druid:987', identification: identification)
+    @cocina = Cocina::Models::Factories.build(:dro, source_id: 'sul:99999')
   end
 
   it 'renders the partial content' do
     render
     expect(rendered)
-      .to have_css 'form input.form-control[value="source id"]'
+      .to have_css 'form input.form-control[value="sul:99999"]'
     expect(rendered).to have_css 'p.form-text'
     expect(rendered).to have_css 'button.btn.btn-primary', text: 'Update'
   end
