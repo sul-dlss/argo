3a84baacb231b57611d628f97aab2fdeaf0d8931
Justin Coyne <jcoyne@justincoyne.com>
2022-04-07 09:40:23 -0500

    Memoize object loaded from DSA
    This avoids repeated unnecessary API calls

--- app/services/license_and_rights_statements_setter.rb
+++ app/services/license_and_rights_statements_setter.rb
@@ -90,7 +90,7 @@ class LicenseAndRightsStatementsSetter
   end
 
   def cocina_object
-    Dor::Services::Client.object(druid).find
+    @cocina_object ||= Dor::Services::Client.object(druid).find
   end
 
   def new_version_message
