e9a2a08b6c2bf2f33bf755f32aaa61e7a2f2d7cc
Peter Mangiafico <peter@mangiafico.org>
2022-04-13 12:37:15 -0700

    additional validation for the descriptive metadata CSV

--- app/services/description_validator.rb
+++ app/services/description_validator.rb
@@ -3,6 +3,7 @@
 # Validate the descriptive metadata spreadsheet
 class DescriptionValidator
   def initialize(csv)
+    @csv = csv
     @headers = csv.headers
     @errors = []
   end
@@ -13,6 +14,15 @@ class DescriptionValidator
     duplicate_headers.each do |header|
       errors << "Duplicate column headers: The header #{header} should occur only once."
     end
+    duplicate_druids.each do |druid|
+      errors << "Duplicate druids: The druid \"#{druid}\" should occur only once."
+    end
+    required_headers.each do |required_header|
+      errors << "#{required_header} column not found." unless @headers.include? required_header
+    end
+    @csv.each.with_index(2) do |row, i|
+      errors << "Missing druid: No druid present in row #{i}." if row['druid'].blank?
+    end
     errors.empty?
   end
 
@@ -20,5 +30,13 @@ class DescriptionValidator
 
   def duplicate_headers
     @headers.group_by { |e| e }.filter { |_k, v| v.count > 1 }.keys
+  end
+
+  def duplicate_druids
+    @csv.map { |row| row['druid'] }.group_by { |e| e }.filter { |_k, v| v.count > 1 }.keys
+  end
+
+  def required_headers
+    %w[druid]
   end
 end
--- spec/services/description_validator_spec.rb
+++ spec/services/description_validator_spec.rb
@@ -4,10 +4,10 @@ require 'rails_helper'
 
 RSpec.describe DescriptionValidator do
   describe '#valid?' do
-    let(:instance) { described_class.new(csv) }
+    let(:instance) { described_class.new(CSV.parse(csv, headers: true)) }
 
     context 'with duplicate columns' do
-      let(:csv) { CSV.parse('druid,title1:value,title2:value,title1:value,title2:value,title3:value', headers: true) }
+      let(:csv) { 'druid,title1:value,title2:value,title1:value,title2:value,title3:value' }
 
       it 'finds errors' do
         expect(instance.valid?).to be false
@@ -15,6 +15,47 @@ RSpec.describe DescriptionValidator do
           'Duplicate column headers: The header title1:value should occur only once.',
           'Duplicate column headers: The header title2:value should occur only once.'
         ]
+      end
+    end
+
+    context 'with missing druid header' do
+      let(:csv) { 'title1:value,title2:value,title3:value' }
+
+      it 'finds errors' do
+        expect(instance.valid?).to be false
+        expect(instance.errors).to eq ['druid column not found.']
+      end
+    end
+
+    context 'with missing druid in a row' do
+      let(:csv) do
+        <<~CSV
+          druid,title1:value,title2:value,title3:value
+          druid:ab123cd4567,cool,stuff,here
+          ,missing,druid,here
+          druid:cd456de5678,value,,
+        CSV
+      end
+
+      it 'finds errors' do
+        expect(instance.valid?).to be false
+        expect(instance.errors).to eq ['Missing druid: No druid present in row 3.']
+      end
+    end
+
+    context 'with duplicate druids in separate rows' do
+      let(:csv) do
+        <<~CSV
+          druid,title1:value,title2:value,title3:value
+          druid:ab123cd4567,cool,stuff,here
+          druid:ab123cd4567,cool2,stuff2,here2
+          druid:cd456de5678,value,,
+        CSV
+      end
+
+      it 'finds errors' do
+        expect(instance.valid?).to be false
+        expect(instance.errors).to eq ['Duplicate druids: The druid "druid:ab123cd4567" should occur only once.']
       end
     end
   end
