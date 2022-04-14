f981b98031523f945b5bbd355e6b24a8280b40dd
Aaron Collier <aaron.collier@stanford.edu>
2022-04-14 15:17:21 -0700

    Add testing for validate_cocina_descriptive_job

--- app/controllers/bulk_actions/validate_cocina_descriptive_jobs_controller.rb
+++ app/controllers/bulk_actions/validate_cocina_descriptive_jobs_controller.rb
@@ -4,5 +4,13 @@ module BulkActions
   class ValidateCocinaDescriptiveJobsController < ApplicationController
     include CreatesBulkActions
     self.action_type = 'ValidateCocinaDescriptiveJob'
+
+    def job_params
+      { csv_file: normalized_csv }
+    end
+
+    def normalized_csv
+      @normalized_csv ||= CsvUploadNormalizer.read(params[:csv_file].path)
+    end
   end
 end
