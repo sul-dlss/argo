a345afe24d1476001dab128303705caf742e77d3
Peter Mangiafico <peter@mangiafico.org>
2022-04-14 21:39:25 -0700

    add cocina path validation for desc metadata csv

--- app/services/description_validator.rb
+++ app/services/description_validator.rb
@@ -14,6 +14,7 @@ class DescriptionValidator
   def valid?
     validate_duplicate_headers
     validate_title_headers
+    validate_header_paths
     validate_cell_values
     if @bulk_job
       validate_druid_headers
@@ -48,6 +49,12 @@ class DescriptionValidator
     errors << 'Title column not found.'
   end
 
+  def validate_header_paths
+    invalid_headers.each do |invalid_header|
+      errors << "Column header invalid: #{invalid_header}"
+    end
+  end
+
   def validate_druid_headers
     errors << 'Druid column not found.' if @headers.exclude?('druid')
   end
@@ -69,5 +76,48 @@ class DescriptionValidator
 
   def duplicate_druids
     @csv.map { |row| row['druid'] }.group_by { |e| e }.filter { |_k, v| v.count > 1 }.keys
+  end
+
+  def invalid_headers
+    # The source_id is only for user reference, and we already validate the druid column is present in bulk jobs
+    @headers.excluding('source_id', 'druid').map do |header|
+      split_address = header.scan(/[[:alpha:]]+|[[:digit:]]+/)
+                            .map { |item| /\d+/.match?(item) ? item.to_i - 1 : item.to_sym }
+
+      next if resolve_address(Cocina::Models::Description, split_address)
+
+      header
+    end.compact
+  end
+
+  def resolve_address(root, address)
+    (first, *rest) = address
+    node = root.schema.find { |key| key.name == first }
+    return false unless node
+
+    type = find_type(node)
+
+    if type.respond_to?(:member) # an array
+      return false unless rest.shift.is_a? Integer
+
+      type = type.member
+    end
+
+    return true if rest.empty?
+
+    resolve_address(type, rest)
+  end
+
+  def find_type(node)
+    type = node.type
+    type = type.type if type.default? # Unwrap default type
+    case type
+    when Dry::Types::Constrained
+      type.type # Unwrap constrained type
+    when Dry::Types::Sum::Constrained
+      type.right # Unwrap nillable type
+    else
+      type
+    end
   end
 end
