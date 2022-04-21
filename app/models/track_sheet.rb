51aed087a99293644bc88c7ea16987983bb03eab
Peter Mangiafico <peter@mangiafico.org>
2022-04-21 14:13:12 -0700

    use object title from solr instead of label from solr (#3550)
    * use object title from solr instead of label from solr
    
    * add a test for tracking sheet generation

--- app/models/track_sheet.rb
+++ app/models/track_sheet.rb
@@ -91,11 +91,11 @@ class TrackSheet
 
   # @param [Hash] doc Solr document or to_solr Hash
   # @return [Array<Array<String>>] Complex array suitable for pdf.table()
-  def doc_to_table(doc)
+  def doc_to_table(solr_doc)
+    doc = solr_doc.with_indifferent_access # solr doc always has string keys, SolrDocument::FIELDS can be strings or symbols
     table_data = []
-    labels = doc[SolrDocument::FIELD_LABEL]
-    label = labels.nil? || labels.empty? ? '' : labels.first
-    label = label[0..110] + '...' if label.length > 110
+    labels = doc[SolrDocument::FIELD_TITLE]
+    label = labels.blank? ? '' : labels.first.truncate(110)
     table_data.push(['Object Label:', label])
     table_data.push(['Project Name:', doc['project_tag_ssim'].to_s]) if doc['project_tag_ssim']
 
