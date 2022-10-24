cb5cfc595807414ccb33ccd6a18a03afb9951d8c
Aaron Collier <aaron.collier@stanford.edu>
2022-10-24 15:03:03 -0700

    Complete removal of bad cop data

--- app/models/permitted_queries.rb
+++ app/models/permitted_queries.rb
@@ -40,7 +40,7 @@ class PermittedQueries
       fl: 'id',
       fq: ['objectType_ssim:adminPolicy', '!project_tag_ssim:Hydrus']
     )['response']['docs']
-    resp.pluck('id')
+    resp.map { |doc| doc['id'] }
   end
 
   ##
