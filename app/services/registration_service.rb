1e009c0a48cfe182f3d1933f7dd6ba28588acb92
Michael J. Giarlo <mjgiarlo@stanford.edu>
2023-05-02 09:08:52 -0700

    Create administrative tags before workflow
    Fixes #1043
    
    This commit makes a small tweak to the Argo registration service, creating administrative tags for an object before creating its workflow. This should *ensure* the problematic behavior of getting too far in a workflow (the `goobiWF` in particular) before administrative tags are created since the tag creation in DSA is synchronous.

--- app/services/registration_service.rb
+++ app/services/registration_service.rb
@@ -11,9 +11,10 @@ class RegistrationService
     response = Dor::Services::Client.objects.register(params: model)
     druid = response.externalIdentifier
 
+    Dor::Services::Client.object(druid).administrative_tags.create(tags:) unless tags.empty?
+
     WorkflowClientFactory.build.create_workflow_by_name(druid, workflow, version: "1")
 
-    Dor::Services::Client.object(druid).administrative_tags.create(tags:) unless tags.empty?
     Success(response)
   rescue Dor::Services::Client::UnexpectedResponse => e
     Failure(e)
