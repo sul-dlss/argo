96288fd36d17b80545349bea2208ed8aa9d0d8f8
Michael J. Giarlo <mjgiarlo@stanford.edu>
2023-07-05 16:32:17 -0700

    Prevent APOs and agreements from being republished
    Fixes #4050

--- spec/jobs/republish_job_spec.rb
+++ spec/jobs/republish_job_spec.rb
@@ -3,18 +3,19 @@
 require "rails_helper"
 
 RSpec.describe RepublishJob do
-  let(:druids) { ["druid:123", "druid:456"] }
+  let(:druid) { "druid:123" }
+  let(:druids) { Array(druid) }
   let(:groups) { [] }
   let(:user) { instance_double(User, to_s: "jcoyne85") }
   let(:bulk_action) { create(:bulk_action) }
-
-  let(:client1) { instance_double(Dor::Services::Client::Object, publish: true) }
-  let(:client2) { instance_double(Dor::Services::Client::Object, publish: true) }
+  let(:object) { FactoryBot.create_for_repository(:persisted_item) }
+  let(:object_client) { instance_double(Dor::Services::Client::Object, publish: true, find: object) }
+  let(:workflow_client) { instance_double(Dor::Workflow::Client, lifecycle: lifecycle) }
+  let(:lifecycle) { Time.zone.now }
 
   before do
-    allow(Dor::Services::Client).to receive(:object).with(druids[0]).and_return(client1)
-    allow(Dor::Services::Client).to receive(:object).with(druids[1]).and_return(client2)
-    allow(Dor::Workflow::Client).to receive(:new).and_return(client)
+    allow(Dor::Services::Client).to receive(:object).with(druid).and_return(object_client)
+    allow(Dor::Workflow::Client).to receive(:new).and_return(workflow_client)
 
     described_class.perform_now(bulk_action.id,
       druids:,
@@ -26,21 +27,49 @@ RSpec.describe RepublishJob do
     FileUtils.rm(bulk_action.log_name)
   end
 
-  context "with already published objects" do
-    let(:client) { instance_double(Dor::Workflow::Client, lifecycle: Time.zone.now) }
-
-    it "publishes objects" do
-      expect(client1).to have_received(:publish)
-      expect(client2).to have_received(:publish)
+  context "with already published item" do
+    it "publishes the object" do
+      expect(object_client).to have_received(:publish)
     end
   end
 
-  context "when objects have never been published" do
-    let(:client) { instance_double(Dor::Workflow::Client, lifecycle: nil) }
+  context "with never published item" do
+    let(:lifecycle) { nil }
 
-    it "does not publish objects" do
-      expect(client1).not_to have_received(:publish)
-      expect(client2).not_to have_received(:publish)
+    it "does not publish the object" do
+      expect(object_client).not_to have_received(:publish)
+    end
+  end
+
+  context "with a collection" do
+    let(:object) { FactoryBot.create_for_repository(:persisted_collection) }
+
+    it "publishes the object" do
+      expect(object_client).to have_received(:publish)
+    end
+  end
+
+  context "with an APO" do
+    let(:object) { FactoryBot.create_for_repository(:persisted_apo) }
+
+    it "does not publish the object" do
+      expect(object_client).not_to have_received(:publish)
+    end
+  end
+
+  context "with an agreement" do
+    let(:object) { FactoryBot.create_for_repository(:agreement) }
+
+    it "does not publish the object" do
+      expect(object_client).not_to have_received(:publish)
+    end
+  end
+
+  context "with a NilModel instance" do
+    let(:object) { NilModel.new(druid) }
+
+    it "does not publish the object" do
+      expect(object_client).not_to have_received(:publish)
     end
   end
 end
