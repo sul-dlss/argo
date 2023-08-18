ee8bde628cbbe5b1f436956537498ec65c0c8267
Justin Littman <justinlittman@stanford.edu>
2023-08-18 14:28:53 -0400

    Remove unnecessary separate method in repository.

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -7,7 +7,10 @@ class Repository
   def self.find(id)
     raise ArgumentError, "Missing identifier" unless id
 
-    maybe_load_cocina(id)
+    object_client = Dor::Services::Client.object(id)
+    object_client.find
+  rescue Dor::Services::Client::UnexpectedResponse
+    NilModel.new(id)
   end
 
   # @return [Cocina::Models::DRO,Cocina::Models::Collection,Cocina::Models::AdminPolicy,NilModel] the updated cocina model instance
@@ -15,16 +18,5 @@ class Repository
   def self.store(cocina_object)
     object_client = Dor::Services::Client.object(cocina_object.externalIdentifier)
     object_client.update(params: cocina_object)
-  end
-
-  # Currently we know that not all objects are Cocina compliant, this ensures that we can at least
-  # receive some object and so, at least administrators can be authorized to operate on it.
-  # See: https://argo.stanford.edu/catalog?f[data_quality_ssim][]=Cocina+conversion+failed
-  # @return [Cocina::Models::DRO,NilModel]
-  def self.maybe_load_cocina(druid)
-    object_client = Dor::Services::Client.object(druid)
-    object_client.find
-  rescue Dor::Services::Client::UnexpectedResponse
-    NilModel.new(druid)
   end
 end
