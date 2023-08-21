d4a544f2d90a7545e1d950e2b37048b8aca38c13
Laura Wrubel <lwrubel@stanford.edu>
2023-08-21 08:12:08 -0400

    apply new rubocop rule

--- app/services/cocina_validator.rb
+++ app/services/cocina_validator.rb
@@ -5,14 +5,14 @@ class CocinaValidator
   include Dry::Monads[:try]
   extend Dry::Monads[:result]
 
-  def self.validate(model, **args)
-    Try[Cocina::Models::ValidationError] { model.new(args) }
+  def self.validate(model, **)
+    Try[Cocina::Models::ValidationError] { model.new(**) }
       .to_result
       .or { |exception| Failure([exception.message]) }
   end
 
-  def self.validate_and_save(model, **args)
-    validate(model, **args).bind do |updated|
+  def self.validate_and_save(model, **)
+    validate(model, **).bind do |updated|
       Try[Dor::Services::Client::UnexpectedResponse] { Repository.store(updated) }
         .to_result
         .or { |e| Failure(e.errors.map { |err| err["detail"] }) }
