152bffce52b2096635e149b74616452fc5c3ae6a
Michael J. Giarlo <mjgiarlo@stanford.edu>
2023-09-27 13:29:34 -0700

    Skip DB seeding in production env
    We encountered this error when resetting Argo in QA. We should probably not try to reset Solr, etc. in prod-like envs?

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,14 +3,16 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-require_relative "../spec/support/create_strategy_repository_pattern"
-require_relative "../spec/support/item_method_sender"
-require_relative "../spec/support/apo_method_sender"
-require_relative "../spec/support/reset_solr"
-
-$stdout.puts "This will clear the Solr repo. Are you sure? [y/n]:"
-if gets.chomp == "y"
-  ResetSolr.reset_solr
-  FactoryBot.create_for_repository(:agreement)
-  FactoryBot.create_for_repository(:persisted_apo, roles: [{name: "dor-apo-manager", members: [{identifier: "sdr:administrator-role", type: "workgroup"}]}])
+unless Rails.env.production?
+  require_relative "../spec/support/create_strategy_repository_pattern"
+  require_relative "../spec/support/item_method_sender"
+  require_relative "../spec/support/apo_method_sender"
+  require_relative "../spec/support/reset_solr"
+  
+  $stdout.puts "This will clear the Solr repo. Are you sure? [y/n]:"
+  if gets.chomp == "y"
+    ResetSolr.reset_solr
+    FactoryBot.create_for_repository(:agreement)
+    FactoryBot.create_for_repository(:persisted_apo, roles: [{name: "dor-apo-manager", members: [{identifier: "sdr:administrator-role", type: "workgroup"}]}])
+  end
 end
