4da42740393ad4a844d335fd5ad8d4590ecf1337
Michael J. Giarlo <mjgiarlo@stanford.edu>
2023-09-27 13:33:43 -0700

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@ unless Rails.env.production?
   require_relative "../spec/support/item_method_sender"
   require_relative "../spec/support/apo_method_sender"
   require_relative "../spec/support/reset_solr"
-  
+
   $stdout.puts "This will clear the Solr repo. Are you sure? [y/n]:"
   if gets.chomp == "y"
     ResetSolr.reset_solr
