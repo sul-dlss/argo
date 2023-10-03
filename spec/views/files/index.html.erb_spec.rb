3a0594f4004c806003378b9f2cdae665f84c4a40
Peter Mangiafico <peter@mangiafico.org>
2023-10-03 12:11:09 -0700

    do not offer download link if file not available for download

--- spec/views/files/index.html.erb_spec.rb
+++ spec/views/files/index.html.erb_spec.rb
@@ -4,9 +4,10 @@ require "rails_helper"
 
 RSpec.describe "files/index" do
   let(:admin) { instance_double(Cocina::Models::FileAdministrative, shelve: true, sdrPreserve: true) }
+  let(:file_url) { "https://stacks-test.stanford.edu/file/druid:rn653dy9317/M1090_S15_B01_F07_0106.jp2" }
 
   before do
-    @file = instance_double(Cocina::Models::File, administrative: admin)
+    @file = instance_double(Cocina::Models::File, administrative: admin, access: access)
     @has_been_accessioned = true
     @last_accessioned_version = "7"
     params[:id] = "M1090_S15_B01_F07_0106.jp2"
@@ -14,8 +15,25 @@ RSpec.describe "files/index" do
     render
   end
 
-  it "renders the partial content" do
-    expect(rendered).to have_content "Stacks"
-    expect(rendered).to have_content "Preservation"
+  context "when download access is world" do
+    let(:access) { instance_double(Cocina::Models::FileAccess, download: "world") }
+
+    it "renders the partial content with links" do
+      expect(rendered).to have_content "Stacks"
+      expect(rendered).to have_content "Preservation"
+      expect(rendered).to have_link file_url, href: file_url
+      expect(rendered).not_to have_content "(not available for download)"
+    end
+  end
+
+  context "when download access is none" do
+    let(:access) { instance_double(Cocina::Models::FileAccess, download: "none") }
+
+    it "renders the partial content without links" do
+      expect(rendered).to have_content "Stacks"
+      expect(rendered).to have_content "Preservation"
+      expect(rendered).not_to have_link file_url, href: file_url
+      expect(rendered).to have_content "(not available for download)"
+    end
   end
 end
