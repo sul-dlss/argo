c482054e0e8ed716fd42daa49f3e7f8f1360e47d
Peter Mangiafico <peter@mangiafico.org>
2023-10-16 14:50:01 -0700

    add copy/paste icon to facet modal

---
+++ app/javascript/controllers/copy_controller.js
@@ -0,0 +1,27 @@
+import bootstrap from 'bootstrap/dist/js/bootstrap'
+import { Controller } from '@hotwired/stimulus'
+
+export default class extends Controller {
+  copy (event) {
+    let formattedOutput = ''
+
+    // iterate over all facet values and construct a tab delimited string with two columns
+    const facetList = document.querySelectorAll('.modal-body .facet-values li')
+    facetList.forEach(function (facetItem) {
+      const facetValue = facetItem.querySelector('.facet-label a').innerHTML
+      const facetCount = facetItem.querySelector('.facet-count').innerHTML
+      formattedOutput += facetValue + '\t' + facetCount + '\n'
+    })
+
+    navigator.clipboard.writeText(formattedOutput)
+    event.preventDefault()
+
+    const popover = new bootstrap.Popover(event.target, {
+      content: 'Copied.',
+      placement: 'top',
+      trigger: 'manual'
+    })
+    popover.show()
+    setTimeout(function () { popover.dispose() }, 1500)
+  }
+}
