65fe014a889024a48d8597213ee2bf0345c2415a
Naomi Dushay <ndushay@stanford.edu>
2023-10-20 14:57:01 -0700

    revert!

--- docker/argo-solr/ci-start.sh
+++ docker/argo-solr/ci-start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 bin/solr -cloud -noprompt -p 8983
-server/scripts/cloud-scripts/zkcli.sh -zkhost localhost:9983 -cmd putfile /opt/solr/security.json
+echo 'zookeeper command about to run'
+server/scripts/cloud-scripts/zkcli.sh -zkhost localhost:9983 -cmd putfile /security.json /opt/solr/security.json
 
 echo 'zookeeper command ran'
 
