2aa0d2f129a1a4fa250c6ee50a809b1ed342f28d
Naomi Dushay <ndushay@stanford.edu>
2024-03-11 12:45:58 -0700

    spec/factories and spec/support updates for DROs/items, including explanatory comments

--- spec/support/item_method_sender.rb
+++ spec/support/item_method_sender.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# Used in conjunction with factories/items.rb to create a Cocina::Models::RequestDRO,
+#  allowing for customization of fields/values in the immutable cocina-model.
 class ItemMethodSender
   def initialize(cocina_request)
     @cocina_model = cocina_request
@@ -7,14 +9,27 @@ class ItemMethodSender
 
   attr_reader :cocina_model
 
+  # @example
+  #   item = create(:persisted_item, title: 'simple title value')
   def title=(title)
     @cocina_model = @cocina_model.new(description: { title: [{ value: title }] })
   end
 
+  # @param [Array<Hash>] title_values - an array of arbitrarily complex cocina title values
+  # @example
+  #   item = create(:persisted_item, title_values: [{ value: 'simple title value' }, { value: 'another title' }])
+  def title_values=(title_values)
+    @cocina_model = @cocina_model.new(description: { title: title_values })
+  end
+
+  # @example
+  #   item = create(:persisted_item, source_id: 'sul:M932_1623_B1_F1_001')
   def source_id=(source_id)
     @cocina_model = @cocina_model.new(identification: @cocina_model.identification.new(sourceId: source_id))
   end
 
+  # @example
+  #   item = create(:persisted_item,  collection_id: 'druid:rt923rd3423')
   def collection_id=(collection_id)
     @cocina_model = @cocina_model.new(structural: Cocina::Models::DROStructural.new(isMemberOf: [collection_id]))
   end
