9444d7a1801706d0ad5d73256988e8faa5397140
Peter Mangiafico <peter@mangiafico.org>
2024-06-03 15:16:44 -0700

    add a spec for text creation workflow creation

--- app/controllers/text_extraction_controller.rb
+++ app/controllers/text_extraction_controller.rb
@@ -1,26 +1,34 @@
 # frozen_string_literal: true
 
 class TextExtractionController < ApplicationController
+  before_action :load_and_authorize_resource
+
   def new; end
 
   def create
     languages = params[:text_extraction_languages] || []
-    cocina_object = Repository.find(params[:item_id])
     context = { runOCR: true, manuallyCorrectedOCR: false, ocrLanguages: languages }
     wf_name = 'ocrWF'
 
     # check the workflow is present and active (not archived)
-    return redirect_to solr_document_path(cocina_object.externalIdentifier), flash: { error: "#{wf_name} already exists!" } if helpers.workflow_active?(wf_name, cocina_object.externalIdentifier, cocina_object.version)
+    return redirect_to solr_document_path(@cocina_object.externalIdentifier), flash: { error: "#{wf_name} already exists!" } if helpers.workflow_active?(wf_name, @cocina_object.externalIdentifier, @cocina_object.version)
 
-    WorkflowClientFactory.build.create_workflow_by_name(cocina_object.externalIdentifier,
+    WorkflowClientFactory.build.create_workflow_by_name(@cocina_object.externalIdentifier,
                                                         wf_name,
                                                         context:,
-                                                        version: cocina_object.version)
+                                                        version: @cocina_object.version)
 
     # Force a Solr update before redirection.
-    Dor::Services::Client.object(cocina_object.externalIdentifier).reindex
+    Dor::Services::Client.object(@cocina_object.externalIdentifier).reindex
 
     msg = "Added #{wf_name}"
-    redirect_to solr_document_path(cocina_object.externalIdentifier), notice: msg
+    redirect_to solr_document_path(@cocina_object.externalIdentifier), notice: msg
+  end
+
+  private
+
+  def load_and_authorize_resource
+    @cocina_object = Repository.find(params[:item_id])
+    authorize! :update, @cocina_object
   end
 end
--- spec/requests/text_extraction_spec.rb
+++ spec/requests/text_extraction_spec.rb
@@ -4,25 +4,49 @@ require 'rails_helper'
 
 RSpec.describe 'TextExtractions', :js do
   let(:current_user) { create(:user) }
+  let(:druid) { 'druid:bc123df4567' }
+  let(:cocina_model) { build(:dro_with_metadata, id: druid, version: 2, type: 'https://cocina.sul.stanford.edu/models/document') }
 
   before do
-    sign_in current_user
+    allow(Repository).to receive(:find).with(druid).and_return(cocina_model)
+    sign_in current_user, groups: ['sdr:administrator-role']
   end
 
-  it 'New page has a populate druids button and div with last search' do
-    visit '/items/druid:kt881ty7544/text_extraction/new'
+  describe '#new' do
+    it 'shows text extraction form with languages' do
+      visit "/items/#{druid}/text_extraction/new"
 
-    expect(page).to have_css 'h3', text: 'Text Extraction'
-    expect(page).to have_css 'div', text: 'Content language'
+      expect(page).to have_css 'h3', text: 'Text Extraction'
+      expect(page).to have_content 'Avoid auto-generating OCR files for PDF documents'
+      expect(page).to have_css 'div', text: 'Content language'
 
-    first('button[aria-label="toggle dropdown"]').click
+      first('button[aria-label="toggle dropdown"]').click
 
-    find('[data-text-extraction-label="Adyghe"]').click
+      find('[data-text-extraction-label="Adyghe"]').click
 
-    expect(page).to have_css 'div', text: 'Selected language(s)'
-    expect(page.all('.language-label').count).to eq 1
+      expect(page).to have_css 'div', text: 'Selected language(s)'
+      expect(page.all('.language-label').count).to eq 1
 
-    find('[data-text-extraction-label="English"]').click
-    expect(page.all('.language-label').count).to eq 2
+      find('[data-text-extraction-label="English"]').click
+      expect(page.all('.language-label').count).to eq 2
+    end
+  end
+
+  describe '#create' do
+    let(:object_client) { instance_double(Dor::Services::Client::Object, reindex: true) }
+    let(:workflow_client) { instance_double(Dor::Workflow::Client, workflow:, create_workflow_by_name: nil, lifecycle: Time.zone.now) }
+    let(:workflow) { instance_double(Dor::Workflow::Response::Workflow, active_for?: false) }
+
+    before do
+      allow(WorkflowClientFactory).to receive(:build).and_return(workflow_client)
+      allow(Dor::Services::Client).to receive(:object).and_return(object_client)
+    end
+
+    it 'adds ocrWF' do
+      post "/items/#{druid}/text_extraction", params: { text_extraction_languages: ['English'] }
+      expect(workflow_client).to have_received(:create_workflow_by_name).with(druid, 'ocrWF', context: { runOCR: true, manuallyCorrectedOCR: false, ocrLanguages: ['English'] }, version: 2)
+      expect(object_client).to have_received(:reindex)
+      expect(response).to redirect_to(solr_document_path(druid))
+    end
   end
 end
