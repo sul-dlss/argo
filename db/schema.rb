65c2b7618d74b0e18cb0a5d33020a7d340b334cc
Justin Coyne <jcoyne@justincoyne.com>
2024-08-01 10:42:29 -0500

    Upgrade to Rails 7.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2021_04_06_023300) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_01_155124) do
   create_table "bookmarks", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "document_id"
