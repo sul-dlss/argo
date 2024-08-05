b1d383b7cd342cada8e6300f6e1a8f0336e2de16
Michael J. Giarlo <mjgiarlo@stanford.edu>
2024-08-05 10:00:53 -0700

    Use ViewComponent 3.x slot syntax

--- spec/components/blacklight_modal_component_spec.rb
+++ spec/components/blacklight_modal_component_spec.rb
@@ -5,9 +5,9 @@ require 'rails_helper'
 RSpec.describe BlacklightModalComponent, type: :component do
   it 'renders the bootstrap modal' do
     render_inline(described_class.new) do |component|
-      component.header { 'header' }
-      component.body { 'body' }
-      component.footer { 'footer' }
+      component.with_header { 'header' }
+      component.with_body { 'body' }
+      component.with_footer { 'footer' }
     end
     expect(page).to have_css('.btn-close')
     expect(page).to have_content('header')
