d72eefdec2c140969347e21740443edb5bac31da
Peter Mangiafico <peter@mangiafico.org>
2024-11-18 10:57:54 -0800

    do not allow sdr-ingest steps to be set to skip or complete

--- app/components/application_component.rb
+++ app/components/application_component.rb
@@ -7,4 +7,8 @@ class ApplicationComponent < ViewComponent::Base
   def inspect
     "#<#{self.class.name}:#{object_id}>"
   end
+
+  def allow_skip_or_complete?(workflow_step)
+    %w[sdr-ingest-transfer sdr-ingest-received].exclude?(workflow_step)
+  end
 end
--- spec/components/workflow_step_status_selector_spec.rb
+++ spec/components/workflow_step_status_selector_spec.rb
@@ -10,12 +10,36 @@ RSpec.describe WorkflowStepStatusSelector, type: :component do
                     status: 'error',
                     pid: 'druid:132',
                     workflow_name: 'accessionWF',
-                    name: 'technical-metadata')
+                    name:)
   end
 
-  it 'has a form' do
-    expect(body.css('select[name="status"] > option').map(&:text)).to eq %w[Select Rerun Skip Complete]
-    expect(body.css('input[name="_method"][value="put"]')).to be_present
-    expect(body.css('input[name="process"]').first['value']).to eq 'technical-metadata'
+  context 'when a step allows all updates' do
+    let(:name) { 'technical-metadata' }
+
+    it 'has a form with all options' do
+      expect(body.css('select[name="status"] > option').map(&:text)).to eq %w[Select Rerun Skip Complete]
+      expect(body.css('input[name="_method"][value="put"]')).to be_present
+      expect(body.css('input[name="process"]').first['value']).to eq 'technical-metadata'
+    end
+  end
+
+  context 'when sdr-ingest-received' do
+    let(:name) { 'sdr-ingest-received' }
+
+    it 'has a form without skip or complete' do
+      expect(body.css('select[name="status"] > option').map(&:text)).to eq %w[Select Rerun]
+      expect(body.css('input[name="_method"][value="put"]')).to be_present
+      expect(body.css('input[name="process"]').first['value']).to eq 'sdr-ingest-received'
+    end
+  end
+
+  context 'when sdr-ingest-transfer' do
+    let(:name) { 'sdr-ingest-transfer' }
+
+    it 'has a form without skip or complete' do
+      expect(body.css('select[name="status"] > option').map(&:text)).to eq %w[Select Rerun]
+      expect(body.css('input[name="_method"][value="put"]')).to be_present
+      expect(body.css('input[name="process"]').first['value']).to eq 'sdr-ingest-transfer'
+    end
   end
 end
