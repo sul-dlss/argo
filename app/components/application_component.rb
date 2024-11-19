377c00d52b2b6f1971a4048830fa7d92084e31e7
Peter Mangiafico <peter@mangiafico.org>
2024-11-19 09:59:59 -0800

    add code comment

--- app/components/application_component.rb
+++ app/components/application_component.rb
@@ -8,6 +8,10 @@ class ApplicationComponent < ViewComponent::Base
     "#<#{self.class.name}:#{object_id}>"
   end
 
+  # this is used by the WorkFlowUpdateButton and the WorklowStepStatusSelect components
+  # to determine if the user is allowed to skip or complete a particular workflow step
+  # (because we do not want to allow the user to manually skip or complete some steps)
+  # see https://github.com/sul-dlss/argo/issues/4670
   def allow_skip_or_complete?(workflow_step)
     %w[sdr-ingest-transfer sdr-ingest-received].exclude?(workflow_step)
   end
