36a87777fc91d8a9fe9dcc2448a83aca9e1738af
Justin Littman <justinlittman@stanford.edu>
2025-03-28 07:30:45 -0400

    Remove redundant purge actions.

--- app/services/purge_service.rb
+++ app/services/purge_service.rb
@@ -1,15 +1,10 @@
 # frozen_string_literal: true
 
-# Removes an object from dor-services-app, the workflow service and solr.
+# Removes an object from SDR.
 class PurgeService
   # @param [String] druid
   # @param [String] user_name
   def self.purge(druid:, user_name:)
     Dor::Services::Client.object(druid).destroy(user_name:)
-    WorkflowClientFactory.build.delete_all_workflows(pid: druid)
-    blacklight_config = CatalogController.blacklight_config
-    solr_conn = blacklight_config.repository_class.new(blacklight_config).connection
-    solr_conn.delete_by_id(druid)
-    solr_conn.commit
   end
 end
--- spec/services/purge_service_spec.rb
+++ spec/services/purge_service_spec.rb
@@ -7,22 +7,14 @@ RSpec.describe PurgeService do
     subject(:purge) { described_class.purge(druid: 'druid:ab123cd4567', user_name: 'dijkstra') }
 
     let(:object_client) { instance_double(Dor::Services::Client::Object, destroy: true) }
-    let(:workflow_client) { instance_double(Dor::Workflow::Client, delete_all_workflows: true) }
-    let(:solr_client) { instance_double(RSolr::Client, delete_by_id: true, commit: true) }
-    let(:repo) { instance_double(Blacklight::Solr::Repository, connection: solr_client) }
 
     before do
       allow(Dor::Services::Client).to receive(:object).and_return(object_client)
-      allow(WorkflowClientFactory).to receive(:build).and_return(workflow_client)
-      allow(Blacklight::Solr::Repository).to receive(:new).and_return(repo)
     end
 
     it 'removes the object' do
       purge
       expect(object_client).to have_received(:destroy)
-      expect(workflow_client).to have_received(:delete_all_workflows)
-      expect(solr_client).to have_received(:delete_by_id)
-      expect(solr_client).to have_received(:commit)
     end
   end
 end
