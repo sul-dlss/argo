f5ee02dae08b29329913f7ccd268eec9ac47bb74
Aaron Collier <aaron.collier@stanford.edu>
2025-05-01 16:41:55 -0700

    Allow titleX.type to be blank if titleX.value is blank

--- app/services/description_validator.rb
+++ app/services/description_validator.rb
@@ -63,13 +63,21 @@ class DescriptionValidator
     end
   end
 
-  # verify that each titleX.structuredValueY.type is a valid cocina title type
+  # verify that each titleX.type and titleX.structuredValueY.type is a valid cocina title type with a corresponding *.value
   def validate_structured_title_types
-    @headers.grep(/\Atitle\d+.structuredValue\d+\.type\z/).each do |header|
-      @csv.values_at(header).flatten.each do |title_type|
-        next if valid_title_type?(title_type)
+    @headers.grep(/\Atitle\d+.type\z|\Atitle\d+.structuredValue\d+\.type\z/).each do |title_type_header|
+      title_value_header = title_type_header.sub('type', 'value')
 
-        errors << "Invalid title type: #{header} = #{title_type}"
+      @csv.each do |row|
+        next if row[title_value_header].blank? && row[title_type_header].blank?
+
+        if row[title_value_header].blank?
+          errors << "Missing title value for #{title_type_header}."
+        elsif valid_title_type?(row[title_type_header])
+          next
+        end
+
+        errors << "Invalid title type: #{title_type_header} = #{row[title_type_header]}"
       end
     end
   end
