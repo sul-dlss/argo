59b602fc7e2cabb4eac1b45942129d7ff998f10d
Justin Littman <justinlittman@stanford.edu>
2025-10-20 16:14:07 -0400

    Tweak rendering of cocina model.
    closes #4915

--- app/javascript/controllers/json_renderer.js
+++ app/javascript/controllers/json_renderer.js
@@ -2,18 +2,23 @@ import * as renderjson from 'renderjson'
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = ['cocina', 'section']
+  static targets = ['section']
+  static values = {
+    cocina: Object
+  }
 
   connect () {
-    this.cocina = JSON.parse(this.cocinaTarget.innerText)
+    renderjson.set_icons('\uF4FC', '\uF2E8') // Bootstrap icons
     this.collapse()
   }
 
-  expand () {
-    this.sectionTarget.replaceChildren(renderjson.set_show_to_level('all')(this.cocina))
+  expand (event) {
+    if (event) event.preventDefault()
+    this.sectionTarget.replaceChildren(renderjson.set_show_to_level('all')(this.cocinaValue))
   }
 
-  collapse () {
-    this.sectionTarget.replaceChildren(renderjson.set_show_to_level(1)(this.cocina))
+  collapse (event) {
+    if (event) event.preventDefault()
+    this.sectionTarget.replaceChildren(renderjson.set_show_to_level(1)(this.cocinaValue))
   }
 }
