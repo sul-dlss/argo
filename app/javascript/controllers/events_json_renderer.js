dcaef49c2076b56da5095637a89663f4e3523489
Aaron Collier <aaron.collier@stanford.edu>
2025-10-20 15:05:18 -0700

    Toggle plus minus button

--- app/javascript/controllers/events_json_renderer.js
+++ app/javascript/controllers/events_json_renderer.js
@@ -2,18 +2,25 @@ import * as renderjson from 'renderjson'
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = ['events', 'eventsSection']
+  static targets = ['events', 'eventsSection', 'toggle']
 
   connect () {
-    this.events = JSON.parse(this.eventsTarget.innerText)
-    this.collapse()
+    this.event = JSON.parse(this.eventsTarget.innerText)[0]
+    this.toggle()
   }
 
-  expand () {
-    this.eventsSectionTarget.replaceChildren(renderjson.set_show_to_level('all')(this.events))
-  }
-
-  collapse () {
-    this.eventsSectionTarget.replaceChildren(renderjson.set_show_to_level(1)(this.events))
+  toggle () {
+    const isCollapsed = this.eventsSectionTarget.classList.contains('collapsed')
+    if (isCollapsed) {
+      this.eventsSectionTarget.classList.remove('collapsed')
+      this.eventsSectionTarget.replaceChildren(renderjson.set_show_to_level('all')(this.event))
+      this.toggleTarget.classList.remove('bi-plus')
+      this.toggleTarget.classList.add('bi-dash')
+    } else {
+      this.eventsSectionTarget.classList.add('collapsed')
+      this.eventsSectionTarget.replaceChildren(renderjson.set_show_to_level(1)(this.event))
+      this.toggleTarget.classList.remove('bi-dash')
+      this.toggleTarget.classList.add('bi-plus')
+    }
   }
 }
--- app/presenters/event_hash_presenter.rb
+++ app/presenters/event_hash_presenter.rb
@@ -2,11 +2,12 @@
 
 # Presents the event hash in a collapsable format for easier viewing
 class EventHashPresenter
+  # param [Event] event the event hash to present
   def initialize(event:)
-    @event = event
+    @event_data = event.data
   end
 
   def render
-    DeepCompactBlank.run(enumerable: @event)
+    DeepCompactBlank.run(enumerable: @event_data)
   end
 end
