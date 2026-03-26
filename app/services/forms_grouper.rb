4588437991cd0f4f55e5f3559c17de948eb151f8
Michael J. Giarlo <mjgiarlo@stanford.edu>
2026-03-25 17:00:14 -0700

    Commit first refactoring step

--- app/services/forms_grouper.rb
+++ app/services/forms_grouper.rb
@@ -22,79 +22,15 @@ class FormsGrouper
 
   def initialize(descriptions:)
     @descriptions = descriptions
-    # Grab types early, before any mutation happens, and put the form type values
-    # in order from the most ubiquitous value to the least. Why? This way the
-    # leftmost columns will be more populated, with less used values pushed to
-    # the right.
-    @ordered_mapping = ordered_mapping_from(descriptions)
+    @ordered_mapping = SeedMappingBuilder.new(descriptions).build
   end
 
   def group
     descriptions.transform_values do |description|
-      # Build up a description-specific mapping as a way to track for the current description
-      # how we are mapping old form elements to new ones.
-      mapping = {}
-
-      # First, we rename all the form header values, e.g., "form1.source.uri" to "old_form1.source.uri"
-      # so that we can track which values have already been changed, else we get collisions and are
-      # unable to distinguish already grouped data from yet-to-be-grouped data.
-      description.transform_keys! { |key| key.match?(/^form\d+/) ? key.sub(/^(\D+)/, 'old_\1') : key }
-
-      description.transform_keys! do |key|
-        # Short-circuit by returning the key unchanged if not form-related.
-        next key unless (form_number = key.match(/^old_form(\d+)/))
-
-        # If we already have a description-specific mapping for the form_number
-        # being operated on, use it and move on
-        next key.sub(/^old_form\d+/, mapping[form_number[0]]) if mapping.key?(form_number[0])
-
-        # Get the type value of the form number corresponding to the key currently being transformed
-        type_for_form_number = description.slice(*description.keys.grep(/^old_form#{form_number[1]}\.type/)).values.first
-
-        # Look up the form number of the type value
-        new_form_number = if description.slice(*description.keys.grep(/form.+type/)).one? do |_key, value|
-                               value == type_for_form_number
-                             end
-                            # If there is only one matching form number in the mapping, use it and move on.
-                            ordered_mapping.key(type_for_form_number)
-                          else
-                            # If there are multiple forms of this type, e.g., an
-                            # item with multiple forms of type "genre", use the
-                            # first form number not already used
-                            unused_form = ordered_mapping.find do |mapped_form_number, type_value|
-                              type_value == type_for_form_number &&
-                                !description.key?(key.sub(/^old_form\d+/, mapped_form_number)) &&
-                                !mapping.key(mapped_form_number)
-                            end
-
-                            if unused_form
-                              unused_form.first
-                            else
-                              # If there is no matching form number, increment the
-                              # current highest form number and use it for form
-                              # elements for this type.
-                              # Unless there isn't a highest form number, in which case,
-                              # start with form1.
-                              new_form = if (max_form = ordered_mapping.max_by { |k, _v| k[/\d+/].to_i }&.first)
-                                           field, number = max_form.scan(/(\D+)(\d+)/).first
-                                           "#{field}#{number.to_i.succ}"
-                                         else
-                                           'form1'
-                                         end
-                              ordered_mapping[new_form] = type_for_form_number
-                              new_form
-                            end
-                          end
-
-        # Fall back to original number if look-up returned nil
-        new_form_number ||= "form#{form_number[1]}"
-
-        # Extend the description-specific mapping with the above information
-        mapping[form_number[0]] = new_form_number
-
-        # Map the current "old" form number element to the new one.
-        key.sub(/^old_form\d+/, new_form_number)
-      end
+      DescriptionRewriter.new(
+        description: description,
+        ordered_mapping: ordered_mapping
+      ).rewrite!
     end
   end
 
@@ -102,46 +38,120 @@ class FormsGrouper
 
   attr_reader :descriptions, :ordered_mapping
 
-  def ordered_mapping_from(descriptions)
-    type_values = descriptions.values.map do |description|
-      description.slice(*description.keys.grep(/^form\d+\.type/)).values
+  class SeedMappingBuilder
+    def initialize(descriptions)
+      @descriptions = descriptions
     end
-    # e.g.: [
-    #         ["genre", "resource type", "form", "extent", "digital origin", "genre"],
-    #         ["genre", "resource type", "resource type", "form", "extent", "digital origin"],
-    #         ["resource type", "form", "extent", "digital origin"]
-    #       ]
-    unique_types_in_order = type_values
-                            .flatten
-                            .index_with { |type| type_values.flatten.count(type) }
-                            .sort_by { |_value, count| -count }
-                            .to_h
-                            .keys
 
-    # ["resource type", "form", "extent", "digital origin", "genre"]
-    repeat_types_counts = {}
-    type_values
-      .map { |row| row&.select { |field| row.count(field) > 1 } }
-      .compact_blank
-      .each do |repeats|
-      repeat_types_counts.merge!(
-        repeats.index_with { |e| repeats.count(e) }
-      )
+    def build
+      typed_rows = descriptions.values.map { |desc| typed_values_in_form_order(desc) }
+      flat = typed_rows.flatten
+
+      unique_in_freq_order = flat.tally.sort_by { |_type, count| -count }.map(&:first)
+
+      max_repeats = Hash.new(1)
+      typed_rows.each do |row|
+        row.tally.each do |type, count|
+          max_repeats[type] = [max_repeats[type], count].max
+        end
+      end
+
+      expanded = []
+      unique_in_freq_order.each do |type|
+        max_repeats[type].times { expanded << type }
+      end
+
+      expanded.map.with_index(1) { |type, i| ["form#{i}", type] }.to_h
     end
-    # repeat_types_counts is now:
-    #
-    # e.g.: {"resource type"=>2, "genre"=>2}
-    repeat_types_counts.each do |value, count|
-      # Subtract 1 from the repeat types since each already appears once in the types list
-      unique_types_in_order.insert(unique_types_in_order.index(value), *Array.new(count - 1) { value })
+
+    private
+
+    attr_reader :descriptions
+
+    def typed_values_in_form_order(description)
+      description.keys
+                 .grep(/\Aform\d+\.type\z/)
+                 .sort_by { |k| k[/\d+/].to_i }
+                 .map { |k| description[k] }
     end
-    # unique_types_in_order is now:
-    #
-    # ["resource type", "resource type", "form", "extent", "digital origin", "genre", "genre"]
-    #
-    # What's important here is the repeat keys (multiple resource types and genres) are immediately adjacent
-    unique_types_in_order.map.with_index(1).to_h { |key, index| ["form#{index}", key] }
-    # {"form1"=>"resource type", "form2"=>"resource type", "form3"=>"form", "form4"=>"extent",
-    #  "form5"=>"digital origin", "form6"=>"genre", "form7"=>"genre"}
+  end
+
+  class DescriptionRewriter
+    def initialize(description:, ordered_mapping:)
+      @description = description
+      @allocator = SlotAllocator.new(description: description, ordered_mapping: ordered_mapping)
+    end
+
+    def rewrite!
+      local_map = {}
+
+      rename_to_old_prefixes!
+
+      description.transform_keys! do |key|
+        old_form = old_form_prefix(key)
+        next key unless old_form
+
+        target = local_map[old_form] ||= allocator.allocate(old_form, key)
+        key.sub(/\Aold_form\d+/, target)
+      end
+
+      description
+    end
+
+    private
+
+    attr_reader :description, :allocator
+
+    def rename_to_old_prefixes!
+      description.transform_keys! { |k| k.sub(/\A(form\d+)/, 'old_\1') }
+    end
+
+    def old_form_prefix(key)
+      m = key.match(/\Aold_form(\d+)/)
+      return unless m
+
+      "old_form#{m[1]}"
+    end
+  end
+
+  class SlotAllocator
+    def initialize(description:, ordered_mapping:)
+      @description = description
+      @ordered_mapping = ordered_mapping
+    end
+
+    def allocate(old_form, key_being_transformed)
+      token = FormToken.token_for(description, old_form)
+
+      target = first_unused_slot_for_token(token, key_being_transformed)
+      return target if target
+
+      append_new_slot(token)
+    end
+
+    private
+
+    attr_reader :description, :ordered_mapping
+
+    def first_unused_slot_for_token(token, key_being_transformed)
+      ordered_mapping.find do |form_num, mapped_token|
+        next false unless mapped_token == token
+
+        remapped_key = key_being_transformed.sub(/\Aold_form\d+/, form_num)
+        !description.key?(remapped_key)
+      end&.first
+    end
+
+    def append_new_slot(token)
+      max = ordered_mapping.keys.map { |k| k[/\d+/].to_i }.max || 0
+      new_form = "form#{max + 1}"
+      ordered_mapping[new_form] = token
+      new_form
+    end  end
+
+  class FormToken
+    def self.token_for(description, old_form)
+      description["#{old_form}.type"] || description["#{old_form}.value"]
+    end
   end
 end
--- app/services/notes_grouper.rb
+++ app/services/notes_grouper.rb
@@ -24,13 +24,43 @@ class NotesGrouper
     @descriptions = descriptions
     # Grab labels and types early, before any mutation happens, and put the note
     # label and type values in order from most ubiquitous to least. Why? This
-    # way the leftmost columns will be more populated, with less used values
+    # way leftmost columns will be more populated, with less used values
     # pushed to the right.
-    @ordered_mapping = ordered_mapping_from(descriptions)
+    @ordered_mapping = SeedMappingBuilder.new(descriptions).build
   end
 
   def group
     descriptions.transform_values do |description|
+      DescriptionRewriter.new(description: description, ordered_mapping: ordered_mapping).rewrite!
+    end
+  end
+
+  private
+
+  attr_reader :descriptions, :ordered_mapping
+
+  class NoteToken
+    def self.from_description(description, prefix)
+      [description["#{prefix}.displayLabel"], description["#{prefix}.type"]]
+    end
+
+    def self.from_grouped_hash(hash, num)
+      [hash["old_note#{num}.displayLabel"], hash["old_note#{num}.type"]]
+    end
+
+    def self.from_ungrouped_hash(hash, num)
+      [hash["note#{num}.displayLabel"], hash["note#{num}.type"]]
+    end
+  end
+
+  class DescriptionRewriter
+    def initialize(description:, ordered_mapping:)
+      @description = description
+      @ordered_mapping = ordered_mapping
+      @slot_allocator = SlotAllocator.new(description: description, ordered_mapping: ordered_mapping)
+    end
+
+    def rewrite!
       # Build up a description-specific mapping as a way to track for the current description
       # how we are mapping old note elements to new ones.
       mapping = {}
@@ -38,49 +68,29 @@ class NotesGrouper
       # First, we rename all the note header values, e.g., "note1.type" to "old_note1.type"
       # so that we can track which values have already been changed, else we get collisions and are
       # unable to distinguish already grouped data from yet-to-be-grouped data.
-      description.transform_keys! { |key| key.match?(/^note\d+/) ? key.sub(/^(\D+)/, 'old_\1') : key }
+      description.transform_keys! do |key|
+        key.match?(/^note\d+/) ? key.sub(/^(\D+)/, 'old_\1') : key
+      end
 
       description.transform_keys! do |key|
         # Short-circuit by returning the key unchanged if not note-related.
-        next key unless (note_number = key.match(/^old_note(\d+)/))
+        note_number = key.match(/^old_note(\d+)/)
+        next key unless note_number
 
         # If we already have a description-specific mapping for the note_number
         # being operated on, use it and move on
-        next key.sub(/^old_note\d+/, mapping[note_number[0]]) if mapping.key?(note_number[0])
+        if mapping.key?(note_number[0])
+          next key.sub(/^old_note\d+/, mapping[note_number[0]])
+        end
 
-        # Get the displayLabel value of the note number corresponding to the key currently being transformed
-        label_for_note_number = description.slice(*description.keys.grep(/^old_note#{note_number[1]}\.displayLabel/)).values.first
-        # Get the type value of the note number corresponding to the key currently being transformed
-        type_for_note_number = description.slice(*description.keys.grep(/^old_note#{note_number[1]}\.type/)).values.first
+        # Get the displayLabel and type values of the note number corresponding to the key currently being transformed
+        note_token = NoteToken.from_description(description, "old_note#{note_number[1]}")
+        label_for_note_number, type_for_note_number = note_token
 
-        # Look up the note number of the [displayLabel, type] tuple value
-        new_note_number = case description.slice(*description.keys.grep(/note.+(displayLabel|type)/))
-                                          .group_by { |k, _v| k.match(/(.*note\d+)\./)[1] }
-                                          .count do |_key, value|
-                                 hash = value.to_h
-                                 num = hash.keys.first[/\d+/]
-                                 [hash["old_note#{num}.displayLabel"],
-                                  hash["old_note#{num}.type"]] == [label_for_note_number, type_for_note_number] ||
-                                   [hash["note#{num}.displayLabel"],
-                                    hash["note#{num}.type"]] == [label_for_note_number, type_for_note_number]
-                               end
-
-                          when 1
-                            # If there is only one matching note number in the mapping, use it and move on.
-                            ordered_mapping.key([label_for_note_number, type_for_note_number])
-                          else
-                            # If there are multiple notes of this type, e.g., an
-                            # item with multiple notes of type "abstract" with a
-                            # nil display label, use the first note number not
-                            # already used.
-                            # Also applies when there are no displayLabels or types for the note
-                            ordered_mapping.find do |mapped_note_number, (label_value, type_value)|
-                              [label_value,
-                               type_value] == [label_for_note_number,
-                                               type_for_note_number] && !description.key?(key.sub(/^old_note\d+/,
-                                                                                                  mapped_note_number))
-                            end&.first
-                          end
+        new_note_number = slot_allocator.allocate(
+          key: key,
+          note_token: note_token,
+        )
 
         # Fall back to original number if look-up returned nil
         new_note_number ||= "note#{note_number[1]}"
@@ -91,95 +101,101 @@ class NotesGrouper
         # Map the current "old" note number element to the new one.
         key.sub(/^old_note\d+/, new_note_number)
       end
+
+      description
+    end
+
+    private
+
+    attr_reader :description, :ordered_mapping, :slot_allocator
+  end
+
+  class SlotAllocator
+    def initialize(description:, ordered_mapping:)
+      @description = description
+      @ordered_mapping = ordered_mapping
+    end
+
+    def allocate(key:, note_token:)
+      case matching_note_tuple_count(note_token)
+      when 1
+        # If there is only one matching note number in the mapping, use it and move on.
+        ordered_mapping.key(note_token)
+      else
+        # If there are multiple notes of this type, e.g., an
+        # item with multiple notes of type "abstract" with a
+        # nil display label, use the first note number not
+        # already used.
+        # Also applies when there are no displayLabels or types for the note
+        ordered_mapping.find do |mapped_note_number, mapped_note_token|
+          mapped_note_token == note_token &&
+            !description.key?(key.sub(/^old_note\d+/, mapped_note_number))
+        end&.first
+      end
+    end
+
+    private
+
+    attr_reader :description, :ordered_mapping
+
+    def matching_note_tuple_count(note_token)
+      description.slice(*description.keys.grep(/note.+(displayLabel|type)/))
+        .group_by { |k, _v| k.match(/(.*note\d+)\./)[1] }
+        .count { |_key, value| tuple_matches?(value, note_token) }
+    end
+
+    def tuple_matches?(value, note_token)
+      hash = value.to_h
+      num = hash.keys.first[/\d+/]
+      NoteToken.from_grouped_hash(hash, num) == note_token ||
+        NoteToken.from_ungrouped_hash(hash, num) == note_token
     end
   end
 
-  private
+  class SeedMappingBuilder
+    def initialize(descriptions)
+      @descriptions = descriptions
+    end
 
-  attr_reader :descriptions, :ordered_mapping
+    def build
+      label_and_type_values = descriptions.values.map do |description|
+        notes_count = description.keys.grep(/^note\d+\./).max_by { |field| field[/\d+/].to_i }
+        next if notes_count.nil?
 
-  def ordered_mapping_from(descriptions)
-    label_and_type_values = descriptions
-                            .values
-                            .map do |description|
-      notes_count = description.keys.grep(/^note\d+\./).max_by { |field| field[/\d+/].to_i }
-      next if notes_count.nil?
-
-      1.upto(notes_count[/\d+/].to_i).map do |note_number|
-        [
-          description["note#{note_number}.displayLabel"],
-          description["note#{note_number}.type"]
-        ]
+        1.upto(notes_count[/\d+/].to_i).map do |note_number|
+          NoteToken.from_description(description, "note#{note_number}")
+        end
       end
+
+      return {} unless label_and_type_values.any?
+
+      # Order based on frequency of a given tuple
+      unique_types_in_order = label_and_type_values
+                              .flatten(1)
+                              .index_with { |note_type| label_and_type_values.flatten(1).count(note_type) }
+                              .sort_by { |_value, count| -count }
+                              .to_h
+                              .keys
+
+      repeat_types_counts = {}
+      label_and_type_values
+        .map { |row| row&.select { |field| row.count(field) > 1 } }
+        .compact_blank
+        .each do |repeats|
+          repeat_types_counts.merge!(
+            repeats.index_with { |e| repeats.count(e) }
+          )
+        end
+
+      repeat_types_counts.each do |value, count|
+        unique_types_in_order.insert(unique_types_in_order.index(value), *Array.new(count - 1) { value })
+      end
+
+      unique_types_in_order.map.with_index(1).to_h { |key, index| ["note#{index}", key] }
     end
-    # e.g., when passed a set of six descriptions
-    # [
-    #   [[nil, "statement of responsibility"], [nil, nil], [nil, "system details"], ["Display label", nil], ["Another label", "condition"], ["Display label", nil]],
-    #   [[nil, "abstract"], [nil, nil], [nil, nil], [nil, nil], [nil, "system details"], ["Display label", nil], ["Another label", "condition"]],
-    #   [[nil, "abstract"], [nil, "statement of responsibility"], [nil, nil], [nil, nil], [nil, "condition"]],
-    #   [[nil, nil], [nil, nil], [nil, nil]],
-    #   [[nil, "abstract"], [nil, "date/sequential designation"], [nil, nil]],
-    #   [[nil, "abstract"], ["Contents", "table of contents"]]
-    # ]
-    return {} unless label_and_type_values.any?
 
-    # Order based on frequency of a given tuple
-    unique_types_in_order = label_and_type_values
-                            .flatten(1)
-                            .index_with { |note_type| label_and_type_values.flatten(1).count(note_type) }
-                            .sort_by { |_value, count| -count }
-                            .to_h
-                            .keys
-    # e.g.:
-    # [
-    #   [nil, nil],
-    #   [nil, "abstract"],
-    #   ["Display label", nil],
-    #   [nil, "statement of responsibility"],
-    #   [nil, "system details"],
-    #   ["Another label", "condition"],
-    #   [nil, "condition"],
-    #   [nil, "date/sequential designation"],
-    #   ["Contents", "table of contents"]
-    # ]
+    private
 
-    repeat_types_counts = {}
-    label_and_type_values
-      .map { |row| row&.select { |field| row.count(field) > 1 } }
-      .compact_blank
-      .each do |repeats|
-      repeat_types_counts.merge!(
-        repeats.index_with { |e| repeats.count(e) }
-      )
-    end
-    # e.g.: {["Display label", nil]=>2, [nil, nil]=>3}
-
-    repeat_types_counts.each do |value, count|
-      unique_types_in_order.insert(unique_types_in_order.index(value), *Array.new(count - 1) { value })
-    end
-    # unique_types_in_order is now:
-    #
-    # [
-    #   [nil, nil],
-    #   [nil, nil],
-    #   [nil, nil],
-    #   [nil, "abstract"],
-    #   ["Display label", nil],
-    #   ["Display label", nil],
-    #   [nil, "statement of responsibility"],
-    #   [nil, "system details"],
-    #   ["Another label", "condition"],
-    #   [nil, "condition"],
-    #   [nil, "date/sequential designation"],
-    #   ["Contents", "table of contents"]
-    # ]
-    #
-    # What's important here is the repeat tuples (e.g., double nils) are immediately adjacent
-
-    unique_types_in_order.map.with_index(1).to_h { |key, index| ["note#{index}", key] }
-    # {"note1"=>[nil, nil], "note2"=>[nil, nil], "note3"=>[nil, nil], "note4"=>[nil, "abstract"],
-    #  "note5"=>["Display label", nil], "note6"=>["Display label", nil], "note7"=>[nil, "statement of responsibility"],
-    #  "note8"=>[nil, "system details"], "note9"=>["Another label", "condition"], "note10"=>[nil, "condition"],
-    #  "note11"=>[nil, "date/sequential designation"], "note12"=>["Contents", "table of contents"]}
+    attr_reader :descriptions
   end
 end
